07ee2864e11f2a7f36770aef8a381ede2403ad85
nagarjuna.p <nagarjuna.p@pennanttech.com>
2014-09-11 07:10:33 +0000

    MyId generator changes for custom components
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@48926 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/com/pennant/util/MyIdgenerator.java
+++ PFSWeb/src/com/pennant/util/MyIdgenerator.java
@@ -1,8 +1,6 @@
 package com.pennant.util;
 
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Desktop;
@@ -10,13 +8,26 @@ import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.metainfo.ComponentInfo;
 import org.zkoss.zk.ui.metainfo.Property;
 import org.zkoss.zk.ui.sys.IdGenerator;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Decimalbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Textbox;
+
+import com.pennant.AccountSelectionBox;
+import com.pennant.CurrencyBox;
+import com.pennant.ExtendedCombobox;
+import com.pennant.webui.util.searchdialogs.ExtendedSearchListBox;
+import com.pennant.webui.util.searchdialogs.ExtendedStaticListBox;
 
 public class MyIdgenerator implements IdGenerator {
 
+
 	public String nextComponentUuid(Desktop desktop, Component comp,
 			ComponentInfo compInfo) {
-		
-		String inputComponent = "textbox|button|text|uppercasebox|listbox|checkbox|radiogroup|radio|decimalbox";
+
+		String inputComponent = "textbox|button|text|uppercasebox|listbox|checkbox|radiogroup|radio|decimalbox"
+				                 +"|extendedcombobox|accountSelectionBox|currencyBox";
 		String pageName;
 		int i = Integer.parseInt(desktop.getAttribute("Id_Num").toString());
 		i++;// Start from 1
@@ -29,57 +40,93 @@ public class MyIdgenerator implements IdGenerator {
 			if (id != null) {
 				uuid.append(id).append("_");
 			}
-			String tag = compInfo.getTag();
-			/*if (tag != null) {
-				uuid.append(tag).append("_");
-			}*/
-		}
-			if (uuid.length() == 0) {
+		} else {
+			uuid.append(getId(comp));
 
-				return "zkcomp_" + i;
+			if (uuid.length() > 0) {
+				return uuid.toString();
 			}
-			else
-			{
-				
-				if (compInfo.getTag() != null && inputComponent.indexOf(compInfo.getTag()) >= 0) {
-					pageName = compInfo.getParent().getPageDefinition().getRequestPath();
-					pageName = pageName.substring(compInfo.getParent().getPageDefinition().getRequestPath().lastIndexOf("/")+1, compInfo.getParent().getPageDefinition().getRequestPath().lastIndexOf("zul")-1);
-					return pageName + "_" + uuid.append(i).toString();		
-				}
-				else {
-					return uuid.append(i).toString();		
-				}
+		}
+
+		if (uuid.length() == 0) {
+			return "zkcomp_" + i;
+		} else {
+
+			if (compInfo.getTag() != null
+					&& inputComponent.indexOf(compInfo.getTag()) >= 0) {
+				pageName = compInfo.getParent().getPageDefinition()
+						.getRequestPath();
+				pageName = pageName.substring(
+						compInfo.getParent().getPageDefinition()
+								.getRequestPath().lastIndexOf("/") + 1,
+						compInfo.getParent().getPageDefinition()
+								.getRequestPath().lastIndexOf("zul") - 1);
+				return pageName + "_" + uuid.append(i).toString();
+			} else {
+				return uuid.append(i).toString();
 			}
+		}
+	}
+
+	public String getId(Component comp) {
+		String result = "";
+		Component p = comp.getParent();
+
+		if (p != null && p.getUuid() != null) {
+			if (p instanceof ExtendedCombobox
+					|| p instanceof ExtendedSearchListBox
+					|| p instanceof ExtendedStaticListBox
+					|| p instanceof AccountSelectionBox
+					|| p instanceof CurrencyBox) {
+				result = comp.getClass().getSimpleName() + "_" + p.getUuid();
+			} else if (comp instanceof Textbox || comp instanceof Button
+					|| comp instanceof Decimalbox || comp instanceof Listitem || comp instanceof Listcell) {
+				Component g = p.getParent();
+
+				if (g != null && g.getUuid() != null) {
+					if (g instanceof ExtendedCombobox
+							|| g instanceof ExtendedSearchListBox
+							|| g instanceof ExtendedStaticListBox
+							|| g instanceof AccountSelectionBox
+							|| g instanceof CurrencyBox) {
+						result = comp.getClass().getSimpleName() + "_"
+								+ p.getUuid();
+					}
+				}
+			} else {
+				// Need To Add Code For Other Components(like Menu Item, List Item, List Cell, etc..)
+			}
+		}
+
+		return result;
 	}
 
 	public String getId(ComponentInfo compInfo) {
 		try {
-		List<Property> properties = compInfo.getProperties();
-		for (Property property : properties) {
-			if ("id".equals(property.getName())) {
-				return property.getRawValue();
+			List<Property> properties = compInfo.getProperties();
+			for (Property property : properties) {
+				if ("id".equals(property.getName())) {
+					return property.getRawValue();
+				}
 			}
-		}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return null;
 		}
-	 	
+
 		return null;
 	}
 
 	public String getType(ComponentInfo compInfo) {
 		try {
-			
-		List<Property> properties = compInfo.getProperties();
-		for (Property property : properties) {
-			if ("type".equals(property.getName())) {
-				return property.getRawValue();
+
+			List<Property> properties = compInfo.getProperties();
+			for (Property property : properties) {
+				if ("type".equals(property.getName())) {
+					return property.getRawValue();
+				}
 			}
-		}
-	
-		}
-		catch (Exception e) {
+
+		} catch (Exception e) {
 			return null;
 		}
 
@@ -87,17 +134,15 @@ public class MyIdgenerator implements IdGenerator {
 	}
 
 	public String getName(ComponentInfo compInfo) {
-		
-		try 
-		{
-		List<Property> properties = compInfo.getProperties();
-		for (Property property : properties) {
-			if ("name".equals(property.getName())) {
-				return property.getRawValue();
+
+		try {
+			List<Property> properties = compInfo.getProperties();
+			for (Property property : properties) {
+				if ("name".equals(property.getName())) {
+					return property.getRawValue();
+				}
 			}
-		}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			return null;
 		}
 
@@ -115,5 +160,4 @@ public class MyIdgenerator implements IdGenerator {
 	public String nextPageUuid(Page page) {
 		return null;
 	}
-
 }
