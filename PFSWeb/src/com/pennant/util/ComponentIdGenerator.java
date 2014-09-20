99f5956bdcd88029e5e37864a9906da0b1880c08
Sai Krishna P <saikrishna.p@pennanttech.com>
2014-09-20 09:02:38 +0000

    Refactoring UUID Generator
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@49312 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/com/pennant/util/ComponentIdGenerator.java
+++ PFSWeb/src/com/pennant/util/ComponentIdGenerator.java
@@ -9,30 +9,50 @@ import org.zkoss.zk.ui.sys.IdGenerator;
 
 public class ComponentIdGenerator implements IdGenerator {
 	private static final String PREFIX = "zk_comp_";
+	private static final String COMP_SET = "";
 
 	@Override
 	public String nextComponentUuid(Desktop desktop, Component comp,
 			ComponentInfo compInfo) {
+		// Set the index back to the desktop
 		String number;
 
 		if ((number = (String) desktop.getAttribute("Id_Num")) == null) {
 			number = "0";
-			desktop.setAttribute("Id_Num", number);
 		}
 
 		int index = Integer.parseInt(number);
 		index++;
 
+		desktop.setAttribute("Id_Num", index);
+
+		// Get the page file name
+		String pageName = "";
+
+		if (compInfo != null) {
+			pageName = compInfo.getParent().getPageDefinition()
+					.getRequestPath();
+			pageName = pageName.substring(pageName.lastIndexOf("/") + 1,
+					pageName.lastIndexOf("zul") - 1);
+		}
+
 		StringBuilder uuid = new StringBuilder("");
 
-		// Use ID if one available
+		// Use "id" if one available
 		if (compInfo != null) {
 			for (Property property : compInfo.getProperties()) {
-				if ("id".equals(property.getName())
-						&& property.getRawValue() != null) {
+				if ("id".equals(property.getName())) {
+					uuid.append(pageName).append("_");
 					uuid.append(property.getRawValue()).append("_");
+					uuid.append(index);
+
+					return uuid.toString();
 				}
 			}
+		}
+
+		if (uuid.length() == 0) {
+			uuid.append(PREFIX);
 		}
 
 		return null;
