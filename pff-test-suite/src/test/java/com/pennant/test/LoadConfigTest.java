57356ac431eaa9ccde2098bffa640ae9057839f7
Sai Krishna P <saikrishna.p@pennanttech.com>
2014-09-25 05:00:41 +0000

    Re-factoring
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@49450 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-test-suite/src/test/java/com/pennant/test/LoadConfigTest.java
+++ pff-test-suite/src/test/java/com/pennant/test/LoadConfigTest.java
@@ -6,48 +6,54 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import com.pennant.equation.util.HostConnection;
+
 public class LoadConfigTest {
 	final String[] CONFIG_LOCATIONS = new String[] {
-			"applicationContext-db.xml", "applicationContext-daos.xml",
-			"applicationContext-equation-interface.xml",
-			"applicationContext-zkoss.xml", "customize-applicationContext.xml" };
+			"../PLFInterface/src/applicationContext-equation-interface.xml",
+			"../PFSJava/src/applicationContext-daos.xml",
+			"../PFSWeb/src/applicationContext-db.xml",
+			"../PFSWeb/src/customize-applicationContext.xml" };
 	static ApplicationContext context;
 
 	@BeforeSuite
 	public void setUp() {
 		System.out.println("Initializing application context...");
 
-//		BeanDefinition definition = BeanDefinitionBuilder
-//				.rootBeanDefinition(Arrays.class, "asList")
-//				.addConstructorArgValue(new String[] {}).getBeanDefinition();
-//
-//		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-//		factory.registerBeanDefinition("args", definition);
-//
-//		GenericApplicationContext parent = new GenericApplicationContext(
-//				factory);
-//		parent.refresh();
-//
-//		context = new ClassPathXmlApplicationContext(CONFIG_LOCATIONS, parent);
+		BeanDefinition definition = BeanDefinitionBuilder
+				.rootBeanDefinition(Arrays.class, "asList")
+				.addConstructorArgValue(new String[] {}).getBeanDefinition();
+
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		factory.registerBeanDefinition("args", definition);
+
+		GenericApplicationContext parent = new GenericApplicationContext(
+				factory);
+		parent.refresh();
+
+		context = new FileSystemXmlApplicationContext(CONFIG_LOCATIONS, parent);
 	}
 
 	@Test
 	public void testBean() {
-		// FinanceTypeServiceImpl impl = (FinanceTypeServiceImpl) context
-		// .getBean("financeTypeService");
-		//
-		// System.out.println(impl instanceof FinanceTypeServiceImpl);
-		System.out.println("Done!!!");
+		Object bean = context.getBean("hostConnection");
+
+		Assert.assertTrue(bean instanceof HostConnection);
+
+		bean = null;
 	}
 
 	@AfterSuite
 	public void tearDown() {
-		System.out.println("Finalizing...");
+		System.out.println("Performing cleanup operations...");
+
+		context = null;
 	}
 }
