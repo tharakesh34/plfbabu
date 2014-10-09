3817cfeb2f7685b71b66c89c41075d918ed2596c
Sai Krishna P <saikrishna.p@pennanttech.com>
2014-10-09 10:06:19 +0000

    Re-factoring schedule test cases
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@49855 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-test-suite/src/test/java/com/pennant/testing/schedule/GraceProfitReducingRateTestFactory.java
+++ pff-test-suite/src/test/java/com/pennant/testing/schedule/GraceProfitReducingRateTestFactory.java
@@ -41,22 +41,10 @@ public class GraceProfitReducingRateTestFactory {
 
 	@DataProvider
 	public Object[][] dataset() {
-		Object[][] result = new Object[7][];
+		Object[][] result = new Object[1][];
 
 		result[0] = new Object[] { "SN01_RR_EQUAL_REQ", "EQUAL", 7500000,
 				11250745, 10347622, "ADJMDT", 14853053, 10663922 };
-		result[1] = new Object[] { "SN01_RR_EQUAL", "EQUAL", 0, 7802114,
-				10222300, "TILLDATE", 11404422, 10538600 };
-		result[2] = new Object[] { "SN01_RR_PFT", "PFT", 0, 90561822, 13230000,
-				"ADJMDT", 93929831, 13537794 };
-		result[3] = new Object[] { "SN01_RR_PRI_REQ", "PRI", 7500000, 11182143,
-				10279020, "ADJMDT", 14780490, 10591359 };
-		result[4] = new Object[] { "SN01_RR_PRI", "PRI", 0, 8718137, 10190530,
-				"TILLDATE", 12316484, 10502869 };
-		result[5] = new Object[] { "SN01_RR_PRIPFT_REQ", "PRI_PFT", 7000000,
-				13081152, 10385450, "ADJMDT", 16449162, 10693244 };
-		result[6] = new Object[] { "SN01_RR_PRIPFT", "PRI_PFT", 0, 7546818,
-				10182267, "TILLDATE", 10914827, 10490061 };
 
 		return result;
 	}
