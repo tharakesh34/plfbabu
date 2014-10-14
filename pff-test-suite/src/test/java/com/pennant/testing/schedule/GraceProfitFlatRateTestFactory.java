eb9d1d1e4bf1d09174c9c1d5eca5790aab79c971
Sai Krishna P <saikrishna.p@pennanttech.com>
2014-10-14 05:06:07 +0000

    Re-factoring schedule test cases
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@49943 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-test-suite/src/test/java/com/pennant/testing/schedule/GraceProfitFlatRateTestFactory.java
+++ pff-test-suite/src/test/java/com/pennant/testing/schedule/GraceProfitFlatRateTestFactory.java
@@ -1,19 +1,24 @@
 package com.pennant.testing.schedule;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
-import org.testng.annotations.DataProvider;
+import jxl.Sheet;
+import jxl.read.biff.BiffException;
+
 import org.testng.annotations.Factory;
 
 import com.pennant.app.constants.CalculationConstants;
 import com.pennant.app.util.DateUtility;
 import com.pennant.backend.model.finance.FinScheduleData;
+import com.pennant.testing.Dataset;
 import com.pennant.util.BeanFactory;
 
 public class GraceProfitFlatRateTestFactory {
-	@Factory(dataProvider = "dataset")
-	public Object[] createObjects(String name, String scheduleMethod,
-			long reqRepayAmt, long expLastRepayAmt, long expTotalProfit) {
+	final String SHEET_NAME = "GraceProfitFlatRate";
+
+	@Factory
+	public Object[] createObjects() throws BiffException, IOException {
 		FinScheduleData schedule = BeanFactory.getSchedule(true);
 		schedule.getFinanceMain().setNumberOfTerms(12);
 		schedule.getFinanceMain().setReqTerms(12);
@@ -33,37 +38,14 @@ public class GraceProfitFlatRateTestFactory {
 		schedule.getFinanceMain().setRepayRateBasis("F");
 		schedule.getFinanceMain().setAllowGrcRepay(true);
 
-		schedule.getFinanceMain().setScheduleMethod(scheduleMethod);
-		schedule.getFinanceMain().setReqRepayAmount(
-				BigDecimal.valueOf(reqRepayAmt));
-		if (reqRepayAmt == 0) {
-			schedule.getFinanceMain().setCalculateRepay(true);
-			schedule.getFinanceMain().setEqualRepay(true);
-		} else {
-			schedule.getFinanceMain().setCalculateRepay(false);
-			schedule.getFinanceMain().setEqualRepay(false);
+		// Prepare the tests
+		Sheet dataset = Dataset.getSchedule(SHEET_NAME);
+		int testCount = dataset.getColumns() - 3;
+		Object[] result = new Object[testCount];
+
+		for (int i = 0; i < testCount; i++) {
+			result[i] = new ScheduleTest(schedule, dataset.getColumn(i + 3));
 		}
-
-		return new Object[] { new ScheduleTest(name, schedule, expLastRepayAmt,
-				expTotalProfit) };
-	}
-
-	@DataProvider
-	public Object[][] dataset() {
-		Object[][] result = new Object[7][];
-
-		result[0] = new Object[] { "SN02_FR_EQUAL_REQ", "EQUAL", 8000000,
-				8633123, 13230000 };
-		result[1] = new Object[] { "SN02_FR_EQUAL", "EQUAL", 0, 8052763,
-				13230000 };
-		result[2] = new Object[] { "SN02_FR_PFT", "PFT", 0, 90561822, 13230000 };
-		result[3] = new Object[] { "SN02_FR_PRI_REQ", "PRI", 7500000, 14255337,
-				13352214 };
-		result[4] = new Object[] { "SN02_FR_PRI", "PRI", 0, 11231767, 13352214 };
-		result[5] = new Object[] { "SN02_FR_PRIPFT_REQ", "PRI_PFT", 7000000,
-				13561822, 13230000 };
-		result[6] = new Object[] { "SN02_FR_PRIPFT", "PRI_PFT", 0, 8061822,
-				13230000 };
 
 		return result;
 	}
