5d0625fbc7b486d7791b5635c17e256f2f688a5e
nagarjuna.p <nagarjuna.p@pennanttech.com>
2014-10-21 04:56:06 +0000

    committed changes regarding Removed unused imports rule in sonar
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@50203 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/com/pennant/backend/dao/finance/impl/FinAgreementDetailDAOImpl.java
+++ PFSJava/src/com/pennant/backend/dao/finance/impl/FinAgreementDetailDAOImpl.java
@@ -49,7 +49,6 @@ import javax.sql.DataSource;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
