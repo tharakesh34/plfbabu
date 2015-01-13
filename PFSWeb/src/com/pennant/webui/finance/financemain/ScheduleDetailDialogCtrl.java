78bc652d892a08fe35b8fcdad947e9c8655fdb46
nagarjuna.p <nagarjuna.p@pennanttech.com>
2015-01-13 06:27:44 +0000

    Schedule Detail Dialog List Header Changes committed
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Trunk@51951 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/com/pennant/webui/finance/financemain/ScheduleDetailDialogCtrl.java
+++ PFSWeb/src/com/pennant/webui/finance/financemain/ScheduleDetailDialogCtrl.java
@@ -1693,21 +1693,6 @@ public class ScheduleDetailDialogCtrl extends GFCBaseListCtrl<FinanceScheduleDet
 		logger.debug("Leaving");
 	}
 
-	public void doSetListHeaders(){
-		if(getFinScheduleData().getFinanceMain().isStepFinance()){
-			//if(getFinScheduleData().getFinanceMain().isAlwManualSteps()){  // TODO  Based on Fin type we need to change list header label name
-				this.listHeader_cashFlowEffect.setLabel(Labels.getLabel("listheader_sellingPricePft.label"));
-				this.listHeader_vSProfit.setLabel(Labels.getLabel("listheader_rebateBucket.label"));
-				this.listHeader_cashFlowEffect.setVisible(true);
-				this.listHeader_vSProfit.setVisible(true);
-			//}
-		} else {
-			this.listHeader_cashFlowEffect.setVisible(false);
-			this.listHeader_vSProfit.setVisible(false);
-			this.listHeader_orgPrincipalDue.setVisible(false);
-		}  
-	}
-	
 	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	// ++++++++++++++++++ getter / setter +++++++++++++++++++//
 	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++//
