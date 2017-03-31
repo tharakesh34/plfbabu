1aa5f30b7f1f0dad6de2e3cc95007fc517044447
madhubabu.p <madhubabu.p@pennanttech.com>
2017-03-31 04:12:30 +0000

    bug fixes
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@70628 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/model/finance/FinanceGraphReportData.java
+++ PFSJava/src/main/java/com/pennant/backend/model/finance/FinanceGraphReportData.java
@@ -57,7 +57,7 @@ public class FinanceGraphReportData implements Serializable {
 	private BigDecimal financeBal;
 
 	public FinanceGraphReportData() {
-		
+		super();
 	}
 	
 	// ******************************************************//
