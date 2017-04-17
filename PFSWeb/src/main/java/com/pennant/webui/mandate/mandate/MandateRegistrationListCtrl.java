ba6e437750f1829fbefa7ce93ca8bd192df3c01e
durgaprasad.g <durgaprasad.g@pennanttech.com>
2017-04-17 09:50:42 +0000

    DataEngine changes...
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@71459 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/mandate/mandate/MandateRegistrationListCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/mandate/mandate/MandateRegistrationListCtrl.java
@@ -43,14 +43,14 @@
 
 package com.pennant.webui.mandate.mandate;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.zkoss.util.resource.Labels;
@@ -76,24 +76,22 @@ import org.zkoss.zul.Window;
 
 import com.pennant.app.util.CurrencyUtil;
 import com.pennant.app.util.DateUtility;
-import com.pennant.backend.dao.mandate.MandateDAO;
-import com.pennant.backend.dao.mandate.MandateStatusDAO;
 import com.pennant.backend.model.mandate.Mandate;
 import com.pennant.backend.service.mandate.MandateService;
 import com.pennant.backend.util.JdbcSearchObject;
 import com.pennant.backend.util.MandateConstants;
 import com.pennant.backend.util.PennantApplicationUtil;
-import com.pennant.backend.util.PennantConstants;
 import com.pennant.backend.util.PennantJavaUtil;
 import com.pennant.backend.util.PennantStaticListUtil;
-import com.pennant.eod.BatchFileUtil;
 import com.pennant.search.Filter;
 import com.pennant.util.PennantAppUtil;
 import com.pennant.webui.util.GFCBaseListCtrl;
 import com.pennant.webui.util.MessageUtil;
+import com.pennanttech.dataengine.DataEngineExport;
 import com.pennanttech.framework.core.SearchOperator.Operators;
 import com.pennanttech.framework.core.constants.SortOrder;
 import com.pennanttech.framework.web.components.MultiLineMessageBox;
+import com.pennanttech.pff.core.App;
 
 /**
  * ************************************************************<br>
@@ -131,6 +129,7 @@ public class MandateRegistrationListCtrl extends GFCBaseListCtrl<Mandate> implem
 	
 	protected Button					button_MandateList_NewMandate;
 	protected Button					button_MandateList_MandateSearch;
+	protected Button					btnDownload;
 
 	protected Longbox					mandateID;
 	protected Combobox					mandateType;
@@ -153,9 +152,8 @@ public class MandateRegistrationListCtrl extends GFCBaseListCtrl<Mandate> implem
 	protected Listbox					sortOperator_InputDate;
 
 	private transient MandateService	mandateService;
-	private MandateStatusDAO			mandateStatusDAO;
-	private MandateDAO					mandateDAO;
-	
+	private DataSource 					dataSource;
+
 	private Map<Long, String> mandateIdMap = new HashMap<Long, String>();
 
 	/**
@@ -353,6 +351,8 @@ public class MandateRegistrationListCtrl extends GFCBaseListCtrl<Mandate> implem
 		searchObject.addField("mandateID");
 		searchObject.addTabelName(this.tableName);
 		searchObject.addFilterIn("nextRoleCode", getUserWorkspace().getUserRoles(), isFirstTask());
+
+		//FIXME the filters adding
 		List<Map<String, Long>> list = getPagedListWrapper().getPagedListService().getBySearchObject(searchObject);
 
 		List<Long> mandateLst = new ArrayList<Long>();
@@ -468,7 +468,6 @@ public class MandateRegistrationListCtrl extends GFCBaseListCtrl<Mandate> implem
 	 */
 
 	public void onClick$btnDownload(Event event) throws Exception {
-
 		logger.debug("Entering" + event.toString());
 
 		List<Long> mandateIdList;
@@ -491,78 +490,34 @@ public class MandateRegistrationListCtrl extends GFCBaseListCtrl<Mandate> implem
 		if (conf == MultiLineMessageBox.NO) {
 			return;
 		}
-
-		//File file = new File(PathUtil.getPath(PathUtil.DOWNLOAD) + "/" + "MandateData.txt");
-		File file = new File("D:/Data-Engine/PFF/Bajaj/Mandate/MandateData.txt");
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-		FileWriter filewriter = BatchFileUtil.getFileWriter(file);
-
+		//FIXME the process monitor
 		try {
-			StringBuilder builder = new StringBuilder();
-//			addField(builder, "MandateID");
-//			addField(builder, "CustCIF");
-//			addField(builder, "CustShrtName");
-//			addField(builder, "MandateType");
-//			addField(builder, "BranchCode");
-//			addField(builder, "MICR");
-//			addField(builder, "AccNumber");
-//			addField(builder, "StartDate");
-//			addField(builder, "ExpiryDate");
-//			builder.append("MaxLimit");
-//			BatchFileUtil.writeline(filewriter, builder.toString());
-			for (Long mandateId : mandateIdList) {
-				Mandate mandate = mandateService.getMandateById(mandateId);
-				StringBuilder builder1 = new StringBuilder();
-				addField(builder1, String.valueOf(mandate.getMandateID()));
-				addField(builder1, mandate.getCustCIF());
-				addField(builder1, mandate.getCustShrtName());
-				addField(builder1, mandate.getMandateType());
-				addField(builder1, mandate.getBranchCode());
-				addField(builder1, mandate.getMICR());
-				addField(builder1, mandate.getAccNumber());
-				addField(builder1, DateUtility.format(mandate.getStartDate(), PennantConstants.DBDateFormat));
-				addField(builder1, DateUtility.format(mandate.getExpiryDate(), PennantConstants.DBDateFormat));
-				builder.append(String.valueOf(mandate.getMaxLimit()));
-				BatchFileUtil.writeline(filewriter, builder1.toString());
-				mandate.setUserDetails(getUserWorkspace().getLoggedInUser());
-				mandateService.processDownload(mandate);
-			}
-			MessageUtil.showInfoMessage(Labels.getLabel("MandateDataList_Request", new String[] { String.valueOf(mandateIdList.size()) }));
+			btnDownload.setDisabled(true);
+			DataEngineExport dataEngine = new DataEngineExport(dataSource, getUserWorkspace().getLoggedInUser().getLoginUsrID(), App.DATABASE.name());
+
+			Map<String, Object> filterMap = new HashMap<>();
+			filterMap.put("MANDATEID", mandateIdList);
+			dataEngine.setFilterMap(filterMap);
+
+			dataEngine.exportData("MANDATES_EXPORT");
+
 		} catch (Exception e) {
-			logger.error("Exception", e);
+			logger.error("Exception :", e);
 		} finally {
-			filewriter.close();
 			this.mandateIdMap.clear();
 			this.listHeader_CheckBox_Comp.setChecked(false);
 			search();
+			btnDownload.setDisabled(false);
+			logger.debug("Leaving");
 		}
 	}
-	
-	
-	public void addField(StringBuilder line, String value) {
-		line.append(value).append(BatchFileUtil.DELIMITER);
-	}
+
 
 	public void setMandateService(MandateService mandateService) {
 		this.mandateService = mandateService;
 	}
-
-	public MandateStatusDAO getMandateStatusDAO() {
-		return mandateStatusDAO;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
-
-	public void setMandateStatusDAO(MandateStatusDAO mandateStatusDAO) {
-		this.mandateStatusDAO = mandateStatusDAO;
-	}
-
-	public MandateDAO getMandateDAO() {
-		return mandateDAO;
-	}
-
-	public void setMandateDAO(MandateDAO mandateDAO) {
-		this.mandateDAO = mandateDAO;
-	}
-
 }
