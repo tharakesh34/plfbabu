88576205d3915ba611d36d5721032b765f000011
satish.k <satish.k@pennanttech.com>
2017-04-23 06:39:38 +0000

    EOD Changes
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@71834 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/eod/util/EODProperties.java
+++ PFSJava/src/main/java/com/pennant/eod/util/EODProperties.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.pennant.backend.dao.applicationmaster.CurrencyDAO;
+import com.pennant.backend.dao.applicationmaster.DPDBucketConfigurationDAO;
+import com.pennant.backend.dao.applicationmaster.DPDBucketDAO;
 import com.pennant.backend.dao.applicationmaster.TakafulProviderDAO;
 import com.pennant.backend.dao.masters.SystemInternalAccountDefinitionDAO;
 import com.pennant.backend.dao.rmtmasters.FinanceTypeDAO;
@@ -13,6 +18,8 @@ import com.pennant.backend.dao.rmtmasters.TransactionEntryDAO;
 import com.pennant.backend.dao.rulefactory.RuleDAO;
 import com.pennant.backend.model.ValueLabel;
 import com.pennant.backend.model.applicationmaster.Currency;
+import com.pennant.backend.model.applicationmaster.DPDBucket;
+import com.pennant.backend.model.applicationmaster.DPDBucketConfiguration;
 import com.pennant.backend.model.applicationmaster.TakafulProvider;
 import com.pennant.backend.model.rmtmasters.FinanceType;
 import com.pennant.backend.model.rmtmasters.TransactionEntry;
@@ -33,29 +40,30 @@ import com.pennant.backend.model.rmtmasters.TransactionEntry;
 
 public class EODProperties {
 
-	private static Map<String, FinanceType> finananceTypesMap;
-	private static Map<String, String> rulesMap;
-	private static Map<String, Currency> currencyMap;
-	private static Map<String, String> internalAccMap;	
-	private static Map<String, TakafulProvider> takafulProviders;	
-	private static Map<Long, List<TransactionEntry>> transactionEntriesMap;	
-	
-	private FinanceTypeDAO financeTypeDAO;
-	private CurrencyDAO currencyDAO;
-	private SystemInternalAccountDefinitionDAO definitionDAO;
-	private RuleDAO ruleDAO;
-	private TransactionEntryDAO transactionEntryDAO;
-	private TakafulProviderDAO takafulProviderDAO;
-	
-	
-	
-	
+	private static Map<String, FinanceType>						finananceTypesMap;
+	private static Map<String, String>							rulesMap;
+	private static Map<String, Currency>						currencyMap;
+	private static Map<String, String>							internalAccMap;
+	private static Map<String, TakafulProvider>					takafulProviders;
+	private static Map<Long, List<TransactionEntry>>			transactionEntriesMap;
+	private static Map<String, List<DPDBucketConfiguration>>	bucketConfigurations;
+	private static Map<Long, String>							dPDBuckets;
+
+	private FinanceTypeDAO										financeTypeDAO;
+	private CurrencyDAO											currencyDAO;
+	private SystemInternalAccountDefinitionDAO					definitionDAO;
+	private RuleDAO												ruleDAO;
+	private TransactionEntryDAO									transactionEntryDAO;
+	private TakafulProviderDAO									takafulProviderDAO;
+	private DPDBucketConfigurationDAO							dPDBucketConfigurationDAO;
+	private DPDBucketDAO										dPDBucketDAO;
+
 	public EODProperties() {
 		super();
 	}
 
 	/**
-	 * This method initialise all the static variable  <br>
+	 * This method initialise all the static variable <br>
 	 * 
 	 */
 	public void init() {
@@ -65,13 +73,17 @@ public class EODProperties {
 		List<ValueLabel> siaNumList = getDefinitionDAO().getEntrySIANumDetails();
 		List<Long> accountSetIdList = getTransactionEntryDAO().getAccountSetIds();
 		List<TakafulProvider> listProviders = getTakafulProviderDAO().getTakafulProviders();
-		
+		List<DPDBucketConfiguration> bucketConfigurationList = dPDBucketConfigurationDAO.getDPDBucketConfigurations();
+		List<DPDBucket> dpdBucketList = dPDBucketDAO.getDPDBuckets();
+
 		finananceTypesMap = new HashMap<String, FinanceType>(finTypeList.size());
 		rulesMap = new HashMap<String, String>(ruleList.size());
 		currencyMap = new HashMap<String, Currency>(ccyCodeList.size());
 		internalAccMap = new HashMap<String, String>(siaNumList.size());
 		takafulProviders = new HashMap<String, TakafulProvider>(siaNumList.size());
-		transactionEntriesMap = new HashMap<Long,  List<TransactionEntry>>(accountSetIdList.size());
+		transactionEntriesMap = new HashMap<Long, List<TransactionEntry>>(accountSetIdList.size());
+		bucketConfigurations=new HashMap<String, List<DPDBucketConfiguration>>();
+		dPDBuckets=new HashMap<Long, String>();
 		
 		for (FinanceType type : finTypeList) {
 			finananceTypesMap.put(type.getFinType().trim(), type);
@@ -88,63 +100,86 @@ public class EODProperties {
 		for (ValueLabel valueLabel : siaNumList) {
 			internalAccMap.put(valueLabel.getLabel(), valueLabel.getValue());
 		}
-		
-		for(Long accountSetId : accountSetIdList) {
-			transactionEntriesMap.put(accountSetId, getTransactionEntryDAO().getListTranEntryForBatch(accountSetId, ""));
+
+		for (Long accountSetId : accountSetIdList) {
+			transactionEntriesMap
+					.put(accountSetId, getTransactionEntryDAO().getListTranEntryForBatch(accountSetId, ""));
 		}
-		
+
 		for (TakafulProvider takafulProvider : listProviders) {
 			takafulProviders.put(takafulProvider.getTakafulCode(), takafulProvider);
 		}
+		
+		for (DPDBucket bucket : dpdBucketList) {
+			dPDBuckets.put(bucket.getBucketID(), bucket.getBucketCode());
+		}
 
-		finTypeList = null;	
+		for (DPDBucketConfiguration bucketConfiguration : bucketConfigurationList) {
+			String prductCode = bucketConfiguration.getProductCode();
+			if (bucketConfigurations.containsKey(prductCode)) {
+				bucketConfigurations.get(prductCode).add(bucketConfiguration);
+			} else {
+				List<DPDBucketConfiguration> list = new ArrayList<DPDBucketConfiguration>();
+				list.add(bucketConfiguration);
+				bucketConfigurations.put(prductCode, list);
+			}
+		}
+
+		finTypeList = null;
 		ruleList = null;
 		ccyCodeList = null;
 		siaNumList = null;
 		accountSetIdList = null;
-		listProviders=null;
+		listProviders = null;
+		bucketConfigurationList = null;
+		dPDBuckets = null;
 	}
 
 	/**
 	 * Method for Fetching Finance type Object
+	 * 
 	 * @param finType
 	 * @return
 	 */
 	public static FinanceType getFinanceType(String finType) {
-		return finananceTypesMap.get(finType);		
+		return finananceTypesMap.get(finType);
 	}
 
 	/**
 	 * Method for Fetch SubHead Rule Code By SubHead Code In Transaction Entry
+	 * 
 	 * @param ruleCode
 	 * @return
 	 */
-	public static String getSubHeadRule(String ruleCode) {		
+	public static String getSubHeadRule(String ruleCode) {
 		return rulesMap.get(ruleCode);
 	}
 
 	/**
 	 * Method for Fetching Currency Number by Currency Code
+	 * 
 	 * @param ccyCode
 	 * @return
 	 */
 	public static String getCcyNumber(String ccyCode) {
 		return currencyMap.get(ccyCode).getCcyNumber();
-		
+
 	}
-	
+
 	/**
 	 * Method for Fetching Currency format by Currency Code
+	 * 
 	 * @param ccyCode
 	 * @return
 	 */
 	public static int getCcyEditField(String ccyCode) {
 		return currencyMap.get(ccyCode).getCcyEditField();
-		
+
 	}
 
 	/**
 	 * Method for Fetching SIA Number Number by SIA Code
+	 * 
 	 * @param ccyCode
 	 * @return
 	 */
@@ -153,76 +188,106 @@ public class EODProperties {
 	}
 
 	/**
-	 * Method for Fetching TakafulProvider by  Code
+	 * Method for Fetching TakafulProvider by Code
+	 * 
 	 * @param code
 	 * @return
 	 */
 	public static TakafulProvider getTakafulProvider(String code) {
 		return takafulProviders.get(code);
 	}
-	
+
 	/**
 	 * Method for Fetching TransactionEntry by accountSetId
+	 * 
 	 * @param accountSetId
 	 * @return List<TransactionEntry>
 	 */
 	public static List<TransactionEntry> getTransactionEntryList(Long accountSetId) {
-		
-		if(transactionEntriesMap.containsKey(accountSetId)) {
+
+		if (transactionEntriesMap.containsKey(accountSetId)) {
 			return transactionEntriesMap.get(accountSetId);
 		} else {
 			return new ArrayList<TransactionEntry>();
 		}
 	}
-	
+
+	public static List<DPDBucketConfiguration> getBucketConfigurations(String productCode) {
+		if (bucketConfigurations.containsKey(productCode)) {
+			return bucketConfigurations.get(productCode);
+		} else {
+			return new ArrayList<DPDBucketConfiguration>();
+		}
+	}
+
+	public static String getBucket(long bucketID) {
+		return dPDBuckets.get(bucketID);
+	}
+
+	public static Long getBucketID(String bucketCode) {
+		for (Entry<Long, String> entry : dPDBuckets.entrySet()) {
+			if (StringUtils.equals(entry.getValue(), bucketCode)) {
+				return entry.getKey();
+			}
+
+		}
+		return new Long(0);
+	}
+
 	public void destroy() {
 		finananceTypesMap = null;
 		rulesMap = null;
 		currencyMap = null;
 		internalAccMap = null;
 		transactionEntriesMap = null;
-		takafulProviders =null;
+		takafulProviders = null;
+		bucketConfigurations = null;
+		dPDBuckets = null;
 	}
 
-	
 	// ******************************************************//
 	// ****************** getter / setter *******************//
 	// ******************************************************//
-	
+
 	public FinanceTypeDAO getFinanceTypeDAO() {
-    	return financeTypeDAO;
-    }
+		return financeTypeDAO;
+	}
+
 	public void setFinanceTypeDAO(FinanceTypeDAO financeTypeDAO) {
-    	this.financeTypeDAO = financeTypeDAO;
-    }
+		this.financeTypeDAO = financeTypeDAO;
+	}
 
 	public CurrencyDAO getCurrencyDAO() {
-    	return currencyDAO;
-    }
+		return currencyDAO;
+	}
+
 	public void setCurrencyDAO(CurrencyDAO currencyDAO) {
-    	this.currencyDAO = currencyDAO;
-    }
+		this.currencyDAO = currencyDAO;
+	}
 
 	public SystemInternalAccountDefinitionDAO getDefinitionDAO() {
-    	return definitionDAO;
-    }
+		return definitionDAO;
+	}
+
 	public void setDefinitionDAO(SystemInternalAccountDefinitionDAO definitionDAO) {
-    	this.definitionDAO = definitionDAO;
-    }
+		this.definitionDAO = definitionDAO;
+	}
 
 	public RuleDAO getRuleDAO() {
-    	return ruleDAO;
-    }
+		return ruleDAO;
+	}
+
 	public void setRuleDAO(RuleDAO ruleDAO) {
-    	this.ruleDAO = ruleDAO;
-    }
-	
+		this.ruleDAO = ruleDAO;
+	}
+
 	public TransactionEntryDAO getTransactionEntryDAO() {
-	    return transactionEntryDAO;
-    }
+		return transactionEntryDAO;
+	}
+
 	public void setTransactionEntryDAO(TransactionEntryDAO transactionEntryDAO) {
-	    this.transactionEntryDAO = transactionEntryDAO;
-    }
+		this.transactionEntryDAO = transactionEntryDAO;
+	}
 
 	public TakafulProviderDAO getTakafulProviderDAO() {
 		return takafulProviderDAO;
@@ -231,5 +296,13 @@ public class EODProperties {
 	public void setTakafulProviderDAO(TakafulProviderDAO takafulProviderDAO) {
 		this.takafulProviderDAO = takafulProviderDAO;
 	}
-	
+
+	public void setDPDBucketConfigurationDAO(DPDBucketConfigurationDAO dPDBucketConfigurationDAO) {
+		this.dPDBucketConfigurationDAO = dPDBucketConfigurationDAO;
+	}
+
+	public void setdPDBucketDAO(DPDBucketDAO dPDBucketDAO) {
+		this.dPDBucketDAO = dPDBucketDAO;
+	}
+
 }
