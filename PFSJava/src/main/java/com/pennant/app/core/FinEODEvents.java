5ab7fedd78cc75a72fa698e8ace61fb48d1fe6b9
satish.k <satish.k@pennanttech.com>
2017-04-23 06:04:27 +0000

    new class for fineodevents created
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@71826 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ PFSJava/src/main/java/com/pennant/app/core/FinEODEvents.java
@@ -0,0 +1,12 @@
+package com.pennant.app.core;
+
+import com.pennanttech.pff.core.model.AbstractEntity;
+
+public class FinEODEvents extends AbstractEntity{
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1183720618731771888L;
+
+}
