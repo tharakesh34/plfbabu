1ce5c442745eb61d9e7202f21284778bdbbad773
Sai Krishna P <saikrishna.p@pennanttech.com>
2017-05-11 09:09:56 +0000

    Re-factoring.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@73333 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-test-suite/src/test/java/com/pennanttech/test/LoadConfigTest.java
+++ pff-test-suite/src/test/java/com/pennanttech/test/LoadConfigTest.java
@@ -3,8 +3,6 @@ package com.pennanttech.test;
 import java.io.IOException;
 import java.util.Arrays;
 
-import jxl.read.biff.BiffException;
-
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -19,13 +17,16 @@ import org.testng.annotations.Test;
 import com.pennant.backend.dao.impl.ErrorDetailsDAOImpl;
 import com.pennanttech.util.Dataset;
 
+import jxl.read.biff.BiffException;
+
 public class LoadConfigTest {
 	final String[]				CONFIG_LOCATIONS	= new String[] {
 			"../PLFInterface/src/main/resources/applicationContext-core-interface.xml",
 			"../PLFInterface/src/main/resources/applicationContext-mq-interface.xml",
 			"../PFSJava/src/main/resources/applicationContext-daos.xml",
 			"../PFSWeb/src/main/resources/applicationContext-db.xml", "/src/test/resources/applicationContext.xml",
-			"../pff-interface-bajaj/src/main/resources/client-interfaces-context.xml" };
+			"../pff-interface-bajaj/src/main/resources/client-interfaces-context.xml",
+			"../PFSWeb/src/main/resources/eod-batch-config-service.xml" };
 	static ApplicationContext	context;
 
 	@BeforeSuite
