a4c2b4256c213edf0b44962c414a610224d63ab8
durgaprasad.g <durgaprasad.g@pennanttech.com>
2017-05-18 13:36:11 +0000

    Interface changes ALM, ControlDump,Posidex.....
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@73631 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSBatch/src/main/java/com/pennant/backend/endofday/tasklet/DataExtract.java
+++ PFSBatch/src/main/java/com/pennant/backend/endofday/tasklet/DataExtract.java
@@ -11,11 +11,21 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
 import com.pennant.app.util.DateUtility;
+import com.pennanttech.bajaj.services.ALMRequestService;
+import com.pennanttech.bajaj.services.ControlDumpRequestService;
+import com.pennanttech.bajaj.services.PosidexRequestService;
+import com.pennanttech.bajaj.services.PosidexResponceService;
+import com.pennanttech.pff.core.Literal;
+import com.pennanttech.pff.core.util.DateUtil;
 
 public class DataExtract implements Tasklet {
-	private Logger		logger	= Logger.getLogger(DataExtract.class);
+	private Logger logger = Logger.getLogger(DataExtract.class);
 
-	private DataSource	dataSource;
+	private DataSource dataSource;
+	private ALMRequestService almRequestService;
+	private ControlDumpRequestService controlDumpRequestService;
+	private PosidexRequestService posidexRequestService;
+	private PosidexResponceService posidexResponceService;
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext context) throws Exception {
@@ -23,7 +33,42 @@ public class DataExtract implements Tasklet {
 		logger.debug("START: Data Extract Preparation On : " + valueDate);
 
 		try {
-			//TODO Add the required code here
+			// ALMRequestService
+			try {
+				logger.debug("ALMRequestService started...");
+				Date alFromDate = DateUtil.getMonthStart(DateUtil.addMonths(DateUtil.getSysDate(), -1));
+				Date alToDate = DateUtil.getMonthEnd(DateUtil.addMonths(DateUtil.getSysDate(), -1));
+				this.almRequestService.sendReqest(new Long(1000), alFromDate, alToDate);
+			} catch (Exception e) {
+				logger.error(Literal.EXCEPTION, e);
+			}
+
+			// ControlDumpRequestService
+			try {
+				logger.debug("ControlDumpRequestService started...");
+				this.controlDumpRequestService.sendReqest(new Long(1000));
+			} catch (Exception e) {
+				logger.error(Literal.EXCEPTION, e);
+			}
+
+			// PosidexRequestService
+			try {
+				logger.debug("PosidexRequestService started...");
+				Date poFromDate = DateUtil.getMonthStart(DateUtil.addMonths(DateUtil.getSysDate(), -1));
+				Date toDate = DateUtil.getMonthEnd(DateUtil.addMonths(DateUtil.getSysDate(), -1));
+				this.posidexRequestService.sendReqest(new Long(1000), poFromDate, toDate);
+			} catch (Exception e) {
+				logger.error(Literal.EXCEPTION, e);
+			}
+
+			// PosidexResponceService
+			try {
+				logger.debug("PosidexResponceService started...");
+				this.posidexResponceService.sendReqest(new Long(1000));
+			} catch (Exception e) {
+				logger.error(Literal.EXCEPTION, e);
+			}
+			// FIXME date values and mappings.....
 
 		} catch (Exception e) {
 			logger.error("Exception", e);
@@ -43,6 +88,22 @@ public class DataExtract implements Tasklet {
 
 	public DataSource getDataSource() {
 		return dataSource;
+	}
+
+	public void setAlmRequestService(ALMRequestService almRequestService) {
+		this.almRequestService = almRequestService;
+	}
+
+	public void setControlDumpRequestService(ControlDumpRequestService controlDumpRequestService) {
+		this.controlDumpRequestService = controlDumpRequestService;
+	}
+
+	public void setPosidexRequestService(PosidexRequestService posidexRequestService) {
+		this.posidexRequestService = posidexRequestService;
+	}
+
+	public void setPosidexResponceService(PosidexResponceService posidexResponceService) {
+		this.posidexResponceService = posidexResponceService;
 	}
 
 }
