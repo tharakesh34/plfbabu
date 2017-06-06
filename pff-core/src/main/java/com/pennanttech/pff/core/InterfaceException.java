dbfc9d39c26ec8c93d3f62eeb0f17aa06f4817cf
Sai Krishna P <saikrishna.p@pennanttech.com>
2017-06-06 08:19:43 +0000

    Re-factoring: Throwing AppException instead of DataAccessException.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@74666 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-core/src/main/java/com/pennanttech/pff/core/InterfaceException.java
+++ pff-core/src/main/java/com/pennanttech/pff/core/InterfaceException.java
@@ -1,15 +1,27 @@
+/**
+ * Copyright 2011 - Pennant Technologies
+ * 
+ * This file is part of Pennant Java Application Framework and related Products. All
+ * components/modules/functions/classes/logic in this software, unless otherwise stated, the property of Pennant
+ * Technologies.
+ * 
+ * Copyright and other intellectual property laws protect these materials. Reproduction or retransmission of the
+ * materials, in whole or in part, in any manner, without the prior written consent of the copyright holder, is a
+ * violation of copyright law.
+ */
 package com.pennanttech.pff.core;
 
-public class InterfaceException extends Exception {
+/**
+ * The class {@code InterfaceException} and its subclasses are a form of {@code Throwable} that indicates conditions
+ * that the interface might catch.
+ */
+public class InterfaceException extends AppException {
 	private static final long	serialVersionUID	= 7376419132045778376L;
 
 	public String				errorMessage;
 	public String				errorCode;
 	public String[]				parameters;
 
-	/**
-	 * @return the errorMessage
-	 */
 	public String getErrorMessage() {
 		return errorMessage;
 	}
@@ -21,13 +33,6 @@ public class InterfaceException extends Exception {
 
 	}
 
-	/**
-	 * PFFInterfaceException with multiple field values
-	 * 
-	 * @param errorCode
-	 * @param parameters
-	 * @param message
-	 */
 	public InterfaceException(String errorCode, String[] parameters, String message) {
 		super(message);
 		this.errorCode = errorCode;
