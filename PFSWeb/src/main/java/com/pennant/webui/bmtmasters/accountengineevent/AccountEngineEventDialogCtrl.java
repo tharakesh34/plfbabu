77b7d4363f2cb7411bae84f6cca984324f2d1f59
Sai Krishna P <saikrishna.p@pennanttech.com>
2017-06-07 14:14:59 +0000

    Re-factoring.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@74798 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/bmtmasters/accountengineevent/AccountEngineEventDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/bmtmasters/accountengineevent/AccountEngineEventDialogCtrl.java
@@ -54,7 +54,6 @@ import org.zkoss.zk.ui.UiException;
 import org.zkoss.zk.ui.WrongValueException;
 import org.zkoss.zk.ui.WrongValuesException;
 import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
@@ -71,7 +70,6 @@ import com.pennant.util.ErrorControl;
 import com.pennant.util.Constraint.PTStringValidator;
 import com.pennant.webui.util.GFCBaseCtrl;
 import com.pennant.webui.util.MessageUtil;
-import com.pennant.webui.util.MultiLineMessageBox;
 
 /**
  * This is the controller class for the
@@ -461,15 +459,7 @@ public class AccountEngineEventDialogCtrl extends GFCBaseCtrl<AccountEngineEvent
 		// Show a confirm box
 		final String msg = Labels.getLabel(
 		"message.Question.Are_you_sure_to_delete_this_record") + "\n\n --> " + aAccountEngineEvent.getAEEventCode();
-		final String title = Labels.getLabel("message.Deleting.Record");
-		MultiLineMessageBox.doSetTemplate();
-
-		int conf = MultiLineMessageBox.show(msg, title,
-				MultiLineMessageBox.YES | MultiLineMessageBox.NO, Messagebox.QUESTION, true);
-
-		if (conf == MultiLineMessageBox.YES) {
-			logger.debug("doDelete: Yes");
-
+		if (MessageUtil.confirm(msg) == MessageUtil.YES) {
 			if (StringUtils.isBlank(aAccountEngineEvent.getRecordType())) {
 				aAccountEngineEvent.setVersion(aAccountEngineEvent.getVersion() + 1);
 				aAccountEngineEvent.setRecordType(PennantConstants.RECORD_TYPE_DEL);
