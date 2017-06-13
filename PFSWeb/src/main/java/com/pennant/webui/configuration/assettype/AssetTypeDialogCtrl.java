7e7e0248c029307775d13c25cf05ce490f0e8aea
Sai Krishna P <saikrishna.p@pennanttech.com>
2017-06-13 12:02:13 +0000

    Re-factoring: Throwing AppException instead of DataAccessException.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@75017 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/configuration/assettype/AssetTypeDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/configuration/assettype/AssetTypeDialogCtrl.java
@@ -71,7 +71,6 @@ import org.zkoss.zul.Label;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
-import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Tab;
 import org.zkoss.zul.Tabpanel;
 import org.zkoss.zul.Textbox;
@@ -295,9 +294,7 @@ public class AssetTypeDialogCtrl extends GFCBaseCtrl <AssetType> {
 			preScriptValidated = true;
 			//check if code mirror is empty or not 
 			if(StringUtils.isNotEmpty(this.preValidation.getValue().trim())){
-				int answer = Messagebox.show("NO Errors Found ! Proceed With Simulation ?", "Validated", 
-						Messagebox.YES | Messagebox.NO, Messagebox.QUESTION);
-				if (answer == Messagebox.YES) {
+				if (MessageUtil.confirm("NO Errors Found! Proceed With Simulation?") == MessageUtil.YES) {
 					// create a new window for input values
 					createSimulationWindow(variables, this.preValidation.getValue());
 				}
@@ -318,9 +315,7 @@ public class AssetTypeDialogCtrl extends GFCBaseCtrl <AssetType> {
 			postScriptValidated = true;
 			//check if code mirror is empty or not 
 			if(StringUtils.isNotEmpty(this.postValidation.getValue().trim())){
-				int answer = Messagebox.show("NO Errors Found ! Proceed With Simulation ?", "Validated", 
-						Messagebox.YES | Messagebox.NO, Messagebox.QUESTION);
-				if (answer == Messagebox.YES) {
+				if (MessageUtil.confirm("NO Errors Found! Proceed With Simulation?") == MessageUtil.YES) {
 					// create a new window for input values
 					createSimulationWindow(variables, this.postValidation.getValue());
 				}
@@ -392,7 +387,7 @@ public class AssetTypeDialogCtrl extends GFCBaseCtrl <AssetType> {
 						if (!fieldNames.contains(variable.get("name"))) {
 							// if new variables found throw error message
 							noerrors = false;
-							Messagebox.show("Unknown Variable :" + variable.get("name"), "Unknown", Messagebox.OK, Messagebox.ERROR);
+							MessageUtil.showError("Unknown Variable :" + variable.get("name"));
 							return noerrors;
 						} else {
 							noerrors = true;
@@ -407,8 +402,8 @@ public class AssetTypeDialogCtrl extends GFCBaseCtrl <AssetType> {
 				for (int i = 0; i < errors.size(); i++) {
 					JSONObject error = (JSONObject) errors.get(i);
 					if (error != null) {
-						Messagebox.show(error.get("reason").toString(), "Error : At Line " + error.get("line") + ",Position " + error.get("character"), Messagebox.OK,
-						        Messagebox.ERROR);
+						MessageUtil.showError("Error : At Line " + error.get("line") + ",Position "
+								+ error.get("character") + "\n\n" + error.get("reason").toString());
 						return false;
 					}
 				}
@@ -430,21 +425,21 @@ public class AssetTypeDialogCtrl extends GFCBaseCtrl <AssetType> {
 		if(!bothValidations){
 			if(isPostValidation){
 				if (!this.postValidation.getValue().contains("errors")) {
-					Messagebox.show("Error Details not found ", "ERROR", Messagebox.OK, Messagebox.ERROR);
+					MessageUtil.showError("Error Details not found ");
 					return false;
 				}
 			}else{
 				if (!this.preValidation.getValue().contains("errors")) {
-					Messagebox.show("Error Details not found ", "ERROR", Messagebox.OK, Messagebox.ERROR);
+					MessageUtil.showError("Error Details not found ");
 					return false;
 				}
 			}
 		}else{
 			if (StringUtils.isNotEmpty(this.preValidation.getValue()) && !this.preValidation.getValue().contains("errors")) {
-				Messagebox.show("Error Details not found in Pre Validations.", "ERROR", Messagebox.OK, Messagebox.ERROR);
+				MessageUtil.showError("Error Details not found in Pre Validations.");
 				return false;
 			}else if(StringUtils.isNotEmpty(this.postValidation.getValue()) && !this.postValidation.getValue().contains("errors")){
-				Messagebox.show("Error Details not found in Post Validations.", "ERROR", Messagebox.OK, Messagebox.ERROR);
+				MessageUtil.showError("Error Details not found in Post Validations.");
 				return false;
 			}
 		}
