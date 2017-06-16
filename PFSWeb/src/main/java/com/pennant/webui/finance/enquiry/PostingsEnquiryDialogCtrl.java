a87a8b212da526a3f2f4ddaa6eddc621c5634d4b
durgaprasad.g <durgaprasad.g@pennanttech.com>
2017-06-16 13:40:13 +0000

    Added payment instructions in to enquiry..
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@75309 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/finance/enquiry/PostingsEnquiryDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/finance/enquiry/PostingsEnquiryDialogCtrl.java
@@ -224,7 +224,7 @@ public class PostingsEnquiryDialogCtrl extends GFCBaseCtrl<ReturnDataSet> {
 	private void doFillPostings() {
 		logger.debug("Entering");
 		fillComboBox(this.postingGroup, PennantConstants.EVENTBASE, PennantStaticListUtil.getPostingGroupList(), "");
-		StringBuilder events = new StringBuilder("'ADDDBSF','ADDDBSN','ADDDBSP','COMPOUND','DEFFRQ','DEFRPY','DPRCIATE','EARLYPAY','EARLYSTL','LATEPAY','PIS_NORM','NORM_PIS','RATCHG','REPAY','SCDCHG','WRITEOFF','CMTDISB', 'STAGE', 'ISTBILL', 'GRACEEND','DISBINS','FEEPAY','VASFEE','MANFEE','INSTDATE'");
+		StringBuilder events = new StringBuilder("'ADDDBSF','ADDDBSN','ADDDBSP','COMPOUND','DEFFRQ','DEFRPY','DPRCIATE','EARLYPAY','EARLYSTL','LATEPAY','PIS_NORM','NORM_PIS','RATCHG','REPAY','SCDCHG','WRITEOFF','CMTDISB', 'STAGE', 'ISTBILL', 'GRACEEND','DISBINS','FEEPAY','VASFEE','MANFEE','INSTDATE','PAYMTINS'");
 		
 		if(this.showAccrual.isChecked()) {
 			events.append(",'AMZ','AMZSUSP'");
