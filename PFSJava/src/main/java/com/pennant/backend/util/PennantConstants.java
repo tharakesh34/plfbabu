b39c4f0c0006d86a2abb7f4d299de6d15edf209c
subbarao.m <subbarao.m@pennanttech.com>
2017-06-19 04:33:54 +0000

    removing SeqAMTVehicleDealer from constans as it is normal sequence
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@75399 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/util/PennantConstants.java
+++ PFSJava/src/main/java/com/pennant/backend/util/PennantConstants.java
@@ -53,7 +53,7 @@ public class PennantConstants {
 		SeqCollateral, SeqPostings, SeqFinanceScoreHeader, SeqDocumentDetails, SeqSecLoginLog,SeqAdvpayment,
 		SeqCollateralMovement,SEQPartnerBank, seqFinLogEntryDetail, SeqFinReceiptHeader, SeqFinReceiptDetail, 
 		SeqFinTypePartnerBanks,SeqFinRepayHeader,SeqFinODPenaltyRates,SeqFinExcessAmount,SeqManualAdviseMovements,
-		SeqBuilderGroup,SeqFeePostings,SeqBuilderProjcet,SeqBuilderCompany,SeqFinFeeReceipts,SeqAMTVehicleDealer,SeqTaxDetail
+		SeqBuilderGroup,SeqFeePostings,SeqBuilderProjcet,SeqBuilderCompany,SeqFinFeeReceipts,SeqTaxDetail
 	}
 
 	public static boolean CITY_FREETEXT = false;
