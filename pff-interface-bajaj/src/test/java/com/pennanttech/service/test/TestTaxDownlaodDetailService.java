f8f3cfb425de03fe62a65079508d2c18924c2efb
durgaprasad.g <durgaprasad.g@pennanttech.com>
2017-06-26 12:49:57 +0000

    GST Changes.....
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@75902 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-interface-bajaj/src/test/java/com/pennanttech/service/test/TestTaxDownlaodDetailService.java
@@ -0,0 +1,35 @@
+package com.pennanttech.service.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.pennanttech.bajaj.services.TaxDownlaodDetailServiceImpl;
+import com.pennanttech.pff.core.util.DateUtil;
+
+public class TestTaxDownlaodDetailService {
+
+	TaxDownlaodDetailServiceImpl taxDownlaodDetailServiceImpl;
+	private ApplicationContext context;
+
+	@Before
+	public void startAHI() {
+		try {
+			context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+			taxDownlaodDetailServiceImpl = context.getBean(TaxDownlaodDetailServiceImpl.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void process() {
+		try {
+			this.taxDownlaodDetailServiceImpl.process(new Long(1000),  DateUtil.parse("03-JUL-17", "dd-MMM-yy"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
