0cff128aeb899fbe4d24466f1e52152d26a8f4dc
madhubabu.p <madhubabu.p@pennanttech.com>
2017-06-29 10:28:00 +0000

    GST File Upload Changes
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@76131 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/finance/enquiry/PostingsEnquiryDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/finance/enquiry/PostingsEnquiryDialogCtrl.java
@@ -284,6 +284,7 @@ public class PostingsEnquiryDialogCtrl extends GFCBaseCtrl<ReturnDataSet> {
 			detail.setPostAmount(PennantAppUtil.amountFormate(dataSet.getPostAmount(), CurrencyUtil.getFormat(dataSet.getAcCcy())));
 			detail.setRevTranCode(dataSet.getRevTranCode());
 			detail.setPostDate(DateUtility.formatDate(dataSet.getPostDate(), DateFormat.LONG_DATE.getPattern()));
+			detail.setValueDate(DateUtility.formatDate(dataSet.getValueDate(), DateFormat.LONG_DATE.getPattern()));
 			accountingDetails.add(detail);
 		}
 
