ef8e31bce9e67d461a365c857245e948948a3f5b
subbarao.m <subbarao.m@pennanttech.com>
2017-07-04 13:36:04 +0000

    Validation changes
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@76584 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/util/PennantStaticListUtil.java
+++ PFSJava/src/main/java/com/pennant/backend/util/PennantStaticListUtil.java
@@ -2742,7 +2742,7 @@ public class PennantStaticListUtil {
 		if(phoneTypeRegex == null){
 			phoneTypeRegex = new ArrayList<ValueLabel>(2);
 			phoneTypeRegex.add(new ValueLabel("[0-9]{10}", Labels.getLabel("listheader_MobileNumber.label")));
-			phoneTypeRegex.add(new ValueLabel("[0-9]{11}", Labels.getLabel("listheader_DealerTelephone.label")));
+			phoneTypeRegex.add(new ValueLabel("[0-9]{13}", Labels.getLabel("listheader_DealerTelephone.label")));
 		}
 		return phoneTypeRegex;
 	}
