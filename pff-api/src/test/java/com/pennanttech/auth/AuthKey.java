1d68a5c9e97ec640889ba98676fceb1321866165
srikanth.m <srikanth.m@pennanttech.com>
2017-08-07 12:26:09 +0000

    Add the crackDown Test case
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@78167 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-api/src/test/java/com/pennanttech/auth/AuthKey.java
+++ pff-api/src/test/java/com/pennanttech/auth/AuthKey.java
@@ -1,16 +1,28 @@
 package com.pennanttech.auth;
 
-import org.apache.commons.codec.binary.Base64;
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.security.crypto.codec.Base64;
 import org.testng.annotations.Test;
 
 @Test
 public class AuthKey {
 	@Test
-	public void generate() {
+	public void generate() throws UnsupportedEncodingException {
 		// Usage: user:<user name>:<password>
-		String authDetails = "user:admin:Pennant@123";
-		String authKey = Base64.encodeBase64String(authDetails.getBytes());
+		String authDetails = "user:maker:test";
+		String authKey = new String(Base64.encode(authDetails.getBytes()), "UTF-8");
+
+		System.out.println(authDetails);
+		System.out.println(authKey);
+	}
+
+	@Test
+	public void crackDown() throws UnsupportedEncodingException {
+		String authKey = "dXNlcjp3cm06UGVubmFudEAxMjM=";
+		String authDetails = new String(Base64.decode(authKey.getBytes()), "UTF-8");
 
 		System.out.println(authKey);
+		System.out.println(authDetails);
 	}
 }
