a671a43d1154fb04f8267be9076fc642519ea734
subbarao.m <subbarao.m@pennanttech.com>
2017-08-08 13:46:15 +0000

    issue fixes
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@78208 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/administration/impl/SecurityUserDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/administration/impl/SecurityUserDAOImpl.java
@@ -164,10 +164,10 @@ public class SecurityUserDAOImpl extends BasisNextidDaoImpl<SecurityUser> implem
 	 * @return SecurityUsers
 	 */
 	@Override
-	public SecurityUser getSecurityUserByLogin(final String usrLogin, String type) {
+	public SecurityUser getSecurityUserByLogin(final long usrId, String type) {
 		logger.debug("Entering ");
 		SecurityUser securityUser = new SecurityUser();
-		securityUser.setUsrLogin(usrLogin);
+		securityUser.setUsrID(usrId);
 	
 		StringBuilder selectSql= new StringBuilder("Select UsrID, UsrLogin, UsrPwd, " );
 		selectSql.append(" UserStaffID, UsrFName, UsrMName, UsrLName, UsrMobile, UsrEmail, " );
@@ -184,7 +184,7 @@ public class SecurityUserDAOImpl extends BasisNextidDaoImpl<SecurityUser> implem
 		}
 		selectSql.append("  From SecUsers");
 		selectSql.append(StringUtils.trimToEmpty(type));
-		selectSql.append(" Where UsrLogin =:UsrLogin");
+		selectSql.append(" Where UsrID =:UsrID");
 
 		logger.debug("selectSql:" + selectSql.toString());
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(securityUser);
