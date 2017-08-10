bc63e20f701727b08d42a61eeab909015947afa9
murthy.y <murthy.y@pennanttech.com>
2017-08-10 17:15:35 +0000

    Re-factor Trail Balance and SAP-GL
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@78366 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ PFSBatch/src/main/java/com/pennant/backend/endofday/tasklet/bajaj/SAPGL.java
@@ -0,0 +1,124 @@
+package com.pennant.backend.endofday.tasklet.bajaj;
+
+import com.pennant.app.constants.AccountConstants;
+import com.pennant.app.util.DateUtility;
+import com.pennant.app.util.SysParamUtil;
+import com.pennant.backend.dao.eod.EODConfigDAO;
+import com.pennant.backend.model.eod.EODConfig;
+import com.pennant.backend.util.BatchUtil;
+import com.pennanttech.bajaj.process.SAPGLReportsProcess;
+import com.pennanttech.dataengine.model.DataEngineStatus;
+import com.pennanttech.pennapps.core.resource.Literal;
+import java.util.Date;
+import java.util.List;
+import javax.sql.DataSource;
+import org.apache.log4j.Logger;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SAPGL implements Tasklet {
+	private Logger logger = Logger.getLogger(SAPGL.class);
+
+	private DataSource dataSource;
+	
+	@Autowired
+	private EODConfigDAO eodConfigDAO;
+
+	public EODConfig getEodConfig() {
+		try {
+			List<EODConfig> list = eodConfigDAO.getEODConfig();
+			if (!list.isEmpty()) {
+				return list.get(0);
+			}
+
+		} catch (Exception e) {
+			logger.error("Exception", e);
+		}
+		return null;
+	}
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext context) throws Exception {
+		Date valueDate = DateUtility.getAppValueDate();
+		logger.debug("START: Data Extract Preparation On : " + valueDate);
+
+		try {
+			
+			boolean monthEnd = false;
+			int amzPostingEvent = SysParamUtil.getValueAsInt(AccountConstants.AMZ_POSTING_EVENT);
+			if (amzPostingEvent == AccountConstants.AMZ_POSTING_APP_MTH_END) {
+				if (valueDate.compareTo(DateUtility.getMonthEnd(valueDate)) == 0) {
+					monthEnd = true;
+				}
+			} else if (amzPostingEvent == AccountConstants.AMZ_POSTING_APP_EXT_MTH_END) {
+				if (getEodConfig() != null && getEodConfig().isInExtMnth()) {
+					if (getEodConfig().getMnthExtTo().compareTo(valueDate) == 0) {
+						monthEnd = true;
+					}
+				}
+
+			}
+			// if month end then only it should run
+			if (monthEnd) {
+
+			}
+
+			new SAPGLProcessThread(new Long(1000)).start();
+			DataEngineStatus status = SAPGLReportsProcess.SAP_GL_STATUS;
+			status.setStatus("I");
+		
+			while ("I".equals(status.getStatus())) {
+				BatchUtil.setExecution(context, "TOTAL", String.valueOf(status.getTotalRecords()));
+				BatchUtil.setExecution(context, "PROCESSED", String.valueOf(status.getProcessedRecords()));
+				
+				if("F".equals(status.getStatus())) {
+					throw new Exception();
+				}
+			}
+
+
+		} catch (Exception e) {
+			logger.error("Exception", e);
+		}
+
+		logger.debug("COMPLETE: Data Extract Preparation On :" + valueDate);
+		return RepeatStatus.FINISHED;
+	}
+
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	// ++++++++++++++++++ getter / setter +++++++++++++++++++//
+	// ++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public class SAPGLProcessThread extends Thread {
+		private long userId;
+
+		public SAPGLProcessThread(long userId) {
+			this.userId = userId;
+		}
+
+		public void run() {
+			try {
+				logger.debug("SAP-GL Process initiated...");
+				sleep(1000);
+				new SAPGLReportsProcess(dataSource, userId, DateUtility.getAppValueDate(), DateUtility.getAppDate()).extractReport();
+			} catch (Exception e) {
+				logger.error(Literal.EXCEPTION, e);
+			}
+		}
+	}
+
+
+
+
+}
