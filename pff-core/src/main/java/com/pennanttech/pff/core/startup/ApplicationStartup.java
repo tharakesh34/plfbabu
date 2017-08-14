fc1d018e4b01942743977a07115984ec5ce1bd01
murthy.y <murthy.y@pennanttech.com>
2017-08-14 10:52:35 +0000

    Re-factored
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@78559 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-core/src/main/java/com/pennanttech/pff/core/startup/ApplicationStartup.java
+++ pff-core/src/main/java/com/pennanttech/pff/core/startup/ApplicationStartup.java
@@ -6,29 +6,31 @@ import javax.servlet.ServletContextListener;
 import org.apache.log4j.Logger;
 
 import com.pennanttech.pennapps.core.resource.Literal;
+import com.pennanttech.pff.core.App;
 import com.pennanttech.pff.core.job.scheduler.AbstractJobScheduler;
+import com.pennanttech.pff.core.util.DateUtil;
+import com.pennanttech.pff.core.util.DateUtil.DateFormat;
 
 public class ApplicationStartup implements ServletContextListener {
 	private static final Logger logger = Logger.getLogger(ApplicationStartup.class);
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		logger.debug(Literal.LEAVING);
-
 		try {
 			AbstractJobScheduler.servletContext = event.getServletContext();
 			AbstractJobScheduler.schedular = AbstractJobScheduler.sf.getScheduler();
-			logger.debug("<<<<<< THE APPLICATION STARTED >>>>>>");
+			String dateTime = DateUtil.getSysDate(DateFormat.LONG_DATE_TIME);
+			logger.info("<<<<<The ".concat(App.NAME).concat(" Started on ").concat(dateTime).concat(">>>>>"));
 		} catch (Exception e) {
 			logger.error(Literal.ENTERING, e);
 			AbstractJobScheduler.shutdown();
 		}
-		logger.debug(Literal.LEAVING);
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		AbstractJobScheduler.shutdown();
-		logger.debug("<<<<<< THE APPLICATION STOPED >>>>>>");
+		String dateTime = DateUtil.getSysDate(DateFormat.LONG_DATE_TIME);
+		logger.info("<<<<<The ".concat(App.NAME).concat(" Stopped on ").concat(dateTime).concat(">>>>>"));
 	}
 }
