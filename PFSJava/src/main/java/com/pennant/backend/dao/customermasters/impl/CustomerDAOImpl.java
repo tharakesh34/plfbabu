b1192a1d981813a7667205897ff28f0900de4429
madhubabu.p <madhubabu.p@pennanttech.com>
2017-08-22 14:22:17 +0000

    Installment amount retrived as NschdPri+NschPft from finprofit details
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@78938 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/customermasters/impl/CustomerDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/customermasters/impl/CustomerDAOImpl.java
@@ -1265,10 +1265,10 @@ public class CustomerDAOImpl extends BasisNextidDaoImpl<Customer> implements Cus
 		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
 		mapSqlParameterSource.addValue("CustID", custId);
 		
-		StringBuilder selectSql = new StringBuilder("SELECT FinReference, FinType, FinStatus, FinStartDate,FinCcy, ");
-		selectSql.append(" FinAmount, DownPayment,FeeChargeAmt, InsuranceAmt, FinRepaymentAmount, NumberOfTerms "); 
-		//selectSql.append(" from FinanceEnquiry_View ");
-		selectSql.append(" from FinanceMain ");
+		StringBuilder selectSql = new StringBuilder("SELECT FM.FinReference, FM.FinType, FM.FinStatus, FM.FinStartDate,FM.FinCcy, FM.FinAmount, FM.DownPayment,FM.FeeChargeAmt,");
+		selectSql.append("FM.InsuranceAmt, FM.FinRepaymentAmount, FM.NumberOfTerms, FT.FintypeDesc As lovDescFinTypeName , T6.MaxinstAmount from FinanceMain FM "); 
+		selectSql.append("INNER JOIN RMTfinanceTypes FT ON FT.Fintype=FM.FinType "); 
+		selectSql.append("LEFT JOIN (select FinReference,(NSchdPri+NSchdPft) MaxInstAmount from FinPftdetails)T6 on T6.FinReference=FM.Finreference"); 
  		selectSql.append(" Where CustId = :CustID");
  		
 		logger.debug("selectSql: " + selectSql.toString());
