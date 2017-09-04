5f2d19932cfd5369dd6de581cd0977f03f8fa3c6
madhubabu.p <madhubabu.p@pennanttech.com>
2017-09-04 13:31:21 +0000

    Adding dedit entry to set as one record
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@79325 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/service/others/impl/JVPostingServiceImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/service/others/impl/JVPostingServiceImpl.java
@@ -314,8 +314,17 @@ public class JVPostingServiceImpl extends GenericService<JVPosting> implements J
 	public JVPosting getJVPostingById(long id) {
 		JVPosting jvPosting = getJVPostingDAO().getJVPostingById(id, "_View");
 		if (jvPosting != null) {
-			jvPosting.setJVPostingEntrysList(getjVPostingEntryDAO().getJVPostingEntryListById(
-					jvPosting.getBatchReference(), "_View"));
+			String debitAc=null;
+			List<JVPostingEntry> entries = getjVPostingEntryDAO().getJVPostingEntryListById(
+					jvPosting.getBatchReference(), "_View");
+			
+			for (int i = 0; i < entries.size(); i++) {
+				if(entries.get(i).getDerivedTxnRef()==0){
+					debitAc=entries.get(i+1).getAccount();
+					entries.get(i).setDebitAccount(debitAc);
+				}
+			}
+			jvPosting.setJVPostingEntrysList(entries);
 		}
 		return jvPosting;
 	}
