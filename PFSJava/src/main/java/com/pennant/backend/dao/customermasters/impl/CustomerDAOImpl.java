64bc08c1d6e3324c09136b6c7b7136ddeb7178fa
madhubabu.p <madhubabu.p@pennanttech.com>
2017-09-11 06:11:48 +0000

    added dftbranch to receive to use for fee postings
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/Release-2.41.0@79588 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/customermasters/impl/CustomerDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/customermasters/impl/CustomerDAOImpl.java
@@ -1234,7 +1234,7 @@ public class CustomerDAOImpl extends BasisNextidDaoImpl<Customer> implements Cus
 		Customer customer = new Customer();
 		customer.setId(id);		
 		
-		StringBuilder selectSql = new StringBuilder("SELECT CustCIF, CustID, CustGroupID,CustCtgCode, CustStsChgDate, CustShrtName, CustCRCPR ");
+		StringBuilder selectSql = new StringBuilder("SELECT CustCIF, CustID, CustGroupID,CustCtgCode, CustStsChgDate, CustShrtName, CustCRCPR,CustDftBranch ");
 		selectSql.append(" FROM  Customers");
 		selectSql.append(" Where CustID =:CustID");
 		
