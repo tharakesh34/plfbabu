1440eb3be3d732ce50704fc621788c949902fdb3
durgaprasad.d <durgaprasad.d@pennanttech.com>
2017-11-13 14:06:25 +0000

    Initial import.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@81449 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-niyogin/src/main/java/com/pennanttech/clients/JSONClient.java
@@ -0,0 +1,58 @@
+package com.pennanttech.clients;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
+
+public class JSONClient {
+
+	private static final Logger logger = Logger.getLogger(JSONClient.class);
+
+	public Object postProcess(String url, String service, Object requestData, Class<?> responseClass) throws Exception {
+		String json = "";
+		Response response = getClient(url, service, requestData);
+		json = response.readEntity(String.class);
+		
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationConfig.Feature.SORT_PROPERTIES_ALPHABETICALLY, false);
+		mapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector());
+		mapper.setSerializationInclusion(Inclusion.NON_NULL);
+		Object objResponse = mapper.readValue(json, responseClass);
+
+		return objResponse;
+	}
+
+	private static Response getClient(String url, String path, Object requestData) {
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonInString = null;
+
+		try {
+			jsonInString = mapper.writeValueAsString(requestData);
+		} catch (Exception e) {
+			logger.error("Exception in jason request string" + e);
+		}
+
+		logger.debug("Jason Request String " + jsonInString);
+
+		Client client = ClientBuilder.newClient().register(JacksonJaxbJsonProvider.class);
+		WebTarget target = client.target(url).path(path);
+		Invocation.Builder builder = target.request(MediaType.APPLICATION_JSON_TYPE);
+
+		Response response = builder.post(Entity.entity(requestData, MediaType.APPLICATION_JSON_TYPE)); // Successful
+		logger.info(response.readEntity(String.class));
+		return response;
+	}
+
+}
