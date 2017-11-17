690f83d3800c10c0aa3211b50e7669ab80d322b2
durgaprasad.d <durgaprasad.d@pennanttech.com>
2017-11-17 12:57:01 +0000

    Packages changed
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@81617 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-niyogin/src/main/java/com/pennanttech/niyogin/dedup/model/MatchedField.java
@@ -0,0 +1,9 @@
+package com.pennanttech.niyogin.model.dedup;
+
+import java.io.Serializable;
+
+public class MatchedField implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ pff-niyogin/src/main/java/com/pennanttech/niyogin/dedup/model/Phone.java
@@ -0,0 +1,43 @@
+package com.pennanttech.niyogin.model.dedup;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class Phone implements Serializable {
+	private static final long	serialVersionUID	= -1626565083533225611L;
+
+	@XmlElement(name = "phone_number")
+	private String				phoneNumber;
+	@XmlElement(name = "phone_type")
+	private String				phoneType;
+
+	public Phone() {
+		super();
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPhoneType() {
+		return phoneType;
+	}
+
+	public void setPhoneType(String phoneType) {
+		this.phoneType = phoneType;
+	}
+
+	@Override
+	public String toString() {
+		return "Phone [phoneNumber=" + phoneNumber + ", phoneType=" + phoneType + "]";
+	}
+
+}
