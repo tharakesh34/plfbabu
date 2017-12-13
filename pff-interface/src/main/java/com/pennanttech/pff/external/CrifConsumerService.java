19d61205a9d8a0b338d56ab86bc4fdaa5beb9869
durgaprasad.d <durgaprasad.d@pennanttech.com>
2017-12-13 13:50:32 +0000

    Refactored
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@82704 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface/src/main/java/com/pennanttech/pff/external/CrifConsumerService.java
+++
@@ -1,8 +0,0 @@
-package com.pennanttech.pff.external;
-
-import com.pennant.backend.model.audit.AuditHeader;
-import com.pennanttech.pennapps.core.InterfaceException;
-
-public interface CrifConsumerService {
-	AuditHeader getCrifBureauConsumer(AuditHeader auditHeader) throws InterfaceException;
-}
