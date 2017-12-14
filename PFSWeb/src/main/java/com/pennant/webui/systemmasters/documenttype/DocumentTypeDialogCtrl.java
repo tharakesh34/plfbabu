4fe98a512c2d93f8a857b2882b84c680ed25d64d
satish.k <satish.k@pennanttech.com>
2017-12-14 09:43:34 +0000

    bug fix filed length is 500 , but in screen it is 50 so update to 150
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@82742 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/systemmasters/documenttype/DocumentTypeDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/systemmasters/documenttype/DocumentTypeDialogCtrl.java
@@ -195,7 +195,7 @@ public class DocumentTypeDialogCtrl extends GFCBaseCtrl<DocumentType> {
 
 		// Empty sent any required attributes
 		this.docTypeCode.setMaxlength(50);
-		this.docTypeDesc.setMaxlength(50);
+		this.docTypeDesc.setMaxlength(150);
 
 		if (isWorkFlowEnabled()) {
 			this.groupboxWf.setVisible(true);
