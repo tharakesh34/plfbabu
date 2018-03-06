4c55de0d7a2753a095f50f8162d49d35579179e7
manikanta.p <manikanta.p@pennanttech.com>
2018-03-06 07:48:37 +0000

    Added getProcessView Service for getting the processview Details.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@86033 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-api/src/main/java/com/pennanttech/pffws/WorkFlowRESTService.java
+++ pff-api/src/main/java/com/pennanttech/pffws/WorkFlowRESTService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import com.pennant.backend.model.ProcessViewDetails;
 import com.pennant.backend.model.WorkFlowDetails;
 import com.pennant.ws.exception.ServiceException;
 
@@ -22,10 +23,13 @@ public interface WorkFlowRESTService {
 	@Path("/workFlowService/updateWorkFlow")
 	public WorkFlowDetails updateWorkFlow(@WebParam(name = "workFlow") WorkFlowDetails workFlowDetails)
 			throws ServiceException;
-
+	
 	@GET
 	@Path("/workFlowService/getWorkFlowDetails/{workflowID: .*}")
 	public WorkFlowDetails getWorkFlowDetails(@PathParam("workflowID") String workFlowId) throws ServiceException;
-
+	
+	@GET
+	@Path("/workFlowService/getProcessView/{args: .*}")
+	public ProcessViewDetails getProcessViewDetails(@PathParam("args") String args) throws ServiceException;
 	
 }
--- pff-api/src/main/java/com/pennanttech/service/impl/WorkFlowWebServiceImpl.java
+++ pff-api/src/main/java/com/pennanttech/service/impl/WorkFlowWebServiceImpl.java
@@ -10,14 +10,20 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pennant.app.util.ErrorUtil;
 import com.pennant.app.util.SessionUserDetails;
+import com.pennant.backend.model.ProcessViewDetails;
 import com.pennant.backend.model.WSReturnStatus;
 import com.pennant.backend.model.WorkFlowDetails;
 import com.pennant.backend.model.audit.AuditDetail;
 import com.pennant.backend.model.audit.AuditHeader;
 import com.pennant.backend.service.WorkFlowDetailsService;
+import com.pennant.backend.util.WorkFlowUtil;
 import com.pennant.ws.exception.ServiceException;
+import com.pennanttech.activity.log.Activity;
+import com.pennanttech.activity.log.ActivityLogService;
 import com.pennanttech.pennapps.core.engine.workflow.WorkflowEngine;
+import com.pennanttech.pennapps.core.engine.workflow.WorkflowEngine.Flow;
 import com.pennanttech.pennapps.core.model.ErrorDetail;
 import com.pennanttech.pennapps.core.model.LoggedInUser;
 import com.pennanttech.pennapps.core.resource.Literal;
@@ -34,6 +40,8 @@ public class WorkFlowWebServiceImpl implements WorkFlowRESTService,WorkFlowSOAPS
 	private  final String Update = "update";
 	private  final String Get = "get";
 
+	private ActivityLogService activityLogService;
+
 	@Override
 	public WorkFlowDetails createWorkFlow(WorkFlowDetails workFlowDetails) throws ServiceException {
 		logger.debug("Entering");
@@ -45,7 +53,7 @@ public class WorkFlowWebServiceImpl implements WorkFlowRESTService,WorkFlowSOAPS
 		workFlowDetails.setWorkflowId(0);
 		workFlowDetails.setLastMntBy(userDetails.getUserId());
 		workFlowDetails.setLastMntOn(new Timestamp(System.currentTimeMillis()));
-			List<ErrorDetail> errorDetails = workFlowDetailsService.doValidations(workFlowDetails, Create); // validating object
+		List<ErrorDetail> errorDetails = workFlowDetailsService.doValidations(workFlowDetails, Create, true); // validating object
 			for(ErrorDetail errDetail : errorDetails) {// returning in case of exception
 				WSReturnStatus status = new WSReturnStatus();
 				status.setReturnCode(errDetail.getCode());
@@ -86,7 +94,7 @@ public class WorkFlowWebServiceImpl implements WorkFlowRESTService,WorkFlowSOAPS
 			workFlowDetails.setLastMntBy(userDetails.getUserId());
 			workFlowDetails.setLastMntOn(new Timestamp(System.currentTimeMillis()));
 			workFlowDetails.setWorkFlowId(workFlowDetails.getWorkFlowDesignId());
-			List<ErrorDetail> errorDetails = workFlowDetailsService.doValidations(workFlowDetails, Update);
+			List<ErrorDetail> errorDetails = workFlowDetailsService.doValidations(workFlowDetails, Update, true);
 
 			for(ErrorDetail errDetail : errorDetails) {
 				WSReturnStatus status = new WSReturnStatus();
@@ -125,7 +133,7 @@ public class WorkFlowWebServiceImpl implements WorkFlowRESTService,WorkFlowSOAPS
 		WorkFlowDetails workFlowDetails = new WorkFlowDetails();
 			if(StringUtils.isNotBlank(workFlowId))
 			workFlowDetails.setWorkflowId(Long.valueOf(workFlowId));
-			List<ErrorDetail> errorDetails = workFlowDetailsService.doValidations(workFlowDetails, Get);
+		List<ErrorDetail> errorDetails = workFlowDetailsService.doValidations(workFlowDetails, Get, false);
 			for(ErrorDetail errDetail : errorDetails) {
 				WSReturnStatus status = new WSReturnStatus();
 				status.setReturnCode(errDetail.getCode());
@@ -170,4 +178,249 @@ public class WorkFlowWebServiceImpl implements WorkFlowRESTService,WorkFlowSOAPS
 		}
 		return firstTaskOwnersAndActors;
 	}
+
+	@Override
+	public ProcessViewDetails getProcessViewDetails(String args) {
+		logger.debug(Literal.ENTERING);
+
+		String[] valueParm = new String[1];
+		ErrorDetail errorDetail;
+		WSReturnStatus status = new WSReturnStatus();
+		ProcessViewDetails processViewDetails = new ProcessViewDetails();
+
+		// Get the parameters from the comma delimited arguments.
+		if (StringUtils.isEmpty(args)) {
+			valueParm[0] = "args parameter";
+			errorDetail = ErrorUtil.getErrorDetail(new ErrorDetail("WFEE08", valueParm), "EN");
+			status.setReturnCode(errorDetail.getCode());
+			status.setReturnText(errorDetail.getError());
+			processViewDetails.setReturnStatus(status);
+			return processViewDetails;
+		}
+
+		String params[] = args.split(",");
+
+		if (params.length != 4) {
+			valueParm[0] = "args";
+			errorDetail = ErrorUtil.getErrorDetail(new ErrorDetail("WFEE09", valueParm), "EN");
+			status.setReturnCode(errorDetail.getCode());
+			status.setReturnText(errorDetail.getError());
+			processViewDetails.setReturnStatus(status);
+			return processViewDetails;
+		}
+
+		// Get the activities for the event.
+		List<Activity> activities = new ArrayList<>();
+
+		try {
+			activities = activityLogService.getActivities(params[0], params[1], Long.valueOf(params[2]),
+					Long.valueOf(params[3]));
+		} catch (Exception e) {
+			logger.error(e);
+			valueParm[0] = "args";
+			errorDetail = ErrorUtil.getErrorDetail(new ErrorDetail("WFEE10", valueParm), "EN");
+			status.setReturnCode(errorDetail.getCode());
+			status.setReturnText(errorDetail.getError());
+			processViewDetails.setReturnStatus(status);
+			return processViewDetails;
+		}
+
+		if (activities.isEmpty()) {
+			valueParm[0] = "activities";
+			errorDetail = ErrorUtil.getErrorDetail(new ErrorDetail("WFEE08", valueParm), "EN");
+			status.setReturnCode(errorDetail.getCode());
+			status.setReturnText(errorDetail.getError());
+			processViewDetails.setReturnStatus(status);
+			return processViewDetails;
+		}
+
+		// Prepare the result.
+		processViewDetails.setRoles(getRoles(activities));
+		processViewDetails.setRolesWithCount(getRolesWithCount(activities));
+		processViewDetails.setRecordStatuses(getRecordStatuses(activities));
+		processViewDetails.setVisitedRoles(getVisitedRoles(activities));
+		processViewDetails.setReturnStatus(APIErrorHandlerService.getSuccessStatus());
+
+		logger.debug(Literal.LEAVING);
+		return processViewDetails;
+	}
+
+	/**
+	 * getRoleCodes method gives the ordered RoleCodes through activities and do the commenting steps in the
+	 * getRoleCodes method<br>
+	 * 
+	 * @param activities
+	 *            (List<Activity>)
+	 * 
+	 * @return roles
+	 */
+	private List<String> getRoles(List<Activity> activities) {
+		logger.debug(Literal.ENTERING);
+
+		List<String> roles = new ArrayList<>();
+
+		// Get the process flow details.
+		long workflowId = activities.get(0).getWorkflowId();
+		WorkFlowDetails workflow = WorkFlowUtil.getWorkflow(workflowId);
+
+		if (workflow == null) {
+			return roles;
+		}
+
+		// Add the roles of visited stages.
+		for (Activity activity : activities) {
+			if (!roles.contains(activity.getRoleCode())) {
+				roles.add(activity.getRoleCode());
+			}
+		}
+
+		// Get the next role code of last stage.
+		String nextRoleCode = activities.get(activities.size() - 1).getNextRoleCode();
+
+		if (StringUtils.isEmpty(nextRoleCode)) {
+			roles.add("completed");
+		} else {
+			// Check whether the next role code is a predecessor.
+			String[] nextRoleCodes = nextRoleCode.split(",");
+			WorkflowEngine engine = new WorkflowEngine(workflow.getWorkFlowXml());
+			String nextTask;
+			String task;
+			List<String> nextRoles = new ArrayList<>();
+
+			for (String nextRole : nextRoleCodes) {
+				nextTask = engine.getUserTaskId(nextRole);
+
+				for (String role : roles) {
+					task = engine.getUserTaskId(role);
+
+					if (engine.compareTo(task, nextTask) == Flow.PREDECESSOR) {
+						nextRoles.add(role);
+					}
+				}
+			}
+
+			// Add the roles that can be visited.
+			if (!nextRoles.isEmpty()) {
+				roles.addAll(nextRoles);
+			}
+
+			// Add the next role code.
+			roles.add(nextRoleCode.replace(',', ';'));
+		}
+
+
+		logger.debug(Literal.LEAVING);
+		return roles;
+	}
+
+	/**
+	 * To get the Roles with count i.e the number of times the Respective Record has met the same Role.
+	 * 
+	 * @param activities
+	 *            (List<Activity>)
+	 * 
+	 * @return rolesWithCount.
+	 */
+	private String getRolesWithCount(List<Activity> activities) {
+		logger.debug(Literal.ENTERING);
+
+		StringBuilder rolesWithCount = new StringBuilder();
+		List<String> uniqueRoles = getUniqueRoles(activities);
+
+		for (int i = 0; i < uniqueRoles.size(); i++) {
+			int roleCount = 0;
+			String role = uniqueRoles.get(i);
+
+			if (i == 0) {
+				roleCount = 1;
+			}
+
+			for (Activity activity : activities) {
+				if (StringUtils.isNotEmpty(activity.getNextRoleCode())) {
+					String nextRoleCodes[] = activity.getNextRoleCode().split(",");
+					for (String nextRoleCode : nextRoleCodes) {
+						if (role.equals(nextRoleCode) && !activity.getRecordStatus().equals("Saved"))
+							++roleCount;
+					}
+				}
+			}
+
+			if (i == 0) {
+				rolesWithCount = rolesWithCount.append(role + "," + roleCount);
+			} else {
+				rolesWithCount = rolesWithCount.append("," + role + "," + roleCount);
+			}
+		}
+
+		logger.debug(Literal.LEAVING);
+		return rolesWithCount.toString();
+	}
+
+	// returns the Record Statuses
+	private List<String> getRecordStatuses(List<Activity> activities) {
+		logger.debug(Literal.ENTERING);
+
+		List<String> recordStatuses = new ArrayList<>();
+
+		for (Activity activity : activities) {
+			if (!activity.getRecordStatus().equals("Saved")) {
+				recordStatuses.add(activity.getRecordStatus());
+			}
+		}
+
+		logger.debug(Literal.LEAVING);
+		return recordStatuses;
+	}
+
+	// returns the Visited Role Codes
+	private List<String> getVisitedRoles(List<Activity> activities) {
+		logger.debug(Literal.ENTERING);
+
+		List<String> visitedRoles = new ArrayList<>();
+
+		for (Activity activity : activities) {
+			if (!activity.getRecordStatus().equals("Saved") && !visitedRoles.contains(activity.getRoleCode())) {
+				visitedRoles.add(activity.getRoleCode());
+			}
+		}
+
+		logger.debug(Literal.LEAVING);
+		return visitedRoles;
+	}
+
+	/**
+	 * getUniqueNextRoleCodes method is to filter the unique nextRoleCodes from the activities
+	 * 
+	 * @param activities
+	 *            (List<Activity>)
+	 * 
+	 * @return uniqueRoles.
+	 */
+	private List<String> getUniqueRoles(List<Activity> activities) {
+		logger.debug(Literal.ENTERING);
+
+		List<String> uniqueRoles = new ArrayList<String>();
+		uniqueRoles.add(activities.get(0).getRoleCode());
+
+		for (Activity activity : activities) {
+			if (StringUtils.isNotEmpty(activity.getNextRoleCode())) {
+				String tempNextRoleCodes[] = activity.getNextRoleCode().split(",");
+				for (String tempNextRoleCode : tempNextRoleCodes) {
+					if (!(uniqueRoles.contains(tempNextRoleCode))) {
+						uniqueRoles.add(tempNextRoleCode);
+					}
+				}
+			}
+
+		}
+
+		logger.debug(Literal.LEAVING);
+		return uniqueRoles;
+	}
+
+	@Autowired
+	public void setActivityLogService(ActivityLogService activityLogService) {
+		this.activityLogService = activityLogService;
+	}
+
 }
