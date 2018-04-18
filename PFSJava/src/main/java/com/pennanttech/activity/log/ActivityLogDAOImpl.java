458fd6fbe941a7c040156841049e2872a96ad0a7
vinay.b <vinay.b@pennanttech.com>
2018-04-18 15:38:40 +0000

    Activitylog Changes for Profectus
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@88079 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennanttech/activity/log/ActivityLogDAOImpl.java
+++ PFSJava/src/main/java/com/pennanttech/activity/log/ActivityLogDAOImpl.java
@@ -24,18 +24,11 @@ public class ActivityLogDAOImpl implements ActivityLogDAO {
 	public List<Activity> getActivities(String tableName, String keyColumn, Object keyValue) {
 		logger.debug(Literal.ENTERING);
 
-		StringBuilder sql = new StringBuilder("select A.AuditId, A.LastMntOn AuditDate, A.Version, A.RoleCode,");
-		sql.append(" A.NextRoleCode, A.RecordStatus, A.TaskId, A.RecordType, A.WorkflowId,A.lastMntBy,");
-
-		if (tableName.equals("FinanceMain")) {
-			sql.append("A.RcdMaintainSts,");
-		}
-
-		sql.append("U.UsrLogin UserLogin ");
-		sql.append("from Adt").append(tableName).append(" A ");
-		sql.append("inner join SecUsers U on U.UsrID = A.LastMntBy ");
-		sql.append("where A.").append(keyColumn).append(" = :keyValue ");
-		sql.append("and AuditImage = 'W' order by auditid");
+		StringBuilder sql = new StringBuilder("select AuditId, AuditDate, Version, RoleCode,");
+		sql.append(" NextRoleCode, RecordStatus, TaskId, RecordType, WorkflowId, lastMntBy,");
+		sql.append("UserLogin ");
+		sql.append("from activitylog_view ");
+		sql.append("where ").append(keyColumn).append(" = :keyValue ");
 		logger.debug("SQL: " + sql.toString());
 
 		MapSqlParameterSource source = new MapSqlParameterSource();
