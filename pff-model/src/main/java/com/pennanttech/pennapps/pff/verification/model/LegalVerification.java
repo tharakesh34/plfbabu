ded1966c1aca28f6224e3676a6d0d1db3648fe03
lakshmi.n <lakshmi.n@pennanttech.com>
2018-05-01 06:08:34 +0000

    Verification module changes committed.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@88583 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-model/src/main/java/com/pennanttech/pennapps/pff/verification/model/LegalVerification.java
@@ -0,0 +1,261 @@
+package com.pennanttech.pennapps.pff.verification.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+import com.pennant.backend.model.audit.AuditDetail;
+import com.pennant.backend.model.documentdetails.DocumentDetails;
+import com.pennant.backend.model.extendedfield.ExtendedFieldHeader;
+import com.pennant.backend.model.extendedfield.ExtendedFieldRender;
+import com.pennanttech.pennapps.core.model.AbstractWorkflowEntity;
+import com.pennanttech.pennapps.core.model.LoggedInUser;
+
+public class LegalVerification extends AbstractWorkflowEntity {
+	private static final long serialVersionUID = 1L;
+
+	private String cif;
+	private String keyReference;
+	private String collateralType;
+	private String collateralReference;
+
+	private Date date;
+	private String agentCode;
+	private String agentName;
+	private int status;
+	private Long reason;
+	private String summaryRemarks;
+
+	private long verificationId;
+	private String reasonCode;
+	private String reasonDesc;
+	private Long custId;
+
+	private List<DocumentDetails> documents = null;
+	@XmlTransient
+	private boolean newRecord = false;
+	@XmlTransient
+	private String lovValue;
+	@XmlTransient
+	private LegalVerification befImage;
+	@XmlTransient
+	private LoggedInUser userDetails;
+	
+	private ExtendedFieldHeader extendedFieldHeader;
+	private ExtendedFieldRender extendedFieldRender;
+	private HashMap<String, List<AuditDetail>> auditDetailMap = new HashMap<String, List<AuditDetail>>();
+
+	public LegalVerification() {
+		super();
+	}
+
+	public LegalVerification(long id) {
+		super();
+		this.setId(id);
+	}
+
+	public Set<String> getExcludeFields() {
+		Set<String> excludeFields = new HashSet<>();
+		excludeFields.add("reasonCode");
+		excludeFields.add("reasonDesc");
+		return excludeFields;
+	}
+
+	public long getId() {
+		return verificationId;
+	}
+
+	public void setId(long id) {
+		this.verificationId = id;
+	}
+
+	public boolean isNew() {
+		return isNewRecord();
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getAgentCode() {
+		return agentCode;
+	}
+
+	public void setAgentCode(String agentCode) {
+		this.agentCode = agentCode;
+	}
+
+	public String getAgentName() {
+		return agentName;
+	}
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Long getReason() {
+		return reason;
+	}
+
+	public void setReason(Long reason) {
+		this.reason = reason;
+	}
+
+	public String getSummaryRemarks() {
+		return summaryRemarks;
+	}
+
+	public void setSummaryRemarks(String summaryRemarks) {
+		this.summaryRemarks = summaryRemarks;
+	}
+
+	public String getCif() {
+		return cif;
+	}
+
+	public void setCif(String cif) {
+		this.cif = cif;
+	}
+
+	public String getKeyReference() {
+		return keyReference;
+	}
+
+	public void setKeyReference(String keyReference) {
+		this.keyReference = keyReference;
+	}
+
+	public long getVerificationId() {
+		return verificationId;
+	}
+
+	public void setVerificationId(long verificationId) {
+		this.verificationId = verificationId;
+	}
+
+	public String getCollateralType() {
+		return collateralType;
+	}
+
+	public void setCollateralType(String collateralType) {
+		this.collateralType = collateralType;
+	}
+
+	public String getCollateralReference() {
+		return collateralReference;
+	}
+
+	public void setCollateralReference(String collateralReference) {
+		this.collateralReference = collateralReference;
+	}
+
+	public String getReasonCode() {
+		return reasonCode;
+	}
+
+	public void setReasonCode(String reasonCode) {
+		this.reasonCode = reasonCode;
+	}
+
+	public String getReasonDesc() {
+		return reasonDesc;
+	}
+
+	public void setReasonDesc(String reasonDesc) {
+		this.reasonDesc = reasonDesc;
+	}
+
+	public Long getCustId() {
+		return custId;
+	}
+
+	public void setCustId(Long custId) {
+		this.custId = custId;
+	}
+
+	public boolean isNewRecord() {
+		return newRecord;
+	}
+
+	public void setNewRecord(boolean newRecord) {
+		this.newRecord = newRecord;
+	}
+
+	public String getLovValue() {
+		return lovValue;
+	}
+
+	public void setLovValue(String lovValue) {
+		this.lovValue = lovValue;
+	}
+
+	public LegalVerification getBefImage() {
+		return befImage;
+	}
+
+	public void setBefImage(LegalVerification befImage) {
+		this.befImage = befImage;
+	}
+
+	public LoggedInUser getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(LoggedInUser userDetails) {
+		this.userDetails = userDetails;
+	}
+
+	public Timestamp getPrevMntOn() {
+		return befImage == null ? null : befImage.getLastMntOn();
+	}
+
+	public List<DocumentDetails> getDocuments() {
+		return documents;
+	}
+
+	public void setDocuments(List<DocumentDetails> documents) {
+		this.documents = documents;
+	}
+	
+	public ExtendedFieldHeader getExtendedFieldHeader() {
+		return extendedFieldHeader;
+	}
+
+	public void setExtendedFieldHeader(ExtendedFieldHeader extendedFieldHeader) {
+		this.extendedFieldHeader = extendedFieldHeader;
+	}
+
+	public ExtendedFieldRender getExtendedFieldRender() {
+		return extendedFieldRender;
+	}
+
+	public void setExtendedFieldRender(ExtendedFieldRender extendedFieldRender) {
+		this.extendedFieldRender = extendedFieldRender;
+	}
+
+	public HashMap<String, List<AuditDetail>> getAuditDetailMap() {
+		return auditDetailMap;
+	}
+
+	public void setAuditDetailMap(HashMap<String, List<AuditDetail>> auditDetailMap) {
+		this.auditDetailMap = auditDetailMap;
+	}
+
+}
