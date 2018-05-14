5f65337be5ff8fbaca1db2b4d8bc9674f233f947
pasvarma <pasvarma@pennanttech.com>
2018-05-14 16:01:56 +0000

    Extended Fields Amount Formating in rules.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@89302 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/finance/financemain/FinanceMainBaseCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/finance/financemain/FinanceMainBaseCtrl.java
@@ -14698,6 +14698,10 @@ public class FinanceMainBaseCtrl extends GFCBaseCtrl<FinanceMain> {
 			for (ExtendedFieldDetail fieldDetail : detail.getExtendedFieldHeader().getExtendedFieldDetails()) {
 				if(fieldDetail.isAllowInRule()){
 					Object value = detail.getExtendedFieldRender().getMapValues().get(fieldDetail.getFieldName());
+					if(StringUtils.equals("CURRENCY",fieldDetail.getFieldType())){
+						value  = PennantAppUtil.formateAmount((BigDecimal) value,CurrencyUtil.getFormat(detail.getCustomerDetails().getCustomer().getCustBaseCcy()));
+					}
+					
 					detail.getCustomerEligibilityCheck().addExtendedField(fieldDetail.getLovDescModuleName()+"_"+fieldDetail.getLovDescSubModuleName()+"_"+fieldDetail.getFieldName(), value);
 				}
 			}	
