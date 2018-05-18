e1d1c578e556674ce407a7f26d60a185d43c5439
durgaprasad.g <durgaprasad.g@pennanttech.com>
2018-05-18 03:21:52 +0000

    Added PDC Type
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@89474 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-core/src/main/java/com/pennant/backend/util/MandateConstants.java
+++ pff-core/src/main/java/com/pennant/backend/util/MandateConstants.java
@@ -48,6 +48,7 @@ public class MandateConstants {
 	public static final String	TYPE_ECS			= "ECS";
 	public static final String	TYPE_DDM			= "DDM";
 	public static final String	TYPE_NACH			= "NACH";
+	public static final String	TYPE_PDC			= "PDC";
 
 	public static final String	MANDATE_AC_TYPE_CA	= "11";
 	public static final String	MANDATE_AC_TYPE_SA	= "10";
