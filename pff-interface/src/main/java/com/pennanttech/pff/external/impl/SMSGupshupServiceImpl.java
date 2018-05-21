effc45423fe8c70d8cee60f6c931d769b7927cb0
pasvarma <pasvarma@pennanttech.com>
2018-05-21 03:57:53 +0000

    Added logging
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@89617 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface/src/main/java/com/pennanttech/pff/external/impl/SMSGupshupServiceImpl.java
+++ pff-interface/src/main/java/com/pennanttech/pff/external/impl/SMSGupshupServiceImpl.java
@@ -5,22 +5,28 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.sql.Timestamp;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.pennant.backend.model.mail.MailTemplate;
+import com.pennanttech.logging.model.InterfaceLogDetail;
 import com.pennanttech.pennapps.core.InterfaceException;
 import com.pennanttech.pennapps.core.resource.Literal;
+import com.pennanttech.pff.InterfaceConstants;
 import com.pennanttech.pff.external.SMSService;
+import com.pennanttech.pff.logging.dao.InterfaceLoggingDAO;
 
 public class SMSGupshupServiceImpl implements SMSService {
 	private static final Logger logger = Logger.getLogger(SMSGupshupServiceImpl.class);
+
 	private String 				userID;
 	private String 				password;
 	private String				serviceUrl=null;
 	private final String 		encoder="UTF-8";
-	
+	private InterfaceLoggingDAO interfaceLoggingDAO;
 	
 	@Override
 	public void sendSms(List<MailTemplate> smsList, String referene) throws InterfaceException {
@@ -36,7 +42,6 @@ public class SMSGupshupServiceImpl implements SMSService {
 				}
 			}
 		}
-
 		logger.debug(Literal.LEAVING);
 	}
 
@@ -48,10 +53,11 @@ public class SMSGupshupServiceImpl implements SMSService {
 	 * @param referene
 	 * 
 	 */
-	private void send(String mobileNo, String messageContent, String referene) {
+	private void send(String mobileNo, String messageContent, String reference) {
 		logger.debug(Literal.ENTERING);
 		StringBuffer smsData= new StringBuffer();
-		
+		Timestamp reqSentOn = new Timestamp(System.currentTimeMillis());
+
 		try {
 
 			smsData.append("method=sendMessage");
@@ -82,13 +88,20 @@ public class SMSGupshupServiceImpl implements SMSService {
 			while ((line = rd.readLine()) != null) {
 				buffer.append(line).append("\n");
 			}
-			// FIXME Response logging
+			String[] response = StringUtils.split(buffer.toString(),"|");
+			
+			if("error".contains(buffer.toString())){
+				doInterfaceLogging(reference, smsData.toString(), buffer.toString(), "999", response[2], reqSentOn,	InterfaceConstants.STATUS_FAILED);
+			}else{
+				doInterfaceLogging(reference, smsData.toString(), buffer.toString(), null, null, reqSentOn,InterfaceConstants.STATUS_SUCCESS);				
+			}
+			
 			rd.close();
 			conn.disconnect();
 
 		} catch (Exception e) {
-
-			// FIXME Exception logging
+			logger.debug(Literal.EXCEPTION, e);
+			doInterfaceLogging(reference, smsData.toString(), null, null, e.getMessage(), reqSentOn,	InterfaceConstants.STATUS_FAILED);
 			// FIXME Exception Handling
 		}
 
@@ -96,7 +109,43 @@ public class SMSGupshupServiceImpl implements SMSService {
 		logger.debug(Literal.LEAVING);
 	}
 
-	
+	/**
+	 * Method for prepare Success logging
+	 * 
+	 * @param reference
+	 * @param requets
+	 * @param response
+	 * @param errorCode
+	 * @param errorDesc
+	 * @param reqSentOn
+	 * @param status
+	 */
+	private void doInterfaceLogging(String reference, String requets, String response, String errorCode,
+			String errorDesc, Timestamp reqSentOn, String status) {
+		logger.debug(Literal.ENTERING);
+		InterfaceLogDetail iLogDetail = new InterfaceLogDetail();
+		iLogDetail.setReference(reference);
+		iLogDetail.setServiceName("CIBIL");
+		iLogDetail.setEndPoint(getServiceUrl());
+		iLogDetail.setRequest(requets);
+		iLogDetail.setReqSentOn(reqSentOn);
+
+		iLogDetail.setResponse(response);
+		iLogDetail.setRespReceivedOn(new Timestamp(System.currentTimeMillis()));
+		iLogDetail.setStatus(status);
+		iLogDetail.setErrorCode(errorCode);
+		if (errorDesc != null && errorDesc.length() > 200) {
+			iLogDetail.setErrorDesc(errorDesc.substring(0, 190));
+		}
+
+		try {
+			interfaceLoggingDAO.save(iLogDetail);
+		} catch (Exception e) {
+			logger.error("Exception", e);
+		}
+		
+		logger.debug(Literal.LEAVING);
+	}
 	
 	public String getUserID() {
 		return userID;
@@ -120,6 +169,14 @@ public class SMSGupshupServiceImpl implements SMSService {
 
 	public void setServiceUrl(String serviceUrl) {
 		this.serviceUrl = serviceUrl;
+	}
+
+	public InterfaceLoggingDAO getInterfaceLoggingDAO() {
+		return interfaceLoggingDAO;
+	}
+
+	public void setInterfaceLoggingDAO(InterfaceLoggingDAO interfaceLoggingDAO) {
+		this.interfaceLoggingDAO = interfaceLoggingDAO;
 	}
 
 }
