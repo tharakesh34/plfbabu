fac114ef7d1e75e905424262bb413200b978ecf6
Sai Krishna P <saikrishna.p@pennanttech.com>
2018-05-22 14:04:29 +0000

    Bug fix: PSD 126612 LMS > PDE > newly added shareholder are not displayed in PDE.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@89734 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/service/customermasters/impl/CustomerDetailsServiceImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/service/customermasters/impl/CustomerDetailsServiceImpl.java
@@ -1,3 +1,45 @@
+/**
+ * Copyright 2011 - Pennant Technologies
+ * 
+ * This file is part of Pennant Java Application Framework and related Products. 
+ * All components/modules/functions/classes/logic in this software, unless 
+ * otherwise stated, the property of Pennant Technologies. 
+ * 
+ * Copyright and other intellectual property laws protect these materials. 
+ * Reproduction or retransmission of the materials, in whole or in part, in any manner, 
+ * without the prior written consent of the copyright holder, is a violation of 
+ * copyright law.
+ */
+
+/**
+ ********************************************************************************************
+ *                                 FILE HEADER                                              *
+ ********************************************************************************************
+ *																							*
+ * FileName    		:  CustomerDetailsServiceImpl.java                                      * 	  
+ *                                                                    						*
+ * Author      		:  PENNANT TECHONOLOGIES              									*
+ *                                                                  						*
+ * Creation Date    :  05-05-2011    														*
+ *                                                                  						*
+ * Modified Date    :  05-05-2011    														*
+ *                                                                  						*
+ * Description 		:                                             							*
+ *                                                                                          *
+ ********************************************************************************************
+ * Date             Author                   Version      Comments                          *
+ ********************************************************************************************
+ * 05-05-2011       Pennant	                 0.1                                            * 
+ *                                                                                          * 
+ * 22-05-2018       Sai Krishna              0.2          1. PSD - Ticket 126612 LMS > PDE >* 
+ *                                                           newly added shareholder are not* 
+ *                                                           displayed in PDE.              * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ ********************************************************************************************
+ */
 package com.pennant.backend.service.customermasters.impl;
 
 import java.util.ArrayList;
@@ -626,7 +668,8 @@ public class CustomerDetailsServiceImpl extends GenericService<Customer> impleme
 				customerDetails
 						.setCustomerIncomeList(getCustomerIncomeDAO().getCustomerIncomeByCustomer(id, false, type));
 			}
-			if (StringUtils.isNotEmpty(customerDetails.getCustomer().getCustCtgCode()) && StringUtils
+			// ### Ticket 126612 LMS > PDE > newly added shareholder are not displayed in PDE. Changed the condition to non individual.
+			if (StringUtils.isNotEmpty(customerDetails.getCustomer().getCustCtgCode()) && !StringUtils
 					.equals(customerDetails.getCustomer().getCustCtgCode(), PennantConstants.PFF_CUSTCTG_INDIV)) {
 				if (ImplementationConstants.ALLOW_CUSTOMER_SHAREHOLDERS) {
 					customerDetails
