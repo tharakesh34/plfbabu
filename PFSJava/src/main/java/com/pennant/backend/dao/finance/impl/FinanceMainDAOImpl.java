466c5f29e45304863a9df1c620ac56c70585cb59
madhubabu.p <madhubabu.p@pennanttech.com>
2018-05-24 08:11:04 +0000

    Bpi treatment added for Eod Execution
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@89826 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/finance/impl/FinanceMainDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/finance/impl/FinanceMainDAOImpl.java
@@ -2747,7 +2747,7 @@ public class FinanceMainDAOImpl extends BasisCodeDAO<FinanceMain> implements Fin
 		selectSql.append(" PastduePftCalMthd, DroppingMethod, RateChgAnyDay, PastduePftMargin, FinRepayMethod, ");
 		selectSql.append(" MigratedFinance, ScheduleMaintained, ScheduleRegenerated, MandateID, ");
 		selectSql.append(" FinStatus, DueBucket, FinStsReason, BankName, Iban, AccountType, DdaReferenceNo, PromotionCode, ");
-		selectSql.append(" FinCategory, ProductCategory, ReAgeBucket,TDSApplicable  ");
+		selectSql.append(" FinCategory, ProductCategory, ReAgeBucket,TDSApplicable,BpiTreatment");
 		selectSql.append(" FROM FinanceMain Where CustID=:CustID ");
 		
 		if (isActive) {
