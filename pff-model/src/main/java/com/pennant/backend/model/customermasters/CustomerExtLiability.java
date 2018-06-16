a6c4a33d8adc5efbc49821b48304d371fd060eec
murthy.y <murthy.y@pennanttech.com>
2018-06-16 09:04:04 +0000

    format the class
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@91111 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-model/src/main/java/com/pennant/backend/model/customermasters/CustomerExtLiability.java
+++ pff-model/src/main/java/com/pennant/backend/model/customermasters/CustomerExtLiability.java
@@ -18,12 +18,12 @@ import com.pennanttech.pennapps.core.model.LoggedInUser;
 @XmlAccessorType(XmlAccessType.NONE)
 public class CustomerExtLiability extends AbstractWorkflowEntity {
 	private static final long serialVersionUID = 1L;
-	
+
 	private long custID = Long.MIN_VALUE;
 	@XmlElement
 	private int liabilitySeq;
 	@XmlElement
-	private Date   finDate;
+	private Date finDate;
 	@XmlElement
 	private String finType;
 	@XmlElement
@@ -31,16 +31,16 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	private String lovDescBankName;
 	private String lovDescFinType;
 	@XmlElement
-	private BigDecimal 	originalAmount = BigDecimal.ZERO;
+	private BigDecimal originalAmount = BigDecimal.ZERO;
 	@XmlElement
-	private BigDecimal 	instalmentAmount = BigDecimal.ZERO;
+	private BigDecimal instalmentAmount = BigDecimal.ZERO;
 	@XmlElement
-	private BigDecimal 	outStandingBal = BigDecimal.ZERO;
+	private BigDecimal outStandingBal = BigDecimal.ZERO;
 	@XmlElement
 	private String finStatus;
 	private String lovDescFinStatus;
 
-	private boolean newRecord=false;
+	private boolean newRecord = false;
 	private String lovValue;
 	private CustomerExtLiability befImage;
 	private LoggedInUser userDetails;
@@ -48,8 +48,8 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	private String lovDescCustCIF;
 	private String lovDescCustShrtName;
 	private String sourceId;
-	
-	//As per Profectus documnet below fields added
+
+	// As per Profectus documnet below fields added
 	private BigDecimal roi = BigDecimal.ZERO;
 	private int tenure = 0;
 	private int tenureBal = 0;
@@ -64,7 +64,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	private String repayFrom;
 	private String loanpurposedesc;
 	private String lovdescrepayfrom;
-	
+
 	public boolean isNew() {
 		return isNewRecord();
 	}
@@ -78,29 +78,30 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 		this.setId(id);
 	}
 
-	public Set<String> getExcludeFields(){
-		Set<String> excludeFields=new HashSet<String>();
+	public Set<String> getExcludeFields() {
+		Set<String> excludeFields = new HashSet<String>();
 		excludeFields.add("sourceId");
 		excludeFields.add("lovdescrepayfrom");
 		excludeFields.add("loanpurposedesc");
 		return excludeFields;
 	}
-	
+
 	// ******************************************************//
 	// ****************** getter / setter *******************//
-	// ******************************************************//	
-	
+	// ******************************************************//
+
 	public long getId() {
 		return custID;
 	}
-	public void setId (long id) {
+
+	public void setId(long id) {
 		this.custID = id;
 	}
-	
-	
+
 	public long getCustID() {
 		return custID;
 	}
+
 	public void setCustID(long custID) {
 		this.custID = custID;
 	}
@@ -108,6 +109,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public int getLiabilitySeq() {
 		return liabilitySeq;
 	}
+
 	public void setLiabilitySeq(int liabilitySeq) {
 		this.liabilitySeq = liabilitySeq;
 	}
@@ -115,6 +117,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public Date getFinDate() {
 		return finDate;
 	}
+
 	public void setFinDate(Date finDate) {
 		this.finDate = finDate;
 	}
@@ -122,6 +125,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getFinType() {
 		return finType;
 	}
+
 	public void setFinType(String finType) {
 		this.finType = finType;
 	}
@@ -129,6 +133,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getBankName() {
 		return bankName;
 	}
+
 	public void setBankName(String bankName) {
 		this.bankName = bankName;
 	}
@@ -136,6 +141,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getLovDescBankName() {
 		return lovDescBankName;
 	}
+
 	public void setLovDescBankName(String lovDescBankName) {
 		this.lovDescBankName = lovDescBankName;
 	}
@@ -143,6 +149,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getLovDescFinType() {
 		return lovDescFinType;
 	}
+
 	public void setLovDescFinType(String lovDescFinType) {
 		this.lovDescFinType = lovDescFinType;
 	}
@@ -150,6 +157,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public BigDecimal getOriginalAmount() {
 		return originalAmount;
 	}
+
 	public void setOriginalAmount(BigDecimal originalAmount) {
 		this.originalAmount = originalAmount;
 	}
@@ -157,6 +165,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public BigDecimal getInstalmentAmount() {
 		return instalmentAmount;
 	}
+
 	public void setInstalmentAmount(BigDecimal instalmentAmount) {
 		this.instalmentAmount = instalmentAmount;
 	}
@@ -164,6 +173,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public BigDecimal getOutStandingBal() {
 		return outStandingBal;
 	}
+
 	public void setOutStandingBal(BigDecimal outStandingBal) {
 		this.outStandingBal = outStandingBal;
 	}
@@ -171,6 +181,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getFinStatus() {
 		return finStatus;
 	}
+
 	public void setFinStatus(String finStatus) {
 		this.finStatus = finStatus;
 	}
@@ -178,6 +189,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getLovDescFinStatus() {
 		return lovDescFinStatus;
 	}
+
 	public void setLovDescFinStatus(String lovDescFinStatus) {
 		this.lovDescFinStatus = lovDescFinStatus;
 	}
@@ -185,27 +197,31 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public boolean isNewRecord() {
 		return newRecord;
 	}
+
 	public void setNewRecord(boolean newRecord) {
 		this.newRecord = newRecord;
 	}
-	
+
 	public String getLovValue() {
 		return lovValue;
 	}
+
 	public void setLovValue(String lovValue) {
 		this.lovValue = lovValue;
 	}
 
-	public CustomerExtLiability getBefImage(){
+	public CustomerExtLiability getBefImage() {
 		return this.befImage;
 	}
-	public void setBefImage(CustomerExtLiability beforeImage){
-		this.befImage=beforeImage;
+
+	public void setBefImage(CustomerExtLiability beforeImage) {
+		this.befImage = beforeImage;
 	}
 
 	public LoggedInUser getUserDetails() {
 		return userDetails;
 	}
+
 	public void setUserDetails(LoggedInUser userDetails) {
 		this.userDetails = userDetails;
 	}
@@ -213,6 +229,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getLovDescCustRecordType() {
 		return lovDescCustRecordType;
 	}
+
 	public void setLovDescCustRecordType(String lovDescCustRecordType) {
 		this.lovDescCustRecordType = lovDescCustRecordType;
 	}
@@ -220,6 +237,7 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getLovDescCustCIF() {
 		return lovDescCustCIF;
 	}
+
 	public void setLovDescCustCIF(String lovDescCustCIF) {
 		this.lovDescCustCIF = lovDescCustCIF;
 	}
@@ -227,14 +245,15 @@ public class CustomerExtLiability extends AbstractWorkflowEntity {
 	public String getLovDescCustShrtName() {
 		return lovDescCustShrtName;
 	}
+
 	public void setLovDescCustShrtName(String lovDescCustShrtName) {
 		this.lovDescCustShrtName = lovDescCustShrtName;
 	}
-	
-	public void setLoginDetails(LoggedInUser userDetails){
+
+	public void setLoginDetails(LoggedInUser userDetails) {
 		setLastMntBy(userDetails.getUserId());
-		this.userDetails=userDetails;
-		
+		this.userDetails = userDetails;
+
 	}
 
 	public String getSourceId() {
