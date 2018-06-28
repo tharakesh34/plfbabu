01bd184e737335279338e68500e0a77d063649ce
durgaprasad.g <durgaprasad.g@pennanttech.com>
2018-06-28 11:35:06 +0000

    Legal Module
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@91815 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ PFSJava/src/main/java/com/pennant/backend/service/legal/LegalDetailService.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2011 - Pennant Technologies
+ * 
+ * This file is part of Pennant Java Application Framework and related Products. 
+ * All components/modules/functions/classes/logic in this software, unless 
+ * otherwise stated, the property of Pennant Technologies. 
+ * 
+ * Copyright and other intellectual property laws protect these materials. 
+ * Reproduction or retransmission of the materials, in whole or in part, in any manner, 
+ * without the prior written consent of the copyright holder, is a violation of 
+ * copyright law.
+ */
+
+/**
+ ********************************************************************************************
+ *                                 FILE HEADER                                              *
+ ********************************************************************************************
+ *																							*
+ * FileName    		:  LegalDetailService.java                                                   * 	  
+ *                                                                    						*
+ * Author      		:  PENNANT TECHONOLOGIES              									*
+ *                                                                  						*
+ * Creation Date    :  16-06-2018    														*
+ *                                                                  						*
+ * Modified Date    :  16-06-2018    														*
+ *                                                                  						*
+ * Description 		:                                             							*
+ *                                                                                          *
+ ********************************************************************************************
+ * Date             Author                   Version      Comments                          *
+ ********************************************************************************************
+ * 16-06-2018       PENNANT	                 0.1                                            * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ *                                                                                          * 
+ ********************************************************************************************
+*/
+package com.pennant.backend.service.legal;
+
+import com.pennant.backend.model.audit.AuditHeader;
+import com.pennant.backend.model.finance.FinanceDetail;
+import com.pennant.backend.model.legal.LegalDetail;
+
+public interface LegalDetailService {
+
+	AuditHeader saveOrUpdate(AuditHeader auditHeader);
+
+	LegalDetail getLegalDetail(long legalId);
+
+	LegalDetail getApprovedLegalDetail(long legalId);
+
+	AuditHeader delete(AuditHeader auditHeader);
+
+	AuditHeader doApprove(AuditHeader auditHeader);
+
+	AuditHeader doReject(AuditHeader auditHeader);
+
+	void saveLegalDetails(FinanceDetail financeDetail);
+}
