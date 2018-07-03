747cfaad7b2ecc53285272d73ef088e8bf412fe0
kesava.j <kesava.j@pennanttech.com>
2018-07-03 09:39:15 +0000

    PSL Data Committing.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@92168 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ PFSJava/src/main/java/com/pennant/backend/model/finance/psl/PSLEndUse.java
@@ -0,0 +1,26 @@
+package com.pennant.backend.model.finance.psl;
+
+public class PSLEndUse {
+	private String purposeCode;
+	private String code;
+	private String description;
+	public String getPurposeCode() {
+		return purposeCode;
+	}
+	public void setPurposeCode(String purposeCode) {
+		this.purposeCode = purposeCode;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ PFSJava/src/main/java/com/pennant/backend/model/finance/psl/PSLPurpose.java
@@ -0,0 +1,33 @@
+package com.pennant.backend.model.finance.psl;
+
+public class PSLPurpose {
+	private String categoryCode;
+	private String subCategoryCode;
+	private String code;
+	private String description;
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public String getSubCategoryCode() {
+		return subCategoryCode;
+	}
+	public void setSubCategoryCode(String subCategoryCode) {
+		this.subCategoryCode = subCategoryCode;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
