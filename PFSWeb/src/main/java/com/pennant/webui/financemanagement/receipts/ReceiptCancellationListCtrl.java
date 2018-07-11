93e7f66cf185506bb3677ad9dc8fcfff126a9b5e
siva.m <siva.m@pennanttech.com>
2018-07-11 09:26:45 +0000

    Filtering for Postgres is corrected on list after approvals
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@92670 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/financemanagement/receipts/ReceiptCancellationListCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/financemanagement/receipts/ReceiptCancellationListCtrl.java
@@ -179,7 +179,7 @@ public class ReceiptCancellationListCtrl extends GFCBaseListCtrl<FinReceiptHeade
 			this.searchObject.addWhereClause(" FinIsActive = 1 AND ReceiptMode IN( '"+RepayConstants.RECEIPTMODE_CHEQUE+"','"+RepayConstants.RECEIPTMODE_DD+"') AND "
 					+ " ReceiptPurpose = '"+FinanceConstants.FINSER_EVENT_SCHDRPY+"' AND (ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_APPROVED+"' "
 					+ " OR (ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_REALIZED+"' AND (RecordType IS NULL OR RecordType='' ))"
-					+ " OR ( ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_BOUNCE+"' AND RecordType IS NOT NULL) ) ");
+					+ " OR ( ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_BOUNCE+"' AND RecordType IS NOT NULL AND RecordType != '') ) ");
 		}else if (StringUtils.equals(this.module, RepayConstants.MODULETYPE_CANCEL)) {
 			this.searchObject.addWhereClause("  FinIsActive = 1 AND ReceiptPurpose = '"+FinanceConstants.FINSER_EVENT_SCHDRPY+"' "
 					+ " AND (ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_APPROVED+"' OR (ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_REALIZED+"' "
@@ -187,7 +187,7 @@ public class ReceiptCancellationListCtrl extends GFCBaseListCtrl<FinReceiptHeade
 		}else if (StringUtils.equals(this.module, RepayConstants.MODULETYPE_FEE)) {
 			this.searchObject.addWhereClause("  FinIsActive = 1 AND ReceiptPurpose = '"+FinanceConstants.FINSER_EVENT_FEEPAYMENT+"' AND "
 					+ " ((ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_FEES+"'  AND( RecordType IS NULL OR RecordType='')) "
-					+ " OR ( ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_CANCEL+"' AND RecordType IS NOT NULL) ) ");
+					+ " OR ( ReceiptModeStatus = '"+RepayConstants.PAYSTATUS_CANCEL+"' AND RecordType IS NOT NULL AND RecordType != '') ) ");
 		}
 	}
 
