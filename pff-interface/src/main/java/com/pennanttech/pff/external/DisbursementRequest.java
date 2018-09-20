fba128caf425b01eb5091662de8822d792eefc1b
murthy.y <murthy.y@pennanttech.com>
2018-09-20 14:30:13 +0000

    Core <=> Bajaj: New Method add in AgreementEngine
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@95503 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface/src/main/java/com/pennanttech/pff/external/DisbursementRequest.java
+++ pff-interface/src/main/java/com/pennanttech/pff/external/DisbursementRequest.java
@@ -1,5 +1,5 @@
 package com.pennanttech.pff.external;
 
 public interface DisbursementRequest {
-	public void sendReqest(Object... params) throws Exception;
+	public String sendReqest(Object... params) throws Exception;
 }
