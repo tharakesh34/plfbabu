b377b03265a17c055497998bdee6f868437a0781
murthy.y <murthy.y@pennanttech.com>
2018-09-21 09:25:49 +0000

    Core <=> Bajaj: Wrong commit reverted
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@95537 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface/src/main/java/com/pennanttech/pff/external/DisbursementRequest.java
+++ pff-interface/src/main/java/com/pennanttech/pff/external/DisbursementRequest.java
@@ -1,5 +1,5 @@
 package com.pennanttech.pff.external;
 
 public interface DisbursementRequest {
-	public String sendReqest(Object... params) throws Exception;
+	public void sendReqest(Object... params) throws Exception;
 }
