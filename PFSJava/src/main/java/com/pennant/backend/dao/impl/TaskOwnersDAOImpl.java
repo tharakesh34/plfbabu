e82de4c468c0958341c3d995c768fba1987ff4d8
Sai Krishna P <saikrishna.p@pennanttech.com>
2018-09-30 15:33:54 +0000

    Core>>Bajaj: Formated
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@95936 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/impl/TaskOwnersDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/impl/TaskOwnersDAOImpl.java
@@ -66,36 +66,36 @@ import com.pennanttech.pennapps.core.resource.Literal;
 
 public class TaskOwnersDAOImpl extends BasicDao<TaskOwners> implements TaskOwnersDAO {
 	private static Logger logger = Logger.getLogger(TaskOwnersDAOImpl.class);
-	
+
 	public TaskOwnersDAOImpl() {
 		super();
 	}
-		
+
 	@Override
-    public void save(TaskOwners taskOwners) {
+	public void save(TaskOwners taskOwners) {
 		logger.debug("Entering");
-		
-		StringBuilder  insertSql = 	new StringBuilder(" INSERT INTO Task_Owners " );
+
+		StringBuilder insertSql = new StringBuilder(" INSERT INTO Task_Owners ");
 		insertSql.append(" (Reference, RoleCode, ActualOwner, CurrentOwner, Processed)");
 		insertSql.append(" Values( :Reference, :RoleCode, :ActualOwner, :CurrentOwner, :Processed)");
 		logger.debug("insertSql: " + insertSql.toString());
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(taskOwners);
 		try {
 			this.jdbcTemplate.update(insertSql.toString(), beanParameters);
-		}catch(DuplicateKeyException e){
+		} catch (DuplicateKeyException e) {
 			logger.debug("Exception: ", e);
 		}
 		logger.debug("Leaving");
-    }
+	}
 
 	@Override
-    public void update(TaskOwners taskOwners) {
+	public void update(TaskOwners taskOwners) {
 		logger.debug("Entering");
-		StringBuilder updateSql = 	new StringBuilder(" UPDATE Task_Owners" );
-		
-		if(taskOwners.getCurrentOwner() != taskOwners.getActualOwner()){
+		StringBuilder updateSql = new StringBuilder(" UPDATE Task_Owners");
+
+		if (taskOwners.getCurrentOwner() != taskOwners.getActualOwner()) {
 			updateSql.append(" SET CurrentOwner=:CurrentOwner");
-		}else{
+		} else {
 			updateSql.append(" SET CurrentOwner=:CurrentOwner,ActualOwner=:ActualOwner");
 		}
 		updateSql.append(" WHERE Reference=:Reference AND RoleCode=:RoleCode");
@@ -104,7 +104,7 @@ public class TaskOwnersDAOImpl extends BasicDao<TaskOwners> implements TaskOwner
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(taskOwners);
 		try {
 			this.jdbcTemplate.update(updateSql.toString(), beanParameters);
-		}catch(DuplicateKeyException e){
+		} catch (DuplicateKeyException e) {
 			logger.debug("Exception: ", e);
 		}
 		logger.debug("Leaving");
@@ -114,86 +114,85 @@ public class TaskOwnersDAOImpl extends BasicDao<TaskOwners> implements TaskOwner
 	@Override
 	public void delete(TaskOwners taskOwners) {
 		logger.debug("Entering ");
-		StringBuilder deleteSql = new StringBuilder(" Delete From Task_Owners" );
+		StringBuilder deleteSql = new StringBuilder(" Delete From Task_Owners");
 		deleteSql.append(" Where Reference =:Reference ");
 
-		logger.debug("deleteSql: "+ deleteSql.toString());
+		logger.debug("deleteSql: " + deleteSql.toString());
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(taskOwners);
-		try{
+		try {
 			this.jdbcTemplate.update(deleteSql.toString(), beanParameters);
-		}catch(DataAccessException e){
+		} catch (DataAccessException e) {
 			logger.debug("Exception: ", e);
 		}
 		logger.debug("Leaving ");
 	}
-	
-	
+
 	@Override
 	public void saveOrUpdateList(List<TaskOwners> taskOwners) {
 		logger.debug("Entering");
 		for (TaskOwners taskOwner : taskOwners) {
 			SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(taskOwner);
-			if(taskOwner.isNewRecord()){
-/*				StringBuilder  insertSql = 	new StringBuilder(" INSERT INTO Task_Owners " );
-				insertSql.append(" (Reference, RoleCode, ActualOwner, CurrentOwner, Processed)");
-				insertSql.append(" Values(  :Reference, :RoleCode, :ActualOwner, :CurrentOwner, :Processed)");
-				logger.debug("insertSql: " + insertSql.toString());
-
-				this.jdbcTemplate.update(insertSql.toString(), beanParameters);
-*/				
+			if (taskOwner.isNewRecord()) {
+				/*
+				 * StringBuilder insertSql = new StringBuilder(" INSERT INTO Task_Owners " );
+				 * insertSql.append(" (Reference, RoleCode, ActualOwner, CurrentOwner, Processed)");
+				 * insertSql.append(" Values(  :Reference, :RoleCode, :ActualOwner, :CurrentOwner, :Processed)");
+				 * logger.debug("insertSql: " + insertSql.toString());
+				 * 
+				 * this.jdbcTemplate.update(insertSql.toString(), beanParameters);
+				 */
 				syncRecord(taskOwner);
-				
-			}else {
+
+			} else {
 				logger.debug("Update");
-				StringBuilder updateSql = 	new StringBuilder(" UPDATE Task_Owners SET Processed=:Processed,");
-				updateSql.append(" CurrentOwner=:CurrentOwner,ActualOwner=:ActualOwner" );
+				StringBuilder updateSql = new StringBuilder(" UPDATE Task_Owners SET Processed=:Processed,");
+				updateSql.append(" CurrentOwner=:CurrentOwner,ActualOwner=:ActualOwner");
 				updateSql.append(" WHERE Reference=:Reference AND RoleCode=:RoleCode");
 				logger.debug("updateSql: " + updateSql.toString());
 
 				int recordCount = this.jdbcTemplate.update(updateSql.toString(), beanParameters);
-				if(recordCount <= 0){
+				if (recordCount <= 0) {
 					logger.debug("Unable to update");
 				}
 			}
 		}
 		logger.debug("Leaving");
 	}
-	
-	private void syncRecord(TaskOwners taskOwner){
+
+	private void syncRecord(TaskOwners taskOwner) {
 		logger.debug(Literal.ENTERING);
 		StringBuilder selectSql = new StringBuilder("SELECT COUNT(*) From Task_Owners");
 		selectSql.append(" Where Reference=:Reference AND RoleCode=:RoleCode AND ActualOwner=:ActualOwner");
 		logger.debug("SelectSQL: " + selectSql.toString());
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(taskOwner);
-		int count =0;
+		int count = 0;
 		try {
-			count= this.jdbcTemplate.queryForObject(selectSql.toString(), beanParameters, Integer.class);
-		}catch (EmptyResultDataAccessException e) {
+			count = this.jdbcTemplate.queryForObject(selectSql.toString(), beanParameters, Integer.class);
+		} catch (EmptyResultDataAccessException e) {
 		}
-		
-		if(count==0){
+
+		if (count == 0) {
 			logger.debug("Insert");
-			StringBuilder  insertSql = 	new StringBuilder(" INSERT INTO Task_Owners " );
+			StringBuilder insertSql = new StringBuilder(" INSERT INTO Task_Owners ");
 			insertSql.append(" (Reference, RoleCode, ActualOwner, CurrentOwner, Processed)");
 			insertSql.append(" Values(  :Reference, :RoleCode, :ActualOwner, :CurrentOwner, :Processed)");
 			logger.debug("insertSql: " + insertSql.toString());
 			this.jdbcTemplate.update(insertSql.toString(), beanParameters);
-		}else{
+		} else {
 			logger.debug("Update");
-			StringBuilder updateSql = 	new StringBuilder(" UPDATE Task_Owners SET Processed=:Processed,");
-			updateSql.append(" CurrentOwner=:CurrentOwner " );
+			StringBuilder updateSql = new StringBuilder(" UPDATE Task_Owners SET Processed=:Processed,");
+			updateSql.append(" CurrentOwner=:CurrentOwner ");
 			updateSql.append(" WHERE Reference=:Reference AND RoleCode=:RoleCode AND ActualOwner=:ActualOwner");
 			logger.debug("updateSql: " + updateSql.toString());
 			this.jdbcTemplate.update(updateSql.toString(), beanParameters);
 		}
 		logger.debug(Literal.LEAVING);
 	}
-	
-	
+
 	@Override
 	public void updateList(List<TaskOwners> taskOwners) {
 		logger.debug("Entering");
-		StringBuilder updateSql = 	new StringBuilder(" UPDATE Task_Owners SET CurrentOwner=:CurrentOwner" );
+		StringBuilder updateSql = new StringBuilder(" UPDATE Task_Owners SET CurrentOwner=:CurrentOwner");
 		updateSql.append(" WHERE Reference=:Reference AND RoleCode=:RoleCode AND ActualOwner=:ActualOwner)");
 		logger.debug("updateSql: " + updateSql.toString());
 
@@ -204,58 +203,59 @@ public class TaskOwnersDAOImpl extends BasicDao<TaskOwners> implements TaskOwner
 	}
 
 	@Override
-    public TaskOwners getTaskOwner(String finReference, String roleCode) {
+	public TaskOwners getTaskOwner(String finReference, String roleCode) {
 		logger.debug("Entering");
 		TaskOwners taskOwners = new TaskOwners();
 		taskOwners.setReference(finReference);
 		taskOwners.setRoleCode(roleCode);
-		StringBuilder selectSql = new StringBuilder("SELECT Reference, RoleCode, ActualOwner, CurrentOwner, Processed From Task_Owners");
+		StringBuilder selectSql = new StringBuilder(
+				"SELECT Reference, RoleCode, ActualOwner, CurrentOwner, Processed From Task_Owners");
 		selectSql.append(" Where Reference=:Reference AND RoleCode=:RoleCode");
-		
+
 		RowMapper<TaskOwners> typeRowMapper = ParameterizedBeanPropertyRowMapper.newInstance(TaskOwners.class);
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(taskOwners);
 		logger.debug("selectSql: " + selectSql.toString());
 		try {
 			taskOwners = this.jdbcTemplate.queryForObject(selectSql.toString(), beanParameters, typeRowMapper);
-		}catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			logger.warn("Exception: ", e);
 			return null;
 		}
 		return taskOwners;
-    }
+	}
 
 	@Override
-    public boolean checkIfUserAlreadyAccessed(String finReferences, String selectedUser, String roleCode) {
+	public boolean checkIfUserAlreadyAccessed(String finReferences, String selectedUser, String roleCode) {
 		logger.debug("Entering");
 		Map<String, String> parameterMap = new HashMap<String, String>();
 		parameterMap.put("Reference", finReferences);
 		parameterMap.put("ActualOwner", selectedUser);
 		parameterMap.put("CurrentOwner", selectedUser);
 		parameterMap.put("RoleCode", roleCode);
-		
+
 		StringBuilder selectSql = new StringBuilder("SELECT count(reference) From Task_Owners");
 		selectSql.append(" Where Reference IN (:Reference) AND ActualOwner=:ActualOwner AND RoleCode=:RoleCode");
 		logger.debug("selectSql: " + selectSql.toString());
-		if(this.jdbcTemplate.queryForObject(selectSql.toString(),parameterMap, Integer.class)>0){
+		if (this.jdbcTemplate.queryForObject(selectSql.toString(), parameterMap, Integer.class) > 0) {
 			return true;
 		}
 		selectSql = new StringBuilder("SELECT count(reference) From Task_Owners");
 		selectSql.append(" Where Reference IN (:Reference) AND CurrentOwner=:CurrentOwner");
 		logger.debug("selectSql: " + selectSql.toString());
-		if(this.jdbcTemplate.queryForObject(selectSql.toString(),parameterMap, Integer.class)>0){
+		if (this.jdbcTemplate.queryForObject(selectSql.toString(), parameterMap, Integer.class) > 0) {
 			return false;
 		}
 		return true;
-    }
+	}
 
 	@Override
-    public String getUserRoleCodeByRefernce(long userId, String reference, List<String> userRoles) {
+	public String getUserRoleCodeByRefernce(long userId, String reference, List<String> userRoles) {
 		logger.debug("Entering");
 		String roleCode = null;
 		MapSqlParameterSource source = new MapSqlParameterSource();
 		source.addValue("CurrentOwner", userId);
 		source.addValue("Reference", reference);
-		Map<String,List<String>> usrRoles =	new HashMap<String,List<String>>();
+		Map<String, List<String>> usrRoles = new HashMap<String, List<String>>();
 		usrRoles.put("UserRoles", userRoles);
 		source.addValues(usrRoles);
 		StringBuilder selectSql = new StringBuilder("SELECT RoleCode From Task_Owners");
@@ -263,51 +263,56 @@ public class TaskOwnersDAOImpl extends BasicDao<TaskOwners> implements TaskOwner
 		logger.debug("selectSql: " + selectSql.toString());
 		try {
 			roleCode = this.jdbcTemplate.queryForObject(selectSql.toString(), source, String.class);
-		}catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			logger.warn("Exception: ", e);
-			selectSql = new StringBuilder("SELECT RoleCode From (select RoleCode, row_number() over (order by CurrentOwner desc)");
-			selectSql.append("row_num From Task_Owners where Reference=:Reference AND CurrentOwner=0 AND RoleCode in (:UserRoles)) Task Where row_num <= 1 ");
+			selectSql = new StringBuilder(
+					"SELECT RoleCode From (select RoleCode, row_number() over (order by CurrentOwner desc)");
+			selectSql.append(
+					"row_num From Task_Owners where Reference=:Reference AND CurrentOwner=0 AND RoleCode in (:UserRoles)) Task Where row_num <= 1 ");
 			logger.debug("selectSql: " + selectSql.toString());
 			try {
 				roleCode = this.jdbcTemplate.queryForObject(selectSql.toString(), source, String.class);
-			}catch (EmptyResultDataAccessException e1) {
+			} catch (EmptyResultDataAccessException e1) {
 				logger.warn("Exception: ", e1);
 				return null;
 			}
 		}
 		logger.debug("Leaving");
 		return roleCode;
-    }
+	}
 
 	@Override
-    public List<TaskOwners> getTaskOwnerList(String reference, String roleCode) {
+	public List<TaskOwners> getTaskOwnerList(String reference, String roleCode) {
 		logger.debug("Entering");
 		MapSqlParameterSource source = new MapSqlParameterSource();
 		source.addValue("Reference", reference);
 		source.addValue("RoleCode", Arrays.asList(StringUtils.trimToEmpty(roleCode).split(",")));
-		StringBuilder selectSql = new StringBuilder("SELECT Reference, RoleCode, ActualOwner, CurrentOwner, Processed From Task_Owners");
+		StringBuilder selectSql = new StringBuilder(
+				"SELECT Reference, RoleCode, ActualOwner, CurrentOwner, Processed From Task_Owners");
 		selectSql.append(" Where Reference=:Reference AND RoleCode IN (:RoleCode)");
-		
+
 		RowMapper<TaskOwners> typeRowMapper = ParameterizedBeanPropertyRowMapper.newInstance(TaskOwners.class);
 		logger.debug("selectSql: " + selectSql.toString());
 		return this.jdbcTemplate.query(selectSql.toString(), source, typeRowMapper);
-    }
+	}
 
 	@Override
 	public void deviationReject(String finreference, String roleCode, String nextRoleCode) {
 
 		MapSqlParameterSource source = new MapSqlParameterSource();
 		source.addValue("Reference", finreference);
-		source.addValue("RoleCode",roleCode);
-		source.addValue("NextRoleCode",nextRoleCode);
-		source.addValue("Processed",0);
+		source.addValue("RoleCode", roleCode);
+		source.addValue("NextRoleCode", nextRoleCode);
+		source.addValue("Processed", 0);
 
-		StringBuilder updateSql = new StringBuilder("Update Task_Owners set Processed=:Processed where Reference=:Reference AND RoleCode=:RoleCode ");
+		StringBuilder updateSql = new StringBuilder(
+				"Update Task_Owners set Processed=:Processed where Reference=:Reference AND RoleCode=:RoleCode ");
 
 		logger.debug("selectSql: " + updateSql.toString());
 		this.jdbcTemplate.update(updateSql.toString(), source);
 
-		StringBuilder deleteSql = new StringBuilder("Delete from Task_Owners where Reference=:Reference AND RoleCode=:NextRoleCode ");
+		StringBuilder deleteSql = new StringBuilder(
+				"Delete from Task_Owners where Reference=:Reference AND RoleCode=:NextRoleCode ");
 
 		logger.debug("selectSql: " + deleteSql.toString());
 		this.jdbcTemplate.update(deleteSql.toString(), source);
