90ad1f3dd23b892d40d10a96cad4025a7778c618
murthy.y <murthy.y@pennanttech.com>
2018-10-11 18:59:39 +0000

    Core=> Bajaj: Formatted
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@96506 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/util/SMTParameterConstants.java
+++ PFSJava/src/main/java/com/pennant/backend/util/SMTParameterConstants.java
@@ -42,40 +42,39 @@ public class SMTParameterConstants {
 		super();
 	}
 
-	public static final String	SUSP_CHECK_REQ		= "SUSP_CHECK_REQ";
-	public static final String	EOD_THREAD_COUNT	= "EOD_THREAD_COUNT";
-	public static final String	IGNORING_BUCKET		= "IGNORING_BUCKET";
-	public static final String	EOD_CHUNK_SIZE		= "EOD_CHUNK_SIZE";
-	public static final String	ACCRUAL_CAL_ON		= "ACCRUAL_CAL_ON";//0-eod, 1-sod
-	public static final String	ACC_EFF_VALDATE		= "ACC_EFF_VALDATE";// Y - Accrual Effective Date will be Value Date, N - Accrual Effective Date will be APP Date
-	public static final String	ROUND_LASTSCHD		= "ROUND_LASTSCHD";
-	public static final String	KYC_PRIORITY		= "DEFAULT_KYC_PRIORITY";
-	public static final String	ID_PANCARD			= "PAN_DOC_TYPE";
-	public static final String	DEFAULT_KYC_PRIORITY = "DEFAULT_KYC_PRIORITY";
-	public static final int		PRESENTATION_HOLD_DAYS	= 2;
-	public static final String	ALMEXTRACT_FROMFINSTARTDATE =  "ALMEXTRACT_FROMFINSTARTDATE";
-	public static final String	PANCARD_REQ			= "PAN_REQ";
-	public static final String	LOAN_REF_FORMAT		= "LOAN_REF_FORMAT";
-	public static final String	LOAN_REF_PREFIX		= "LOAN_REF_PREFIX";
-	public static final String	FEESALLOWZERO		= "FEESALLOWZERO";
-	public static final String	ELGMETHOD			= "ELGMETHOD";
-	public static final String	ALW_DIFF_RPYHCY_NPA	= "ALW_DIFF_RPYHCY_NPA";
-	public static final String	RPYHCY_ON_NPA		= "RPYHCY_ON_NPA";
-	public static final String	DISB_PAID_STATUS	= "DISB_PAID_STATUS";
-	public static final String	IS_CREDITREVIEW_TAB_REQ	= "IS_CREDITREVIEW_TAB_REQ";
-	public static final String	PRESENTMENT_RESPONSE_ROW_LENGTH	= "PRESENTMENT_RESPONSE_ROW_LENGTH";
-	public static final String	NUMBEROF_UNDATED_CHEQUES	= "NUMBEROF_UNDATED_CHEQUES";
-	public static final String	NUMBEROF_PDC_CHEQUES	= "NUMBEROF_PDC_CHEQUES";
-	public static final String	QRY_MGMT_TEMPLATE	= "QRY_MGMT_TEMPLATE";
-	public static final String  DISB_ALLOW_WITH_OTC_UNDER_COVENANT_FULLFILL="COVENANT DETAILS REQUIRED";
-	public static final String  BAJAJFINANCE_STARTDATE ="1/1/2017";
-	public static final String	CUSTBRANCH			= "CUSTBRANCH";
-	
+	public static final String SUSP_CHECK_REQ = "SUSP_CHECK_REQ";
+	public static final String EOD_THREAD_COUNT = "EOD_THREAD_COUNT";
+	public static final String IGNORING_BUCKET = "IGNORING_BUCKET";
+	public static final String EOD_CHUNK_SIZE = "EOD_CHUNK_SIZE";
+	public static final String ACCRUAL_CAL_ON = "ACCRUAL_CAL_ON";//0-eod, 1-sod
+	public static final String ACC_EFF_VALDATE = "ACC_EFF_VALDATE";// Y - Accrual Effective Date will be Value Date, N - Accrual Effective Date will be APP Date
+	public static final String ROUND_LASTSCHD = "ROUND_LASTSCHD";
+	public static final String KYC_PRIORITY = "DEFAULT_KYC_PRIORITY";
+	public static final String ID_PANCARD = "PAN_DOC_TYPE";
+	public static final String DEFAULT_KYC_PRIORITY = "DEFAULT_KYC_PRIORITY";
+	public static final int PRESENTATION_HOLD_DAYS = 2;
+	public static final String ALMEXTRACT_FROMFINSTARTDATE = "ALMEXTRACT_FROMFINSTARTDATE";
+	public static final String PANCARD_REQ = "PAN_REQ";
+	public static final String LOAN_REF_FORMAT = "LOAN_REF_FORMAT";
+	public static final String LOAN_REF_PREFIX = "LOAN_REF_PREFIX";
+	public static final String FEESALLOWZERO = "FEESALLOWZERO";
+	public static final String ELGMETHOD = "ELGMETHOD";
+	public static final String ALW_DIFF_RPYHCY_NPA = "ALW_DIFF_RPYHCY_NPA";
+	public static final String RPYHCY_ON_NPA = "RPYHCY_ON_NPA";
+	public static final String DISB_PAID_STATUS = "DISB_PAID_STATUS";
+	public static final String IS_CREDITREVIEW_TAB_REQ = "IS_CREDITREVIEW_TAB_REQ";
+	public static final String PRESENTMENT_RESPONSE_ROW_LENGTH = "PRESENTMENT_RESPONSE_ROW_LENGTH";
+	public static final String NUMBEROF_UNDATED_CHEQUES = "NUMBEROF_UNDATED_CHEQUES";
+	public static final String NUMBEROF_PDC_CHEQUES = "NUMBEROF_PDC_CHEQUES";
+	public static final String QRY_MGMT_TEMPLATE = "QRY_MGMT_TEMPLATE";
+	public static final String DISB_ALLOW_WITH_OTC_UNDER_COVENANT_FULLFILL = "COVENANT DETAILS REQUIRED";
+	public static final String BAJAJFINANCE_STARTDATE = "1/1/2017";
+	public static final String CUSTBRANCH = "CUSTBRANCH";
+
 	/**
-	 * NO_ADJ 		  = No Adjustment, leave the Profit Fraction
-	 * ADJ_LAST_INST  = All Profit Fractions adjust to last schedule
-	 * ADJ_NEXT_INST  = Profit Fraction adjust to immediate next schedule
+	 * NO_ADJ = No Adjustment, leave the Profit Fraction ADJ_LAST_INST = All Profit Fractions adjust to last schedule
+	 * ADJ_NEXT_INST = Profit Fraction adjust to immediate next schedule
 	 * 
 	 */
-	public static final String	ROUND_ADJ_METHOD	= "ROUND_ADJ_METHOD";
+	public static final String ROUND_ADJ_METHOD = "ROUND_ADJ_METHOD";
 }
