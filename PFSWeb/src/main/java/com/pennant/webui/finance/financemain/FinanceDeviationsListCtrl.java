a3a98091581f9001e7af3c2654bc1c1ead387392
vinay.b <vinay.b@pennanttech.com>
2018-10-24 10:38:40 +0000

    PPM #18124
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@97113 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/finance/financemain/FinanceDeviationsListCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/finance/financemain/FinanceDeviationsListCtrl.java
@@ -188,6 +188,7 @@ public class FinanceDeviationsListCtrl extends GFCBaseListCtrl<FinanceMain> {
 	}
 
 	protected void doPrintResults() {
+		//super.doPrintResults();
 		try {
 			new PTListReportUtils(this.loanType.getValue() + "FinanceMain", super.searchObject,
 					this.pagingFinanceMainList.getTotalSize() + 1);
