db8c2c5b20d57d79e2002f9408c97a3cdc3eb5d3
shinde.b <shinde.b@pennanttech.com>
2019-01-02 14:16:51 +0000

    clean up.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@101628 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/applicationmaster/businessvertical/BusinessVerticalDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/applicationmaster/businessvertical/BusinessVerticalDialogCtrl.java
@@ -65,7 +65,6 @@ import com.pennant.backend.service.applicationmaster.BusinessVerticalService;
 import com.pennant.backend.util.PennantConstants;
 import com.pennant.backend.util.PennantRegularExpressions;
 import com.pennant.util.ErrorControl;
-import com.pennant.util.Constraint.PTDecimalValidator;
 import com.pennant.util.Constraint.PTStringValidator;
 import com.pennant.webui.util.GFCBaseCtrl;
 import com.pennanttech.pennapps.core.model.ErrorDetail;
