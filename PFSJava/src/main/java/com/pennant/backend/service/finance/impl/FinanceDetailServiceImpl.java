b466758761431d5ab9e229bdbf971400d7e6fc4f
Sai Krishna P <saikrishna.p@pennanttech.com>
2019-01-30 09:18:49 +0000

    Re-factoring...
    git-svn-id: http://192.168.110.32/svn/Products/PFF/Base/Branches/MLOD@102976 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/service/finance/impl/FinanceDetailServiceImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/service/finance/impl/FinanceDetailServiceImpl.java
@@ -215,7 +215,6 @@ import com.pennant.backend.model.rulefactory.Rule;
 import com.pennant.backend.model.smtmasters.PFSParameter;
 import com.pennant.backend.model.systemmasters.IncomeType;
 import com.pennant.backend.service.UpdateAttributeServiceTask;
-import com.pennant.backend.service.amtmasters.VehicleDealerService;
 import com.pennant.backend.service.authorization.AuthorizationLimitService;
 import com.pennant.backend.service.collateral.CollateralMarkProcess;
 import com.pennant.backend.service.collateral.CollateralSetupService;
@@ -344,7 +343,6 @@ public class FinanceDetailServiceImpl extends GenericFinanceDetailService implem
 	private FinTypeExpenseDAO finTypeExpenseDAO;
 	private FinExpenseDetailsDAO finExpenseDetailsDAO;
 	private FinIRRDetailsDAO finIRRDetailsDAO;
-	private VehicleDealerService vehicleDealerService;
 	private PSLDetailService pSLDetailService;
 	private CollateralSetupService collateralSetupService;
 	private HoldDisbursementDAO holdDisbursementDAO;
@@ -739,7 +737,7 @@ public class FinanceDetailServiceImpl extends GenericFinanceDetailService implem
 			dealerIds.add(financeMain.getConnector());
 		}
 		if (dealerIds.size() > 0) {
-			List<VehicleDealer> vehicleDealerList = vehicleDealerService.getVehicleDealerById(dealerIds);
+			List<VehicleDealer> vehicleDealerList = getVehicleDealerService().getVehicleDealerById(dealerIds);
 			if (vehicleDealerList != null && !vehicleDealerList.isEmpty()) {
 				for (VehicleDealer dealer : vehicleDealerList) {
 					if (dealer.getDealerId() == dmaCode) {
@@ -10536,10 +10534,6 @@ public class FinanceDetailServiceImpl extends GenericFinanceDetailService implem
 
 	public void setFinIRRDetailsDAO(FinIRRDetailsDAO finIRRDetailsDAO) {
 		this.finIRRDetailsDAO = finIRRDetailsDAO;
-	}
-
-	public void setVehicleDealerService(VehicleDealerService vehicleDealerService) {
-		this.vehicleDealerService = vehicleDealerService;
 	}
 
 	public CollateralSetupService getCollateralSetupService() {
