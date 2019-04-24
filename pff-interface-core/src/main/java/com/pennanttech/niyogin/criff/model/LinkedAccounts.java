5bb86557dddf8ad650ae18b5b5b47a0d16ab2c09
murthy.y <murthy.y@pennanttech.com>
2019-04-24 07:46:07 +0000

    Single Source
    git-svn-id: http://192.168.110.32/svn/Products/PFF/core/branches/mlod@108256 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface-core/src/main/java/com/pennanttech/niyogin/criff/model/LinkedAccounts.java
+++
@@ -1,5 +0,0 @@
-package com.pennanttech.niyogin.criff.model;
-
-public class LinkedAccounts {
-
-}
--- pff-interface-core/src/main/java/com/pennanttech/niyogin/dedup/model/MatchedField.java
+++
@@ -1,9 +0,0 @@
-package com.pennanttech.niyogin.dedup.model;
-
-import java.io.Serializable;
-
-public class MatchedField implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
