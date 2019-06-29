ec597b89e1c85a1d6942f1825bf6e2b25512b520
satish.s <satish.s@pennanttech.com>
2019-06-29 11:16:06 +0000

    Ledger Download changes.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/core/branches/mlod@113366 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ PFSWeb/src/main/java/com/pennant/webui/applicationmaster/download/LedgerDownloadProcessCtrl.java
@@ -0,0 +1,47 @@
+
+package com.pennant.webui.applicationmaster.download;
+
+import org.apache.log4j.Logger;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.ForwardEvent;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Radio;
+import org.zkoss.zul.Radiogroup;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+import com.pennant.app.util.DateUtility;
+import com.pennant.webui.util.GFCBaseCtrl;
+import com.pennanttech.external.clix.services.impl.LedgerDownloadServiceImpl;
+
+public class LedgerDownloadProcessCtrl extends GFCBaseCtrl {
+	private static final long serialVersionUID = 223801324705386693L;
+	private static final Logger logger = Logger.getLogger(LedgerDownloadProcessCtrl.class);
+
+	protected Window window_Download; // autoWired
+	protected Textbox fileName;
+	protected Radio radioDownload; // autoWired
+	protected Radio radioUpload;
+	protected Button btnStartLedgerFile;
+	private LedgerDownloadServiceImpl ledgerDownloadService;
+
+	public LedgerDownloadProcessCtrl() {
+		super();
+	}
+
+	public void onCreate$window_Download(Event event) throws Exception {
+		logger.debug("Entering");
+		setPageComponents(window_Download);
+		logger.debug("Leaving" + event.toString());
+	}
+
+	public void setLedgerDownloadService(LedgerDownloadServiceImpl ledgerDownloadService) {
+		this.ledgerDownloadService = ledgerDownloadService;
+	}
+
+	public void onClick$btnStartLedgerFile(ForwardEvent event) throws Exception {
+		ledgerDownloadService.processDownload(DateUtility.getAppDate(),DateUtility.getLastBusinessdate(),false);
+	}
+
+}
