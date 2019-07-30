96a678ae364198f48b77ed761e2f59111d3063c1
durgaprasad.g <durgaprasad.g@pennanttech.com>
2019-07-30 08:29:59 +0000

    Version updation 5.0.7 to 5.0.8
    git-svn-id: http://192.168.110.32/svn/Products/PFF/core/branches/mlod@115770 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-interface/src/main/java/com/pennanttech/pff/external/CustomerPaymentService.java
@@ -0,0 +1,9 @@
+package com.pennanttech.pff.external;
+
+import java.util.List;
+
+import com.pennant.backend.model.finance.FinAdvancePayments;
+
+public interface CustomerPaymentService {
+	public void processOnlinePayment(List<FinAdvancePayments> finAdvPaymentList);
+}
