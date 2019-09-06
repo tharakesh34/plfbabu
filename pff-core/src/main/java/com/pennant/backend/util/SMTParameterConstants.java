4c6226c2665f3f7951681654cd6bb46c1a0ea204
vijaya.a <vijaya.a@pennanttech.com>
2019-09-06 06:50:01 +0000

    Clix new functionality committed.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/trunk@118946 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-core/src/main/java/com/pennant/backend/util/SMTParameterConstants.java
+++ pff-core/src/main/java/com/pennant/backend/util/SMTParameterConstants.java
@@ -250,4 +250,5 @@ public class SMTParameterConstants {
 	public static final String CUST_PAN_VALIDATION = "CUST_PAN_VALIDATION"; // Profit Waiver in Fee Waivers
 	public static final String CUSTOM_DEVIATION_FILE_PATH = "CUSTOM_DEVIATION_FILE_PATH"; //Custom Deviations File Path
 	public static final String ALLOW_CIBIL_REQUEST = "ALLOW_CIBIL_REQUEST"; //Cibil Request
+	public static final String CLIX_VERIFICATIONS_CUSTOMERVIEW = "CLIX_VERIFICATIONS_CUSTOMERVIEW";
 }
