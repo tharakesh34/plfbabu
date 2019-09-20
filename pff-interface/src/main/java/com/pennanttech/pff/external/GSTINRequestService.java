8a75dba8bbd9b5ee78aa180f1b342d1452bca764
kesava.j <kesava.j@pennanttech.com>
2019-09-20 09:39:02 +0000

    GST verification Service
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@119928 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface/src/main/java/com/pennanttech/pff/external/GSTINRequestService.java
+++ pff-interface/src/main/java/com/pennanttech/pff/external/GSTINRequestService.java
@@ -3,5 +3,7 @@ package com.pennanttech.pff.external;
 import com.pennant.backend.model.finance.financetaxdetail.GSTINInfo;
 
 public interface GSTINRequestService {
+	public GSTINInfo gstinValidation(String gstnNumber);
+
 	public GSTINInfo gstinValidation(String gstnNumber, String cif, String loginId);
 }
