6d17d24a61eaad1187bec9fa96ff964abed6983a
murthy.y <murthy.y@pennanttech.com>
2019-09-20 12:30:40 +0000

    domain  Validation service renamed
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@119954 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-interface/src/main/java/com/pennanttech/pff/external/DomainCheckService.java
+++ pff-interface/src/main/java/com/pennanttech/pff/external/DomainCheckService.java
@@ -3,7 +3,7 @@ package com.pennanttech.pff.external;
 import com.pennant.backend.model.customermasters.CustomerDetails;
 import com.pennant.backend.model.finance.FinanceDetail;
 
-public interface DominValidationService {
+public interface DomainCheckService {
 	void validateDomain(CustomerDetails customerDetails);
 
 	void validateDomain(FinanceDetail detail);
