b8ecc81346413c941363106da7293c1f973333b6
pasvarma <pasvarma@pennanttech.com>
2019-10-09 06:21:04 +0000

    Added Interface Status
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@121060 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-interface/src/main/java/com/pennanttech/external/services/ExternalServiceStatus.java
@@ -0,0 +1,27 @@
+package com.pennanttech.external.services;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class ExternalServiceStatus {
+
+	private static String sqlQry ="Select active from EXTINTERFACECONF where code = :ServiceCode ";
+	private static NamedParameterJdbcTemplate jdbcTemplate;
+	
+	public static boolean isServiceActive(String serviceCode){
+		MapSqlParameterSource paramMap = new MapSqlParameterSource();
+		paramMap.addValue("ServiceCode", serviceCode);
+		try {
+			return jdbcTemplate.queryForObject(sqlQry, paramMap, Boolean.class);
+		} catch (Exception e) {
+			
+		}
+		return false;
+	}
+
+	public static void setDataSource(DataSource dataSource) {
+		ExternalServiceStatus.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+	}
+}
