1a2e092b861b32678c283aadb69aeea00c411c5f
varaprasad.k <varaprasad.k@pennanttech.com>
2019-10-11 14:19:54 +0000

    Push Notification Interface Related Changes.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@121238 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-model/src/main/java/com/pennant/backend/model/pushnotifications/PushNotificationsRequest.java
@@ -0,0 +1,78 @@
+package com.pennant.backend.model.pushnotifications;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "referenceId", "docCategory", "custDocTitle", "custDocIssuedCountry", "custDocSysName",
+		"custDocIssuedOn", "custDocExpDate", "docPurpose", "docName", "doctype", "docImage", "docUri" })
+@XmlRootElement(name = "DocumentDetail")
+@XmlAccessorType(XmlAccessType.NONE)
+public class PushNotificationsRequest {
+	private static final long serialVersionUID = -5569765259024813213L;
+	private String finReference;
+	private String roleCode;
+	private String recordStatus;
+	private String nextRoleCode;
+	private String product;
+
+	public PushNotificationsRequest() {
+		super();
+	}
+
+
+	public String getFinReference() {
+		return finReference;
+	}
+
+	public void setFinReference(String finReference) {
+		this.finReference = finReference;
+	}
+
+	public String getRoleCode() {
+		return roleCode;
+	}
+
+	public void setRoleCode(String roleCode) {
+		this.roleCode = roleCode;
+	}
+
+	public String getRecordStatus() {
+		return recordStatus;
+	}
+
+	public void setRecordStatus(String recordStatus) {
+		this.recordStatus = recordStatus;
+	}
+
+	public String getNextRoleCode() {
+		return nextRoleCode;
+	}
+
+	public void setNextRoleCode(String nextRoleCode) {
+		this.nextRoleCode = nextRoleCode;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
+    
+	
+    
+
+	
+
+	
+
+}
