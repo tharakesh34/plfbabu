e4f1a5150a856bbf5bb0137ad935d1a014a12ed9
amanullah.m <amanullah.m@pennanttech.com>
2019-10-23 12:45:42 +0000

    added CustomerTaxDetails.java
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@122032 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-model/src/main/java/com/pennant/backend/model/CustomerTaxDetails.java
@@ -0,0 +1,59 @@
+package com.pennant.backend.model;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class CustomerTaxDetails implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private long requestId;
+	private long customerId;
+	private String loanrefeNo;
+	private String orderStatus;
+	private long transactionId;
+	private String requestedBy;
+	private Timestamp requestDate;
+	
+	public long getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(long requestId) {
+		this.requestId = requestId;
+	}
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	public String getLoanrefeNo() {
+		return loanrefeNo;
+	}
+	public void setLoanrefeNo(String loanrefeNo) {
+		this.loanrefeNo = loanrefeNo;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public String getRequestedBy() {
+		return requestedBy;
+	}
+	public void setRequestedBy(String requestedBy) {
+		this.requestedBy = requestedBy;
+	}
+	public Timestamp getRequestDate() {
+		return requestDate;
+	}
+	public void setRequestDate(Timestamp requestDate) {
+		this.requestDate = requestDate;
+	}
+	
+}
