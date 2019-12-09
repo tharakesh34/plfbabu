f49b0ad407bbed023fd724c25de72e802ce54a0d
murthy.y <murthy.y@pennanttech.com>
2019-12-09 07:34:40 +0000

    JdbcUtil class added
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@125475 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-core/src/main/java/com/pennanttech/pennapps/core/jdbc/JdbcUtil.java
@@ -0,0 +1,31 @@
+package com.pennanttech.pennapps.core.jdbc;
+
+import java.sql.Date;
+
+public class JdbcUtil {
+	/**
+	 * Private constructor to hide the implicit public one.
+	 * 
+	 * @throws IllegalAccessException
+	 *         If the constructor is used to create and initialize a new instance of the declaring class by suppressing
+	 *         Java language access checking.
+	 */
+	private JdbcUtil() throws IllegalAccessException {
+		throw new IllegalAccessException();
+	}
+
+	/**
+	 * Returns {@link java.sql.Date} for the specified {@link Date}.
+	 * 
+	 * @param date
+	 *        The date object which which needs to be convert to {@link java.sql.Date} .
+	 * @return A {@link java.sql.Date} or <code>null</code> if input date is <code>null</code>.
+	 */
+	public static Date getDate(java.util.Date date) {
+		if (date == null) {
+			return null;
+		}
+
+		return new java.sql.Date(date.getTime());
+	}
+}
