bf3c47eba10cae72f4c3db4f799caccf6ca6f3d3
saikumar.b <saikumar.b@pennanttech.com>
2020-01-04 13:33:42 +0000

    New Service InsurancePremium(IIFL)
    z
    
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@127645 6963f9ea-81fa-44da-aa10-7ebde50aa541

---
+++ pff-interface/src/main/java/com/pennanttech/pff/external/InsuranceProspectService.java
@@ -0,0 +1,8 @@
+package com.pennanttech.pff.external;
+
+import com.pennant.backend.model.finance.FinanceDetail;
+
+public interface InsuranceProspectService {
+
+	public String getInsurancePremimumAPIResult(FinanceDetail financeDetail);
+}
