cd0e66f79eb3fef75c1339f074ed3c7e00f0613c
narendra.m <narendra.m@pennanttech.com>
2020-01-04 10:52:23 +0000

    PSD# 145740 The No.of terms are displayed incorrect in Loan basic details screen.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@127634 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/finance/enquiry/model/FinanceEnquiryListModelItemRenderer.java
+++ PFSWeb/src/main/java/com/pennant/webui/finance/enquiry/model/FinanceEnquiryListModelItemRenderer.java
@@ -38,11 +38,7 @@ public class FinanceEnquiryListModelItemRenderer implements ListitemRenderer<Fin
 		lc.setParent(item);
 		lc = new Listcell(DateUtility.formatToLongDate(enquiry.getFinStartDate()));
 		lc.setParent(item);
-		if (enquiry.getNOInst() > 0) {
-			lc = new Listcell(String.valueOf(enquiry.getNOInst()));
-		} else {
-			lc = new Listcell(String.valueOf(enquiry.getNumberOfTerms()));
-		}
+		lc = new Listcell(String.valueOf(enquiry.getNumberOfTerms()));//PSD# 145740
 		lc.setParent(item);
 		lc = new Listcell(DateUtility.formatToLongDate(enquiry.getMaturityDate()));
 		lc.setParent(item);
