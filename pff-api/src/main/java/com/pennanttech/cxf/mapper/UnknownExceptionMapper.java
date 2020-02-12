6479ef833ef3d6ebab35742392c689797f8fc295
shahbaj.b <shahbaj.b@pennanttech.com>
2020-02-12 22:31:38 +0000

    API service security issue i.e version disclosure, trace method enabled and missing security headers.
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@131024 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-api/src/main/java/com/pennanttech/cxf/mapper/UnknownExceptionMapper.java
+++ pff-api/src/main/java/com/pennanttech/cxf/mapper/UnknownExceptionMapper.java
@@ -5,16 +5,23 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.pennant.backend.model.WSReturnStatus;
+import com.pennanttech.pennapps.core.resource.Literal;
 
 @Provider
 public class UnknownExceptionMapper implements ExceptionMapper<Exception> {
+	private static final Logger logger = LogManager.getLogger(ExceptionMapperImpl.class);
 
 	@Override
 	public Response toResponse(Exception exception) {
+		logger.error(Literal.EXCEPTION, exception);
 		WSReturnStatus returnStatus = new WSReturnStatus();
-		returnStatus.setReturnCode("9999");
-		returnStatus.setReturnText("Unable to process request");
+		returnStatus.setReturnCode("900");
+		returnStatus.setReturnText(
+				"Unable to process the request. Please try again later or contact the system administrator.");
 		ResponseBuilder builder = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
 		builder.type("application/json");
 		builder.entity(returnStatus);
