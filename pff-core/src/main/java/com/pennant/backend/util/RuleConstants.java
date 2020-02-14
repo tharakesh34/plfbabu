a6ef6f5df40ddf55011b8b0aa3f26c950bd24bef
meena.a <meena.a@pennanttech.com>
2020-02-14 15:44:50 +0000

    get gst on rule
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@131223 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-core/src/main/java/com/pennant/backend/util/RuleConstants.java
+++ pff-core/src/main/java/com/pennant/backend/util/RuleConstants.java
@@ -149,6 +149,7 @@ public class RuleConstants {
 	public static final String CODE_SGST = "SGST"; // SGST
 	public static final String CODE_UGST = "UGST"; // UGST
 	public static final String CODE_TOTAL_GST = "TOTAL_GST"; // TOTAL GST
+	public static final String CODE_GST_PERCENTAGE = "CODE_GST_PERCENTAGE"; // TOTAL GST
 
 	public static final String CODE_TOTAL_AMOUNT_INCLUDINGGST = "TOTAL_IGST";
 	public static final String CODE_CESS = "CESS";
