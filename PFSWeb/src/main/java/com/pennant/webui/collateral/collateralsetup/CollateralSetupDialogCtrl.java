7df683251957c19dbbad63651c3ef492a6f72b13
satyanarayana.g <satyanarayana.g@pennanttech.com>
2020-02-22 10:19:16 +0000

    In the CO-Owner details, providing the details as non-bank customer and saving the record. then the CO-Owner added as non-bank customer is not visible in the approval screen.(GS)
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@132674 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/collateral/collateralsetup/CollateralSetupDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/collateral/collateralsetup/CollateralSetupDialogCtrl.java
@@ -1771,7 +1771,7 @@ public class CollateralSetupDialogCtrl extends GFCBaseCtrl<CollateralSetup> {
 				lc = new Listcell(coOwnerDetail.getCoOwnerCIF());
 				item.appendChild(lc);
 
-				lc = new Listcell(coOwnerDetail.getCoOwnerIDType());
+				lc = new Listcell(PennantApplicationUtil.getLabelDesc(coOwnerDetail.getCoOwnerIDType(), PennantAppUtil.getIdentityType()));
 				item.appendChild(lc);
 
 				lc = new Listcell(coOwnerDetail.getMobileNo());
