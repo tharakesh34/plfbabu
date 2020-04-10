0a0b4cb9c4a91fae23daafe4a84f3429fa6c545b
Sai Krishna P <saikrishna.p@pennanttech.com>
2020-04-10 04:07:40 +0000

    ST#68 Remove PLF Interface Project
    git-svn-id: http://192.168.110.32/svn/Products/PFF/git-migration/branches/mlod@137939 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/Interface/service/impl/ChequeVerifyInterfaceServiceImpl.java
+++ PFSJava/src/main/java/com/pennant/Interface/service/impl/ChequeVerifyInterfaceServiceImpl.java
@@ -1,11 +1,9 @@
 package com.pennant.Interface.service.impl;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.pennant.Interface.service.ChequeVerifyInterfaceService;
 import com.pennant.coreinterface.model.chequeverification.ChequeVerification;
-import com.pennant.coreinterface.process.ChequeVerificationProcess;
 import com.pennanttech.pennapps.core.InterfaceException;
 
 public class ChequeVerifyInterfaceServiceImpl implements ChequeVerifyInterfaceService {
@@ -15,8 +13,6 @@ public class ChequeVerifyInterfaceServiceImpl implements ChequeVerifyInterfaceSe
 	public ChequeVerifyInterfaceServiceImpl() {
 
 	}
-
-	private ChequeVerificationProcess chequeVerificationProcess;
 
 	/**
 	 * Method for sending cheque verification request to middleware
@@ -30,22 +26,6 @@ public class ChequeVerifyInterfaceServiceImpl implements ChequeVerifyInterfaceSe
 	public ChequeVerification verifySecurityCheque(ChequeVerification chequeVerification) throws InterfaceException {
 		logger.debug("Entering");
 		logger.debug("Leaving");
-		if (chequeVerificationProcess != null) {
-			return chequeVerificationProcess.sendChequeVerificationReq(chequeVerification);
-		}
 		return null;
 	}
-
-	// ******************************************************//
-	// ****************** getter / setter *******************//
-	// ******************************************************//
-
-	/*
-	 * public ChequeVerificationProcess getChequeVerificationProcess() { return chequeVerificationProcess; }
-	 */
-	@Autowired(required = false)
-	public void setChequeVerificationProcess(ChequeVerificationProcess chequeVerificationProcess) {
-		this.chequeVerificationProcess = chequeVerificationProcess;
-	}
-
 }
