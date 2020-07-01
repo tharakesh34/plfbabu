b5416caaf45978961f08803fd19ef205538765ee
manojkumar.p <manojkumar.p@pennanttech.com>
2020-07-01 15:52:27 +0000

    CET issues
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/mlod@143439 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-extension/src/main/java/com/pennanttech/pennapps/pff/service/spreadsheet/SpreadSheetService.java
+++ pff-extension/src/main/java/com/pennanttech/pennapps/pff/service/spreadsheet/SpreadSheetService.java
@@ -2,8 +2,9 @@ package com.pennanttech.pennapps.pff.service.spreadsheet;
 
 import java.util.Map;
 
+import com.pennant.backend.model.finance.CreditReviewDetails;
 import com.pennant.backend.model.finance.FinanceDetail;
 
 public interface SpreadSheetService {
-	Map<String, Object> prepareDataMap(FinanceDetail financeDetail);
+	Map<String, Object> prepareDataMap(FinanceDetail financeDetail, CreditReviewDetails creditReviewDetail);
 }
