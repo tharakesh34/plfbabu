90e05e018543e99a80d24ee5c75a8baa53c8d617
murthy.y <murthy.y@pennanttech.com>
2020-07-10 21:27:31 +0000

    Stop displaying Menus's based on the implementation constants.
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/mlod@144133 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/rmtmasters/financetype/FinanceTypeDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/rmtmasters/financetype/FinanceTypeDialogCtrl.java
@@ -632,10 +632,6 @@ public class FinanceTypeDialogCtrl extends GFCBaseCtrl<FinanceType> {
 	@Override
 	protected void doSetProperties() {
 		super.pageRightName = "FinanceTypeDialog";
-
-		this.grcAdvIntAndEMIReq.setVisible(ImplementationConstants.ALLOW_ADV_INT_EMI);
-		this.advIntAndEMIReq.setVisible(ImplementationConstants.ALLOW_ADV_INT_EMI);
-		this.dsfAndCashCtlReq.setVisible(ImplementationConstants.ALLOW_DSF_CASHCLT);
 	}
 
 	// Component Events
@@ -1089,6 +1085,13 @@ public class FinanceTypeDialogCtrl extends GFCBaseCtrl<FinanceType> {
 		getUserWorkspace().allocateAuthorities(super.pageRightName);
 		this.btnDelete.setVisible(getUserWorkspace().isAllowed("button_FinanceTypeDialog_btnDelete"));
 		this.btnSave.setVisible(getUserWorkspace().isAllowed("button_FinanceTypeDialog_btnSave"));
+		
+		if (!isOverdraft && !consumerDurable) {
+			this.grcAdvIntAndEMIReq.setVisible(ImplementationConstants.ALLOW_ADV_INT_EMI);
+			this.advIntAndEMIReq.setVisible(ImplementationConstants.ALLOW_ADV_INT_EMI);
+			this.dsfAndCashCtlReq.setVisible(ImplementationConstants.ALLOW_DSF_CASHCLT);
+		}
+		
 		logger.debug("Leaving");
 	}
 
