74bb37c1bfb284977c581e4719c8fd9fc96eec71
murthy.y <murthy.y@pennanttech.com>
2020-07-24 06:12:52 +0000

    format
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/mlod@144919 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/finance/financemain/FinCollateralDetailDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/finance/financemain/FinCollateralDetailDialogCtrl.java
@@ -93,8 +93,8 @@ import com.pennant.util.Constraint.PTStringValidator;
 import com.pennant.webui.util.GFCBaseCtrl;
 import com.pennanttech.pennapps.core.InterfaceException;
 import com.pennanttech.pennapps.core.model.ErrorDetail;
-import com.pennanttech.pennapps.web.util.MessageUtil;
 import com.pennanttech.pennapps.core.util.DateUtil.DateFormat;
+import com.pennanttech.pennapps.web.util.MessageUtil;
 
 /**
  * This is the controller class for the /WEB-INF/pages/Finance/FinCollateralDetailDialog.zul file.
