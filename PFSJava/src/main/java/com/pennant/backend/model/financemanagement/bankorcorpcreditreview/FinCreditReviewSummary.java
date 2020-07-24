99a922de2b22504400670739bc927a31f44190b7
murthy.y <murthy.y@pennanttech.com>
2020-07-24 04:50:05 +0000

    format
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/mlod@144893 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/model/financemanagement/bankorcorpcreditreview/FinCreditReviewSummary.java
+++ PFSJava/src/main/java/com/pennant/backend/model/financemanagement/bankorcorpcreditreview/FinCreditReviewSummary.java
@@ -1,6 +1,7 @@
 package com.pennant.backend.model.financemanagement.bankorcorpcreditreview;
 
 import java.math.BigDecimal;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
