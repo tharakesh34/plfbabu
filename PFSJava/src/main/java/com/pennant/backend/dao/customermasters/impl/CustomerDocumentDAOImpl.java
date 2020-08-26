51112311d69702e95bb7399012e2c4c7d5420efa
gopalaswamy.m <gopalaswamy.m@pennanttech.com>
2020-08-26 17:56:01 +0000

    ST#1236 PSD 158743 : Document disappearing issue in move loan stage API
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/mlod@146601 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/customermasters/impl/CustomerDocumentDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/customermasters/impl/CustomerDocumentDAOImpl.java
@@ -498,11 +498,11 @@ public class CustomerDocumentDAOImpl extends BasicDao<CustomerDocument> implemen
 		StringBuilder selectSql = new StringBuilder(
 				"Select CustID, CustDocCategory docCategory, 'CUSTOMER' categoryCode, ");
 		selectSql.append(" CustDocType DocType, CustDocName DocName, DocRefId, DocPurpose, DocUri,");
+		selectSql.append(" CustDocTitle, CustDocSysName, custDocIssuedCountry,");
 		if (type.contains("View")) {
-			selectSql.append(" lovDescCustDocCategory lovDescDocCategoryName, CustDocTitle, CustDocSysName,");
+			selectSql.append(" lovDescCustDocCategory lovDescDocCategoryName, ");
 			selectSql.append(" custDocRcvdOn, custDocExpDate, custDocIssuedOn, lovDescCustCIF, lovDescCustShrtName,");
-			selectSql.append(
-					" custDocIssuedCountry, lovDescCustDocIssuedCountry, custDocIsVerified, custDocVerifiedBy, custDocIsAcrive,");
+			selectSql.append(" lovDescCustDocIssuedCountry, custDocIsVerified, custDocVerifiedBy, custDocIsAcrive,");
 			selectSql.append(" DocExpDateIsMand,DocIssueDateMand,DocIdNumMand,");
 		}
 		selectSql.append(" Version, LastMntOn, LastMntBy, RecordStatus, RoleCode, NextRoleCode,");
