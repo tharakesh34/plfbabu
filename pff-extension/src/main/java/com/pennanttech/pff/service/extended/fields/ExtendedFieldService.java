bc8eb2e482294f961bfbd103a87a72afb46c6f15
gopal.p <gopal.p@pennanttech.com>
2021-03-28 11:17:16 +0000

    removed unused methods.
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-5.10.0@158479 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- pff-extension/src/main/java/com/pennanttech/pff/service/extended/fields/ExtendedFieldService.java
+++ pff-extension/src/main/java/com/pennanttech/pff/service/extended/fields/ExtendedFieldService.java
@@ -10,10 +10,6 @@ public interface ExtendedFieldService {
 
 	public void setExtendedFields(Object object, String moduleName, List<Map<String, Object>> details);
 
-	void update(String reference, int seqNo, Map<String, Object> mappedValues, String type, String tableName);
-
-	void save(Map<String, Object> mappedValues, String type, String tableName);
-
 	Map<String, Object> getExtendedField(String reference, String tableName, String type);
 
 	public void processExtendedFields(String tableName, Object extendedField);
