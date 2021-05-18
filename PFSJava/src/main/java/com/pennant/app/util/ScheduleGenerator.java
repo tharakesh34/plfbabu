5f032be218c9ca99d3593d71f3fa23ab42433c48
gireesh.g <gireesh.g@pennanttech.com>
2021-05-18 16:46:04 +0000

    PSD#172321:	while checking  Auto grace extension the interest which was claculating  f or Auto grace it was consider the defult  rate , not consider the rate whch was set for grace. PSD#172416:	"Grace period interest rate not getting updated, when increasing the grace period months, for eg. Initially for first 3 months grace period interest rate is 15%, after increasing grace period months say -3 under change gestation period. As interest rate should be reflected for 3 +3 =6 months. It is only getting reflected for first 3 months."
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-6.0.0@160974 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/app/util/ScheduleGenerator.java
+++ PFSJava/src/main/java/com/pennant/app/util/ScheduleGenerator.java
@@ -302,8 +302,8 @@ public class ScheduleGenerator {
 			curSchd.setTDSApplicable(TDSCalculator.isTDSApplicable(financeMain));
 
 			if (DateUtility.compare(curSchd.getSchDate(), financeMain.getGrcPeriodEndDate()) < 0) {
-				//Bugfix:Removed '=' condition to fix ROI Issue in Change Moratorium at GraceEnd 
-				if (DateUtility.compare(curSchd.getSchDate(), newSchdAfter) > 0) {
+				// Bugfix:Removed '=' condition to fix ROI Issue in Change Moratorium at GraceEnd
+				if (DateUtility.compare(curSchd.getSchDate(), newSchdAfter) >= 0) {
 					curSchd.setActRate(prvSchd.getActRate());
 					curSchd.setBaseRate(prvSchd.getBaseRate());
 					curSchd.setSplRate(prvSchd.getSplRate());
