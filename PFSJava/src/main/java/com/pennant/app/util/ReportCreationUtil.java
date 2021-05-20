5cfb8490f7c7beaf57a167addaaedc582bf936c3
murthy.y <murthy.y@pennanttech.com>
2021-05-20 19:57:10 +0000

    Re-factoring Reports module
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-6.0.0@161099 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/app/util/ReportCreationUtil.java
+++ PFSJava/src/main/java/com/pennant/app/util/ReportCreationUtil.java
@@ -2,10 +2,14 @@ package com.pennant.app.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -13,21 +17,26 @@ import org.apache.logging.log4j.Logger;
 import org.zkoss.util.media.AMedia;
 import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zkplus.spring.SpringUtil;
 import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.Window;
 
 import com.pennant.app.constants.CalculationConstants;
 import com.pennant.backend.model.finance.BulkProcessHeader;
+import com.pennant.backend.util.PennantConstants;
 import com.pennanttech.pennapps.core.AppException;
 import com.pennanttech.pennapps.core.resource.Literal;
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.fill.JRAbstractLRUVirtualizer;
+import net.sf.jasperreports.engine.fill.JRSwapFileVirtualizer;
+import net.sf.jasperreports.engine.util.JRSwapFile;
 import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 
 public class ReportCreationUtil {
@@ -89,14 +98,85 @@ public class ReportCreationUtil {
 
 		setDataSource(parameters, listData);
 
+		String jasperPrint = fillReportToFile(reportName, template, parameters, mainDS);
+
+		dowloadExcel(reportName, jasperPrint);
+
+		logger.info(Literal.LEAVING);
+	}
+
+	public static void downloadExcel(String reportPath, String reportName, String userName, String whereCond,
+			StringBuilder searchCriteriaDesc, Connection connection) {
+		logger.info(Literal.ENTERING);
+
+		Map<String, Object> parameters = getParameters(userName, reportName, whereCond, searchCriteriaDesc);
+
+		String jasperPrint = fillReportToFile(reportPath, reportName, parameters, connection);
+
+		dowloadExcel(reportName, jasperPrint);
+
+		logger.info(Literal.LEAVING);
+	}
+
+	public static String fillReportToFile(String reportPath, String reportName, Map<String, Object> parameters,
+			JRBeanCollectionDataSource mainDS) {
+
+		String template = getTemplate(reportPath, reportName);
+
+		int maxSize = 250;
+		JRSwapFile swapFile = new JRSwapFile(System.getProperty("java.io.tmpdir"), 250, 250);
+		JRAbstractLRUVirtualizer virtualizer = new JRSwapFileVirtualizer(maxSize, swapFile, true);
+		parameters.put(JRParameter.REPORT_VIRTUALIZER, virtualizer);
+
 		String printfileName = null;
 		try {
 			printfileName = JasperFillManager.fillReportToFile(template, parameters, mainDS);
+			virtualizer.setReadOnly(true);
 		} catch (JRException e) {
 			logger.error(Literal.EXCEPTION, e);
 			throw new AppException(String.format(RPT_EXCEPTION, reportName));
+		} finally {
+			if (virtualizer != null) {
+				virtualizer.cleanup();
+			}
 		}
 
+		return printfileName;
+	}
+
+	public static String fillReportToFile(String reportPath, String reportName, Map<String, Object> parameters,
+			Connection connection) {
+
+		String template = getTemplate(reportPath, reportName);
+
+		int maxSize = 250;
+		JRSwapFile swapFile = new JRSwapFile(System.getProperty("java.io.tmpdir"), 250, 250);
+		JRAbstractLRUVirtualizer virtualizer = new JRSwapFileVirtualizer(maxSize, swapFile, true);
+		parameters.put(JRParameter.REPORT_VIRTUALIZER, virtualizer);
+
+		String printfileName = null;
+		try {
+			printfileName = JasperFillManager.fillReportToFile(template, parameters, connection);
+			virtualizer.setReadOnly(true);
+		} catch (JRException e) {
+			logger.error(Literal.EXCEPTION, e);
+			throw new AppException(String.format(RPT_EXCEPTION, reportName));
+		} finally {
+			if (virtualizer != null) {
+				virtualizer.cleanup();
+			}
+		}
+
+		return printfileName;
+	}
+
+	public static void dowloadExcel(String reportName, String printfileName) {
+		byte[] excelData = getExcelData(reportName, printfileName);
+
+		Filedownload.save(new AMedia(reportName, "xls", "application/vnd.ms-excel", excelData));
+	}
+
+	public static byte[] getExcelData(String reportName, String printfileName) {
 		SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
 		configuration.setDetectCellType(true);
 		configuration.setWhitePageBackground(false);
@@ -106,22 +186,27 @@ public class ReportCreationUtil {
 		configuration.setIgnoreCellBorder(false);
 		configuration.setCollapseRowSpan(true);
 		configuration.setImageBorderFixEnabled(false);
+		configuration.setMaxRowsPerSheet(Integer.decode("200000"));
+		configuration.setMaxRowsPerSheet(Integer.decode("200000"));
+
+		return getExcelData(reportName, printfileName, configuration);
+	}
+
+	private static byte[] getExcelData(String reportName, String printfileName,
+			SimpleXlsReportConfiguration configuration) {
+		byte[] excelData = null;
 
 		JRXlsExporter excelExporter = new JRXlsExporter();
 		excelExporter.setExporterInput(new SimpleExporterInput(printfileName));
 		excelExporter.setConfiguration(configuration);
 
 		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-			excelExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
-			excelExporter.exportReport();
-			Filedownload.save(new AMedia(reportName, "xls", "application/vnd.ms-excel", outputStream.toByteArray()));
-
+			excelData = outputStream.toByteArray();
 		} catch (Exception e) {
 			logger.error(Literal.EXCEPTION, e);
 			throw new AppException(String.format(RPT_EXCEPTION, reportName));
 		}
-
-		logger.info(Literal.LEAVING);
+		return excelData;
 	}
 
 	public static String getTemplate(String reportName) {
@@ -156,6 +241,32 @@ public class ReportCreationUtil {
 				parameters.put("recalTypeSubParm", "T");
 			}
 		}
+
+		return parameters;
+	}
+
+	private static Map<String, Object> getParameters(String userName, String reportName) {
+		Map<String, Object> parameters = new HashMap<>();
+
+		parameters.put("userName", userName);
+		parameters.put("reportHeading", reportName);
+		parameters.put("reportGeneratedBy", Labels.getLabel("Reports_footer_ReportGeneratedBy.lable"));
+		parameters.put("appDate", SysParamUtil.getAppDate());
+		parameters.put("appCcy", SysParamUtil.getAppCurrency());
+		parameters.put("appccyEditField", SysParamUtil.getValueAsInt(PennantConstants.LOCAL_CCY_FORMAT));
+		parameters.put("unitParam", "Pff");
+		parameters.put("productLogo", PathUtil.getPath(PathUtil.REPORTS_IMAGE_PRODUCT));
+		parameters.put("bankName", Labels.getLabel("label_ClientName"));
+
+		return parameters;
+	}
+
+	private static Map<String, Object> getParameters(String userName, String reportName, String whereCond,
+			StringBuilder searchCriteriaDesc) {
+		Map<String, Object> parameters = getParameters(userName, reportName);
+
+		parameters.put("whereCondition", whereCond);
+		parameters.put("searchCriteria", searchCriteriaDesc.toString());
 
 		return parameters;
 	}
@@ -214,5 +325,21 @@ public class ReportCreationUtil {
 			}
 		}
 		return false;
+	}
+	
+	private static final String RPT_ERROR_1 = "Unable to get the connection from %s. Please contact the system administrator.";
+
+
+	public static Connection getConnection() {
+		return getConnection("dataSource");
+	}
+
+	public static Connection getConnection(String dataSourceName) {
+		DataSource dataSource = (DataSource) SpringUtil.getBean(dataSourceName);
+		try {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new AppException(String.format(RPT_ERROR_1, dataSourceName));
+		}
 	}
 }
--- PFSWeb/src/main/java/com/pennant/util/ReportGenerationUtil.java
+++ PFSWeb/src/main/java/com/pennant/util/ReportGenerationUtil.java
@@ -42,32 +42,21 @@
  */
 package com.pennant.util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.zkoss.spring.SpringUtil;
-import org.zkoss.util.media.AMedia;
-import org.zkoss.util.resource.Labels;
-import org.zkoss.zhtml.Filedownload;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zul.Window;
 
 import com.pennant.app.util.ErrorUtil;
 import com.pennant.app.util.PathUtil;
 import com.pennant.app.util.ReportCreationUtil;
-import com.pennant.app.util.SysParamUtil;
 import com.pennant.backend.util.PennantConstants;
 import com.pennant.webui.finance.enquiry.PostingsEnquiryDialogCtrl;
 import com.pennanttech.pennapps.core.model.ErrorDetail;
@@ -75,12 +64,8 @@ import com.pennanttech.pennapps.core.resource.Literal;
 import com.pennanttech.pennapps.web.util.MessageUtil;
 
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporterParameter;
-import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRXlsExporter;
-import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 
 public class ReportGenerationUtil implements Serializable {
 	private static final long serialVersionUID = 7293149519883033383L;
@@ -168,157 +153,12 @@ public class ReportGenerationUtil implements Serializable {
 
 	public static void generateReport(String userName, String reportName, String whereCond,
 			StringBuilder searchCriteriaDesc) {
-		logger.debug("Entering");
-		
-		
-		String reportSrc = ReportCreationUtil.getTemplate(PathUtil.REPORTS_ORGANIZATION, reportName);
-		
+		logger.info(Literal.ENTERING);
 
-		Connection connection = null;
-		DataSource dataSourceObj = null;
+		ReportCreationUtil.downloadExcel(PathUtil.REPORTS_ORGANIZATION, reportName, userName, whereCond,
+				searchCriteriaDesc, ReportCreationUtil.getConnection());
 
-		try {
-
-			dataSourceObj = (DataSource) SpringUtil.getBean("dataSource");
-			connection = dataSourceObj.getConnection();
-
-			Map<String, Object> reportArgumentsMap = new HashMap<String, Object>(5);
-			reportArgumentsMap.put("userName", userName);
-			reportArgumentsMap.put("reportHeading", reportName);
-			reportArgumentsMap.put("reportGeneratedBy", Labels.getLabel("Reports_footer_ReportGeneratedBy.lable"));
-			reportArgumentsMap.put("appDate", SysParamUtil.getAppDate());
-			reportArgumentsMap.put("appCcy", SysParamUtil.getAppCurrency());
-			reportArgumentsMap.put("appccyEditField", SysParamUtil.getValueAsInt(PennantConstants.LOCAL_CCY_FORMAT));
-			reportArgumentsMap.put("unitParam", "Pff");
-			reportArgumentsMap.put("whereCondition", whereCond);
-			reportArgumentsMap.put("organizationLogo", PathUtil.getPath(PathUtil.REPORTS_IMAGE_CLIENT));
-			reportArgumentsMap.put("productLogo", PathUtil.getPath(PathUtil.REPORTS_IMAGE_PRODUCT));
-			reportArgumentsMap.put("bankName", Labels.getLabel("label_ClientName"));
-			reportArgumentsMap.put("searchCriteria", searchCriteriaDesc.toString());
-
-			Connection con = null;
-			DataSource reportDataSourceObj = null;
-
-			try {
-
-				reportDataSourceObj = (DataSource) SpringUtil.getBean("dataSource");
-				con = reportDataSourceObj.getConnection();
-
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				String printfileName = JasperFillManager.fillReportToFile(reportSrc, reportArgumentsMap, con);
-				JRXlsExporter excelExporter = new JRXlsExporter();
-				excelExporter.setParameter(JRExporterParameter.INPUT_FILE_NAME, printfileName);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_COLUMNS, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_IGNORE_GRAPHICS, Boolean.FALSE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_IGNORE_CELL_BORDER, Boolean.FALSE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_COLLAPSE_ROW_SPAN, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_IMAGE_BORDER_FIX_ENABLED, Boolean.FALSE);
-				excelExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, outputStream);
-				excelExporter.exportReport();
-				Filedownload
-						.save(new AMedia(reportName, "xls", "application/vnd.ms-excel", outputStream.toByteArray()));
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		} catch (SQLException e1) {
-			logger.error(e1.getMessage());
-		} finally {
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					logger.error(e.getMessage());
-				}
-			}
-			connection = null;
-			dataSourceObj = null;
-		}
-
-		logger.debug("Leaving");
+		logger.info(Literal.LEAVING);
 	}
 
-	// #PSD:152141 UAT2: Users:Report: Indaas accounting report not available --END
-	public static void generateReport(String userName, String reportName, String whereCond,
-			StringBuilder searchCriteriaDesc, Window window, boolean createExcel, String fromDate) {
-		logger.debug("Entering");
-
-		Connection connection = null;
-		DataSource dataSourceObj = null;
-
-		try {
-
-			dataSourceObj = (DataSource) SpringUtil.getBean("dataSource");
-			connection = dataSourceObj.getConnection();
-
-			HashMap<String, Object> reportArgumentsMap = new HashMap<String, Object>(5);
-			reportArgumentsMap.put("userName", userName);
-			reportArgumentsMap.put("reportHeading", reportName);
-			reportArgumentsMap.put("reportGeneratedBy", Labels.getLabel("Reports_footer_ReportGeneratedBy.lable"));
-			reportArgumentsMap.put("appDate", SysParamUtil.getAppDate());
-			reportArgumentsMap.put("appCcy", SysParamUtil.getValueAsString("APP_DFT_CURR"));
-			reportArgumentsMap.put("appccyEditField", SysParamUtil.getValueAsInt(PennantConstants.LOCAL_CCY_FORMAT));
-			reportArgumentsMap.put("unitParam", "Pff");
-			reportArgumentsMap.put("whereCondition", whereCond);
-			reportArgumentsMap.put("organizationLogo", PathUtil.getPath(PathUtil.REPORTS_IMAGE_CLIENT));
-			reportArgumentsMap.put("productLogo", PathUtil.getPath(PathUtil.REPORTS_IMAGE_PRODUCT));
-			reportArgumentsMap.put("bankName", Labels.getLabel("label_ClientName"));
-			reportArgumentsMap.put("searchCriteria", searchCriteriaDesc.toString());
-			reportArgumentsMap.put("fromDate", fromDate);
-
-			String reportSrc = PathUtil.getPath(PathUtil.REPORTS_ORGANIZATION) + "/" + reportName + ".jasper";
-
-			Connection con = null;
-			DataSource reportDataSourceObj = null;
-
-			try {
-				File file = new File(reportSrc);
-				if (!file.exists()) {
-					MessageUtil.showError(
-							String.format("%s report not configured, please contact system administrator", reportName));
-					return;
-				}
-
-				logger.debug("Buffer started");
-
-				reportDataSourceObj = (DataSource) SpringUtil.getBean("dataSource");
-				con = reportDataSourceObj.getConnection();
-
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				String printfileName = JasperFillManager.fillReportToFile(reportSrc, reportArgumentsMap, con);
-				JRXlsExporter excelExporter = new JRXlsExporter();
-				excelExporter.setParameter(JRExporterParameter.INPUT_FILE_NAME, printfileName);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_COLUMNS, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_IGNORE_GRAPHICS, Boolean.FALSE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_IGNORE_CELL_BORDER, Boolean.FALSE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_COLLAPSE_ROW_SPAN, Boolean.TRUE);
-				excelExporter.setParameter(JRXlsExporterParameter.IS_IMAGE_BORDER_FIX_ENABLED, Boolean.FALSE);
-				excelExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, outputStream);
-				excelExporter.exportReport();
-				Filedownload
-						.save(new AMedia(reportName, "xls", "application/vnd.ms-excel", outputStream.toByteArray()));
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		} catch (SQLException e1) {
-			logger.error(e1.getMessage());
-		} finally {
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					logger.error(e.getMessage());
-				}
-			}
-			connection = null;
-			dataSourceObj = null;
-		}
-
-		logger.debug("Leaving");
-	}
 }
