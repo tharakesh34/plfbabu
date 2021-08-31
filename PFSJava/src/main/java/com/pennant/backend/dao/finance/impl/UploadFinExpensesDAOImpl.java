7461a39408df00c3ec6369f4246c11e3dd12f671
gireesh.g <gireesh.g@pennanttech.com>
2021-08-31 14:45:26 +0000

    ST#2367- FINID changes
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-6.0.0_FinID@164962 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/finance/impl/UploadFinExpensesDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/finance/impl/UploadFinExpensesDAOImpl.java
@@ -1,58 +1,43 @@
 /**
  * Copyright 2011 - Pennant Technologies
  * 
- * This file is part of Pennant Java Application Framework and related Products. 
- * All components/modules/functions/classes/logic in this software, unless 
- * otherwise stated, the property of Pennant Technologies. 
+ * This file is part of Pennant Java Application Framework and related Products. All
+ * components/modules/functions/classes/logic in this software, unless otherwise stated, the property of Pennant
+ * Technologies.
  * 
- * Copyright and other intellectual property laws protect these materials. 
- * Reproduction or retransmission of the materials, in whole or in part, in any manner, 
- * without the prior written consent of the copyright holder, is a violation of 
- * copyright law.
+ * Copyright and other intellectual property laws protect these materials. Reproduction or retransmission of the
+ * materials, in whole or in part, in any manner, without the prior written consent of the copyright holder, is a
+ * violation of copyright law.
  */
 
 /**
  ********************************************************************************************
- *                                 FILE HEADER                                              *
+ * FILE HEADER *
  ********************************************************************************************
- *																							*
- * FileName    		:  UploadFinExpensesDAOImpl.java                                        * 	  
- *                                                                    						*
- * Author      		:  PENNANT TECHONOLOGIES              									*
- *                                                                  						*
- * Creation Date    :  17-12-2017    														*
- *                                                                  						*
- * Modified Date    :  17-12-2017    														*
- *                                                                  						*
- * Description 		:                                             							*
- *                                                                                          *
+ * * FileName : UploadFinExpensesDAOImpl.java * * Author : PENNANT TECHONOLOGIES * * Creation Date : 17-12-2017 * *
+ * Modified Date : 17-12-2017 * * Description : * *
  ********************************************************************************************
- * Date             Author                   Version      Comments                          *
+ * Date Author Version Comments *
  ********************************************************************************************
- * 14-08-2013       Pennant	                 0.1                                            * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
+ * 14-08-2013 Pennant 0.1 * * * * * * * * *
  ********************************************************************************************
-*/
+ */
 
 package com.pennant.backend.dao.finance.impl;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 
 import com.pennant.backend.dao.finance.UploadFinExpensesDAO;
 import com.pennant.backend.model.expenses.UploadFinExpenses;
 import com.pennanttech.pennapps.core.jdbc.BasicDao;
+import com.pennanttech.pennapps.core.jdbc.JdbcUtil;
+import com.pennanttech.pennapps.core.resource.Literal;
 
 /**
  * DAO methods implementation for the <b>UploadFinExpenses model</b> class.<br>
@@ -65,27 +50,42 @@ public class UploadFinExpensesDAOImpl extends BasicDao<UploadFinExpenses> implem
 		super();
 	}
 
-	/**
-	 * Method for saving Fee schedule Details list
-	 */
 	@Override
 	public void saveUploadFinExpenses(List<UploadFinExpenses> uploadFinExpensesList) {
-		logger.debug("Entering");
+		StringBuilder sql = new StringBuilder("Insert Into UploadFinExpenses");
+		sql.append(" (UploadId, FinType, FinID, FinReference, FinApprovalStartDate, FinApprovalEndDate");
+		sql.append(", ExpenseTypeCode, Percentage, AmountValue, Type, Status, Reason)");
+		sql.append(" Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-		StringBuilder insertSql = new StringBuilder();
-		insertSql.append(" INSERT INTO UploadFinExpenses");
-		insertSql.append(
-				" (UploadId, FinType, FinReference, FinApprovalStartDate, FinApprovalEndDate, ExpenseTypeCode, Percentage, AmountValue, Type, Status, Reason) ");
-		insertSql.append(
-				" VALUES (:UploadId, :FinType, :FinReference, :FinApprovalStartDate, :FinApprovalEndDate, :ExpenseTypeCode, :Percentage, :AmountValue, :Type, :Status, :Reason)");
+		logger.debug(Literal.SQL + sql.toString());
 
-		logger.debug("insertSql: " + insertSql.toString());
+		this.jdbcOperations.batchUpdate(sql.toString(), new BatchPreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				UploadFinExpenses ufe = uploadFinExpensesList.get(i);
 
-		SqlParameterSource[] beanParameters = SqlParameterSourceUtils.createBatch(uploadFinExpensesList.toArray());
+				int index = 1;
 
-		this.jdbcTemplate.batchUpdate(insertSql.toString(), beanParameters);
+				ps.setLong(index++, ufe.getUploadId());
+				ps.setString(index++, ufe.getFinType());
+				ps.setLong(index++, ufe.getFinID());
+				ps.setString(index++, ufe.getFinReference());
+				ps.setDate(index++, JdbcUtil.getDate(ufe.getFinApprovalStartDate()));
+				ps.setDate(index++, JdbcUtil.getDate(ufe.getFinApprovalEndDate()));
+				ps.setString(index++, ufe.getExpenseTypeCode());
+				ps.setBigDecimal(index++, ufe.getPercentage());
+				ps.setBigDecimal(index++, ufe.getAmountValue());
+				ps.setString(index++, ufe.getType());
+				ps.setString(index++, ufe.getStatus());
+				ps.setString(index++, ufe.getReason());
 
-		logger.debug("Leaving");
+			}
+
+			@Override
+			public int getBatchSize() {
+				return uploadFinExpensesList.size();
+			}
+		});
 	}
 
 }
