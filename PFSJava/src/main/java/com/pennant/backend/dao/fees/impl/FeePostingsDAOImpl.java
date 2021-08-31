dd1447dea31613bc8ec7ec7aefc6a9f72d9f39bf
gireesh.g <gireesh.g@pennanttech.com>
2021-08-31 14:36:40 +0000

    ST#2367- FINID changes
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-6.0.0_FinID@164955 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/backend/dao/fees/impl/FeePostingsDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/fees/impl/FeePostingsDAOImpl.java
@@ -1,45 +1,27 @@
 /**
  * Copyright 2011 - Pennant Technologies
  * 
- * This file is part of Pennant Java Application Framework and related Products. 
- * All components/modules/functions/classes/logic in this software, unless 
- * otherwise stated, the property of Pennant Technologies. 
+ * This file is part of Pennant Java Application Framework and related Products. All
+ * components/modules/functions/classes/logic in this software, unless otherwise stated, the property of Pennant
+ * Technologies.
  * 
- * Copyright and other intellectual property laws protect these materials. 
- * Reproduction or retransmission of the materials, in whole or in part, in any manner, 
- * without the prior written consent of the copyright holder, is a violation of 
- * copyright law.
+ * Copyright and other intellectual property laws protect these materials. Reproduction or retransmission of the
+ * materials, in whole or in part, in any manner, without the prior written consent of the copyright holder, is a
+ * violation of copyright law.
  */
 
 /**
  ********************************************************************************************
- *                                 FILE HEADER                                              *
+ * FILE HEADER *
  ********************************************************************************************
- *																							*
- * FileName    		:  VASRecordingDAOImpl.java                                                   * 	  
- *                                                                    						*
- * Author      		:  PENNANT TECHONOLOGIES              									*
- *                                                                  						*
- * Creation Date    :  02-12-2016    														*
- *                                                                  						*
- * Modified Date    :  02-12-2016    														*
- *                                                                  						*
- * Description 		:                                             							*
- *                                                                                          *
+ * * FileName : VASRecordingDAOImpl.java * * Author : PENNANT TECHONOLOGIES * * Creation Date : 02-12-2016 * * Modified
+ * Date : 02-12-2016 * * Description : * *
  ********************************************************************************************
- * Date             Author                   Version      Comments                          *
+ * Date Author Version Comments *
  ********************************************************************************************
- * 02-12-2016       PENNANT	                 0.1                                            * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
+ * 02-12-2016 PENNANT 0.1 * * * * * * * * *
  ********************************************************************************************
-*/
+ */
 package com.pennant.backend.dao.fees.impl;
 
 import org.apache.commons.lang.StringUtils;
@@ -47,17 +29,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.pennant.backend.dao.fees.FeePostingsDAO;
 import com.pennant.backend.model.fees.FeePostings;
 import com.pennanttech.pennapps.core.ConcurrencyException;
 import com.pennanttech.pennapps.core.DependencyFoundException;
+import com.pennanttech.pennapps.core.jdbc.JdbcUtil;
 import com.pennanttech.pennapps.core.jdbc.SequenceDao;
+import com.pennanttech.pennapps.core.resource.Literal;
 
 /**
  * DAO methods implementation for the <b>VASRecording model</b> class.<br>
@@ -69,162 +48,210 @@ public class FeePostingsDAOImpl extends SequenceDao<FeePostings> implements FeeP
 
 	@Override
 	public FeePostings getFeePostings() {
-		logger.debug("Entering");
 		FeePostings feePostings = new FeePostings();
-		logger.debug("Leaving");
+
 		return feePostings;
 	}
 
 	@Override
 	public FeePostings getNewFeePostings() {
-		logger.debug("Entering");
 		FeePostings feePostings = new FeePostings();
+
 		feePostings.setNewRecord(true);
-		logger.debug("Leaving");
+
 		return feePostings;
 	}
 
 	@Override
 	public FeePostings getFeePostingsById(long postId, String type) {
-		logger.debug("Entering");
+		StringBuilder sql = new StringBuilder("Select");
+		sql.append(" PostId, PostAgainst, Reference, FeeTyeCode, PostingAmount, PostDate, ValueDate");
+		sql.append(", Remarks, PartnerBankId, PostingDivision, Version, LastMntBy, LastMntOn");
+		sql.append(", RecordStatus, RoleCode, NextRoleCode, TaskId, NextTaskId, RecordType, WorkflowId");
 
-		MapSqlParameterSource source = null;
-		StringBuilder sql = null;
-
-		sql = new StringBuilder(
-				"Select PostId, PostAgainst, Reference, FeeTyeCode, PostingAmount, PostDate, ValueDate,Remarks,PartnerBankId,PostingDivision,");
-		sql.append(
-				" Version , LastMntBy, LastMntOn,RecordStatus, RoleCode, NextRoleCode, TaskId, NextTaskId, RecordType, WorkflowId");
 		if (StringUtils.trimToEmpty(type).contains("View")) {
-			sql.append(", partnerBankName,partnerBankAc,partnerBankAcType,accountSetId,DivisionCodeDesc");
+			sql.append(", PartnerBankName, PartnerBankAc, PartnerBankAcType, AccountSetId, DivisionCodeDesc");
 		}
+
 		sql.append(" From FeePostings");
 		sql.append(StringUtils.trimToEmpty(type));
-		sql.append(" Where PostId =:PostId");
-		logger.debug("selectSql: " + sql.toString());
+		sql.append(" Where PostId = ?");
 
-		RowMapper<FeePostings> typeRowMapper = BeanPropertyRowMapper.newInstance(FeePostings.class);
+		logger.debug(Literal.SQL + sql.toString());
 
-		source = new MapSqlParameterSource();
-		source.addValue("PostId", postId);
 		try {
-			return this.jdbcTemplate.queryForObject(sql.toString(), source, typeRowMapper);
+			return this.jdbcOperations.queryForObject(sql.toString(), (rs, rowNum) -> {
+				FeePostings fp = new FeePostings();
+
+				fp.setPostId(rs.getLong("PostId"));
+				fp.setPostAgainst(rs.getString("PostAgainst"));
+				fp.setReference(rs.getString("Reference"));
+				fp.setFeeTyeCode(rs.getString("FeeTyeCode"));
+				fp.setPostingAmount(rs.getBigDecimal("PostingAmount"));
+				fp.setPostDate(rs.getTimestamp("PostDate"));
+				fp.setValueDate(rs.getTimestamp("ValueDate"));
+				fp.setRemarks(rs.getString("Remarks"));
+				fp.setPartnerBankId(rs.getLong("PartnerBankId"));
+				fp.setPostingDivision(rs.getString("PostingDivision"));
+				fp.setVersion(rs.getInt("Version"));
+				fp.setLastMntBy(rs.getLong("LastMntBy"));
+				fp.setLastMntOn(rs.getTimestamp("LastMntOn"));
+				fp.setRecordStatus(rs.getString("RecordStatus"));
+				fp.setRoleCode(rs.getString("RoleCode"));
+				fp.setNextRoleCode(rs.getString("NextRoleCode"));
+				fp.setTaskId(rs.getString("TaskId"));
+				fp.setNextTaskId(rs.getString("NextTaskId"));
+				fp.setRecordType(rs.getString("RecordType"));
+				fp.setWorkflowId(rs.getLong("WorkflowId"));
+
+				if (StringUtils.trimToEmpty(type).contains("View")) {
+					fp.setPartnerBankName(rs.getString("PartnerBankName"));
+					fp.setPartnerBankAc(rs.getString("PartnerBankAc"));
+					fp.setPartnerBankAcType(rs.getString("PartnerBankAcType"));
+					fp.setAccountSetId(rs.getString("AccountSetId"));
+					fp.setDivisionCodeDesc(rs.getString("DivisionCodeDesc"));
+				}
+
+				return fp;
+			}, postId);
 		} catch (EmptyResultDataAccessException e) {
-			logger.warn("Exception: ", e);
-		} finally {
-			source = null;
-			sql = null;
+			//
 		}
-		logger.debug("Leaving");
+
 		return null;
 	}
 
 	@Override
-	public void save(FeePostings feePostings, String type) {
-
-		logger.debug("Entering");
-		if (feePostings.getId() == Long.MIN_VALUE) {
-			feePostings.setId(getNextValue("SeqFeePostings"));
+	public void save(FeePostings fp, String type) {
+		if (fp.getId() == Long.MIN_VALUE) {
+			fp.setId(getNextValue("SeqFeePostings"));
 		}
 
-		StringBuilder insertSql = new StringBuilder();
-		insertSql.append("Insert Into FeePostings");
-		insertSql.append(StringUtils.trimToEmpty(type));
-		insertSql.append(
-				" (PostId, PostAgainst, Reference, FeeTyeCode, PostingAmount, PostDate, ValueDate,Remarks,PartnerBankId,PostingDivision,");
-		insertSql.append("  Version , LastMntBy, LastMntOn, RecordStatus, RoleCode, NextRoleCode,");
-		insertSql.append("	TaskId, NextTaskId, RecordType, WorkflowId)");
-		insertSql.append(
-				"  Values(:PostId, :PostAgainst, :Reference, :FeeTyeCode, :PostingAmount, :PostDate, :ValueDate,:Remarks,:PartnerBankId,:PostingDivision,");
-		insertSql.append(
-				"  :Version , :LastMntBy, :LastMntOn, :RecordStatus, :RoleCode, :NextRoleCode, :TaskId, :NextTaskId, :RecordType, :WorkflowId)");
+		StringBuilder sql = new StringBuilder("Insert Into FeePostings");
+		sql.append(StringUtils.trimToEmpty(type));
+		sql.append(" (PostId, PostAgainst, Reference, FeeTyeCode, PostingAmount, PostDate, ValueDate");
+		sql.append(", Remarks, PartnerBankId, PostingDivision, Version, LastMntBy, LastMntOn, RecordStatus");
+		sql.append(", RoleCode, NextRoleCode, TaskId, NextTaskId, RecordType, WorkflowId)");
+		sql.append(" Values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-		logger.debug("insertSql: " + insertSql.toString());
+		logger.debug(Literal.SQL + sql.toString());
 
-		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(feePostings);
-		this.jdbcTemplate.update(insertSql.toString(), beanParameters);
-		logger.debug("Leaving");
+		this.jdbcOperations.update(sql.toString(), ps -> {
+			int index = 1;
 
+			ps.setLong(index++, fp.getPostId());
+			ps.setString(index++, fp.getPostAgainst());
+			ps.setString(index++, fp.getReference());
+			ps.setString(index++, fp.getFeeTyeCode());
+			ps.setBigDecimal(index++, fp.getPostingAmount());
+			ps.setDate(index++, JdbcUtil.getDate(fp.getPostDate()));
+			ps.setDate(index++, JdbcUtil.getDate(fp.getValueDate()));
+			ps.setString(index++, fp.getRemarks());
+			ps.setLong(index++, fp.getPartnerBankId());
+			ps.setString(index++, fp.getPostingDivision());
+			ps.setInt(index++, fp.getVersion());
+			ps.setLong(index++, fp.getLastMntBy());
+			ps.setTimestamp(index++, fp.getLastMntOn());
+			ps.setString(index++, fp.getRecordStatus());
+			ps.setString(index++, fp.getRoleCode());
+			ps.setString(index++, fp.getNextRoleCode());
+			ps.setString(index++, fp.getTaskId());
+			ps.setString(index++, fp.getNextTaskId());
+			ps.setString(index++, fp.getRecordType());
+			ps.setLong(index++, fp.getWorkflowId());
+		});
 	}
 
 	@Override
-	public void update(FeePostings feePostings, String type) {
-		int recordCount = 0;
-		logger.debug("Entering");
-		StringBuilder updateSql = new StringBuilder("Update FeePostings");
-		updateSql.append(StringUtils.trimToEmpty(type));
-		updateSql.append(
-				" Set PostId = :PostId, PostAgainst = :PostAgainst, Reference = :Reference, FeeTyeCode = :FeeTyeCode, PostingAmount = :PostingAmount, PostDate = :PostDate, ");
-		updateSql.append(
-				" ValueDate = :ValueDate, Remarks = :Remarks, PartnerBankId = :PartnerBankId, PostingDivision =:PostingDivision,");
-		updateSql.append(
-				" Version= :Version, LastMntBy = :LastMntBy, LastMntOn = :LastMntOn, RecordStatus= :RecordStatus, RoleCode = :RoleCode, NextRoleCode = :NextRoleCode, TaskId = :TaskId, NextTaskId = :NextTaskId,");
-		updateSql.append(" RecordType = :RecordType, WorkflowId = :WorkflowId");
-		updateSql.append(" Where PostId= :PostId");
+	public void update(FeePostings fp, String type) {
+		StringBuilder sql = new StringBuilder("Update FeePostings");
+		sql.append(StringUtils.trimToEmpty(type));
+		sql.append(" Set PostId = ?, PostAgainst = ?, Reference = ?, FeeTyeCode = ?, PostingAmount = ?, PostDate = ?");
+		sql.append(", ValueDate = ?, Remarks = ?, PartnerBankId = ?, PostingDivision = ?");
+		sql.append(", Version = ?, LastMntBy = ?, LastMntOn = ?, RecordStatus = ?, RoleCode = ?, NextRoleCode = ?");
+		sql.append(", TaskId = ?, NextTaskId = ?, RecordType = ?, WorkflowId = ?");
+		sql.append(" Where PostId= ?");
 
 		if (!type.endsWith("_Temp")) {
-			updateSql.append("  AND Version= :Version-1");
+			sql.append(" and Version = ?");
 		}
-		logger.debug("updateSql: " + updateSql.toString());
 
-		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(feePostings);
-		recordCount = this.jdbcTemplate.update(updateSql.toString(), beanParameters);
+		logger.debug(Literal.SQL + sql.toString());
+
+		int recordCount = this.jdbcOperations.update(sql.toString(), ps -> {
+			int index = 1;
+
+			ps.setLong(index++, fp.getPostId());
+			ps.setString(index++, fp.getPostAgainst());
+			ps.setString(index++, fp.getReference());
+			ps.setString(index++, fp.getFeeTyeCode());
+			ps.setBigDecimal(index++, fp.getPostingAmount());
+			ps.setDate(index++, JdbcUtil.getDate(fp.getPostDate()));
+			ps.setDate(index++, JdbcUtil.getDate(fp.getValueDate()));
+			ps.setString(index++, fp.getRemarks());
+			ps.setLong(index++, fp.getPartnerBankId());
+			ps.setString(index++, fp.getPostingDivision());
+			ps.setInt(index++, fp.getVersion());
+			ps.setLong(index++, fp.getLastMntBy());
+			ps.setTimestamp(index++, fp.getLastMntOn());
+			ps.setString(index++, fp.getRecordStatus());
+			ps.setString(index++, fp.getRoleCode());
+			ps.setString(index++, fp.getNextRoleCode());
+			ps.setString(index++, fp.getTaskId());
+			ps.setString(index++, fp.getNextTaskId());
+			ps.setString(index++, fp.getRecordType());
+			ps.setLong(index++, fp.getWorkflowId());
+
+			if (!type.endsWith("_Temp")) {
+				ps.setInt(index++, fp.getVersion() - 1);
+			}
+
+		});
 
 		if (recordCount <= 0) {
 			throw new ConcurrencyException();
 		}
-		logger.debug("Leaving");
 	}
 
 	@Override
-	public void delete(FeePostings feePostings, String type) {
-		logger.debug("Entering");
-		int recordCount = 0;
+	public void delete(FeePostings fp, String type) {
+		StringBuilder sql = new StringBuilder("Delete From FeePostings");
+		sql.append(StringUtils.trimToEmpty(type));
+		sql.append(" Where PostId = ?");
 
-		StringBuilder deleteSql = new StringBuilder("Delete From feePostings");
-		deleteSql.append(StringUtils.trimToEmpty(type));
-		deleteSql.append(" Where PostId =:PostId");
+		logger.debug(Literal.SQL + sql.toString());
 
-		logger.debug("deleteSql: " + deleteSql.toString());
-
-		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(feePostings);
 		try {
-			recordCount = this.jdbcTemplate.update(deleteSql.toString(), beanParameters);
+			int recordCount = this.jdbcOperations.update(sql.toString(), ps -> {
+				int index = 1;
+
+				ps.setLong(index++, fp.getPostId());
+			});
+
 			if (recordCount <= 0) {
 				throw new ConcurrencyException();
 			}
+
 		} catch (DataAccessException e) {
 			throw new DependencyFoundException(e);
 		}
-		logger.debug("Leaving");
 	}
 
-	/**
-	 * Method for Fetching Count for Assigned partnerBankId to Different Finances/Commitments
-	 */
 	@Override
 	public int getAssignedPartnerBankCount(long partnerBankId, String type) {
-		logger.debug("Entering");
+		StringBuilder sql = new StringBuilder("Select Count(PartnerBankId)");
+		sql.append(" From FeePostings");
+		sql.append(StringUtils.trimToEmpty(type));
+		sql.append(" Where PartnerBankId = ?");
 
-		int assignedCount = 0;
-		MapSqlParameterSource source = new MapSqlParameterSource();
-		source.addValue("PartnerBankId", partnerBankId);
-
-		StringBuilder selectSql = new StringBuilder(" Select Count(1) ");
-		selectSql.append(" From FeePostings");
-		selectSql.append(StringUtils.trimToEmpty(type));
-		selectSql.append(" Where PartnerBankId = :PartnerBankId ");
-
-		logger.debug("selectSql: " + selectSql.toString());
+		logger.debug(Literal.SQL + sql.toString());
 
 		try {
-			assignedCount = this.jdbcTemplate.queryForObject(selectSql.toString(), source, Integer.class);
+			return this.jdbcOperations.queryForObject(sql.toString(), Integer.class, partnerBankId);
 		} catch (EmptyResultDataAccessException e) {
-			logger.info(e);
-			assignedCount = 0;
+			//
 		}
-		logger.debug("Leaving");
-		return assignedCount;
-	}
 
+		return 0;
+	}
 }
