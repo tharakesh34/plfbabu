f8ddd74dd6ee23c89c06b9df783be5a30ace2b60
gireesh.g <gireesh.g@pennanttech.com>
2021-10-21 09:54:48 +0000

    Issues Addressed while doing SIT
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-6.0.0_FinID@166899 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSJava/src/main/java/com/pennant/app/util/ScheduleGenerator.java
+++ PFSJava/src/main/java/com/pennant/app/util/ScheduleGenerator.java
@@ -415,29 +415,6 @@ public class ScheduleGenerator {
 
 		}
 
-		// SATYA : Disbursement Details from Grace End onwards
-		for (FinanceDisbursement disbursementDetail : finScheduleData.getDisbursementDetails()) {
-
-			Date disbDate = formatDate(disbursementDetail.getDisbDate());
-
-			if (disbDate.compareTo(financeMain.getGrcPeriodEndDate()) < 0) {
-				continue;
-			}
-
-			BigDecimal prvTermDisbAmount = BigDecimal.ZERO;
-			FinanceScheduleDetail schedule = new FinanceScheduleDetail();
-
-			if (finScheduleData.getScheduleMap() != null && finScheduleData.getScheduleMap().containsKey(disbDate)) {
-
-				schedule = finScheduleData.getScheduleMap().get(disbDate);
-				prvTermDisbAmount = schedule.getDisbAmount();
-			}
-
-			schedule.setDisbOnSchDate(true);
-			schedule.setFeeChargeAmt(disbursementDetail.getFeeChargeAmt());
-			schedule.setDisbAmount(disbursementDetail.getDisbAmount().add(prvTermDisbAmount));
-		}
-
 		finScheduleData.getScheduleMap().clear();
 		financeMain.setSkipRateReset(true);
 
