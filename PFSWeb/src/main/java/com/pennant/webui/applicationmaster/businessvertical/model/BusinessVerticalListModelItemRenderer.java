f133b4fec93b18d7f16b4431bd509b3d1c9778cc
murthy.y <murthy.y@pennanttech.com>
2021-10-25 06:30:19 +0000

    changes merged from Core MLOD @r166959
    git-svn-id: http://192.168.110.32/svn/pennapps/pff/root/branches/PLF-Release-6.0.0_FinID@166993 6963f9ea-81fa-44da-aa10-7ebde50aa541

--- PFSWeb/src/main/java/com/pennant/webui/applicationmaster/businessvertical/model/BusinessVerticalListModelItemRenderer.java
+++ PFSWeb/src/main/java/com/pennant/webui/applicationmaster/businessvertical/model/BusinessVerticalListModelItemRenderer.java
@@ -1,45 +1,27 @@
 /**
  * Copyright 2011 - Pennant Technologies
  * 
- * This file is part of Pennant Java Application Framework and related Products. 
- * All components/modules/functions/classes/logic in this software, unless 
- * otherwise stated, the property of Pennant Technologies. 
+ * This file is part of Pennant Java Application Framework and related Products. All
+ * components/modules/functions/classes/logic in this software, unless otherwise stated, the property of Pennant
+ * Technologies.
  * 
- * Copyright and other intellectual property laws protect these materials. 
- * Reproduction or retransmission of the materials, in whole or in part, in any manner, 
- * without the prior written consent of the copyright holder, is a violation of 
- * copyright law.
+ * Copyright and other intellectual property laws protect these materials. Reproduction or retransmission of the
+ * materials, in whole or in part, in any manner, without the prior written consent of the copyright holder, is a
+ * violation of copyright law.
  */
 
 /**
  ********************************************************************************************
- *                                 FILE HEADER                                              *
+ * FILE HEADER *
  ********************************************************************************************
- *																							*
- * FileName    		:  BusinessVerticalListModelItemRenderer.java                                                   * 	  
- *                                                                    						*
- * Author      		:  PENNANT TECHONOLOGIES              									*
- *                                                                  						*
- * Creation Date    :  14-12-2018    														*
- *                                                                  						*
- * Modified Date    :  14-12-2018    														*
- *                                                                  						*
- * Description 		:                                             							*
- *                                                                                          *
+ * * FileName : BusinessVerticalListModelItemRenderer.java * * Author : PENNANT TECHONOLOGIES * * Creation Date :
+ * 14-12-2018 * * Modified Date : 14-12-2018 * * Description : * *
  ********************************************************************************************
- * Date             Author                   Version      Comments                          *
+ * Date Author Version Comments *
  ********************************************************************************************
- * 14-12-2018       PENNANT	                 0.1                                            * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
- *                                                                                          * 
+ * 14-12-2018 PENNANT 0.1 * * * * * * * * *
  ********************************************************************************************
-*/
+ */
 package com.pennant.webui.applicationmaster.businessvertical.model;
 
 import java.io.Serializable;
@@ -77,6 +59,7 @@ public class BusinessVerticalListModelItemRenderer implements ListitemRenderer<B
 		lc = new Listcell();
 		final Checkbox act = new Checkbox();
 
+		act.setDisabled(true);
 		act.setChecked(businessVertical.isActive());
 		lc.appendChild(act);
 		lc.setParent(item);
