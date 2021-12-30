eb06536d3219511e751a8cb5894ce8906e5d5d0b
gireesh.g <gireesh.g@PENNANT-VSP78.pennant.local>
2021-12-30 15:33:53 +0530

    PSD#186212 : After saving loan,  if we delete collateral and try to add another collateral Getting validation "41005: Some one else processed this record Reference:1500AGR0013952 Collateral Ref:CT3610000001.

--- PFSWeb/src/main/java/com/pennant/webui/collateral/collateralassignment/CollateralAssignmentDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/collateral/collateralassignment/CollateralAssignmentDialogCtrl.java
@@ -976,7 +976,11 @@ public class CollateralAssignmentDialogCtrl extends GFCBaseCtrl<CollateralAssign
 
 	protected boolean doCustomDelete(final CollateralAssignment collateralAssignment, String tranType) {
 		tranType = PennantConstants.TRAN_DEL;
-		collateralAssignment.setNewRecord(true);
+
+		if (isWorkFlowEnabled()) {
+			collateralAssignment.setNewRecord(true);
+		}
+
 		AuditHeader auditHeader = newAssignmentDetailProcess(collateralAssignment, tranType);
 		auditHeader = ErrorControl.showErrorDetails(this.window_CollateralAssignmentDetailDialog, auditHeader);
 		int retValue = auditHeader.getProcessStatus();
