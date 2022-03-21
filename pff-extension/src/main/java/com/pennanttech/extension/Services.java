ddc67c8d80b28d8ce8bcfb348e3639511cb485ef
gopal.p <gopal.p@PENNANT0127.pennant.local>
2022-03-21 20:58:52 +0530

    ST#3721 - Exclude/Include menus as part of customization.

---
+++ pff-extension/src/main/java/com/pennanttech/extension/Services.java
@@ -0,0 +1,43 @@
+package com.pennanttech.extension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+import com.pennanttech.extension.web.menu.IMenuExtension;
+import com.pennanttech.pennapps.core.FactoryException;
+
+@Component
+public class Services {
+	private static Set<String> services = new HashSet<>();
+
+	@PostConstruct
+	public void initialize() {
+		services.addAll(getExtension().getExclude());
+	}
+
+	public static boolean isExclude(String serviceName) {
+		return services.contains(serviceName);
+	}
+
+	private static IMenuExtension getExtension() {
+		IMenuExtension menuExtension;
+		String exception = "The IExtensionServices implimentation should be available in the client exetension layer to override the API services list.";
+		try {
+			Object object = Class.forName("com.pennanttech.extension.web.menu.MenuExtension").newInstance();
+			if (object != null) {
+				menuExtension = (IMenuExtension) object;
+				return menuExtension;
+			} else {
+				throw new FactoryException(exception);
+			}
+		} catch (Exception e) {
+			throw new FactoryException(exception);
+
+		}
+	}
+
+}
