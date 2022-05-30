691447bdd985fe68449a86a24ce82ed6b4018975
saikrishna.p <saikrishna.p@PENNANT0185.pennant.local>
2022-05-30 15:27:04 +0530

    Refactor: Removed unnecessary throws exception for component event handlers.

--- PFSWeb/src/main/java/com/pennant/webui/applicationmaster/cluster/ClusterDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/applicationmaster/cluster/ClusterDialogCtrl.java
@@ -105,9 +105,8 @@ public class ClusterDialogCtrl extends GFCBaseCtrl<Cluster> {
 	 * The framework calls this event handler when an application requests that the window to be created.
 	 * 
 	 * @param event An event sent to the event handler of the component.
-	 * @throws Exception
 	 */
-	public void onCreate$window_ClusterDialog(Event event) throws Exception {
+	public void onCreate$window_ClusterDialog(Event event) {
 		logger.debug(Literal.ENTERING);
 
 		// Set the page level components.
--- PFSWeb/src/main/java/com/pennant/webui/applicationmaster/clusterhierarchey/ClusterHierarcheyDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/applicationmaster/clusterhierarchey/ClusterHierarcheyDialogCtrl.java
@@ -111,9 +111,8 @@ public class ClusterHierarcheyDialogCtrl extends GFCBaseCtrl<ClusterHierarchy> {
 	 * The framework calls this event handler when an application requests that the window to be created.
 	 * 
 	 * @param event An event sent to the event handler of the component.
-	 * @throws Exception
 	 */
-	public void onCreate$window_ClusterHierarcheyDialog(Event event) throws Exception {
+	public void onCreate$window_ClusterHierarcheyDialog(Event event) {
 		logger.debug(Literal.ENTERING);
 
 		// Set the page level components.
