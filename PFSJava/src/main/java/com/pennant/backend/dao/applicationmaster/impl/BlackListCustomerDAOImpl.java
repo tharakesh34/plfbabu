140050b30656ea109f5d6733c715dd5a5fe85dc6
saikrishna.p <saikrishna.p@PENNANT0185.pennant.local>
2022-07-18 12:50:24 +0530

    Re-factor: Fixed poor error handling.

--- PFSJava/src/main/java/com/pennant/backend/dao/applicationmaster/impl/BlackListCustomerDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/applicationmaster/impl/BlackListCustomerDAOImpl.java
@@ -55,6 +55,7 @@ import com.pennanttech.pennapps.core.DependencyFoundException;
 import com.pennanttech.pennapps.core.jdbc.JdbcUtil;
 import com.pennanttech.pennapps.core.jdbc.SequenceDao;
 import com.pennanttech.pennapps.core.resource.Literal;
+import com.pennanttech.pennapps.core.resource.Message;
 import com.pennanttech.pff.core.TableType;
 import com.pennanttech.pff.core.util.QueryUtil;
 
@@ -167,10 +168,9 @@ public class BlackListCustomerDAOImpl extends SequenceDao<BlackListCustomers> im
 				return su;
 			}, id);
 		} catch (EmptyResultDataAccessException e) {
-			//
+			logger.warn(Message.NO_RECORD_FOUND);
+			return null;
 		}
-
-		return null;
 	}
 
 	@Override
