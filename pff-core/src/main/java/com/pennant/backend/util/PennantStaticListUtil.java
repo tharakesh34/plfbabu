75dcbfd34e8ecac6c886f42f68163d956940f844
gireesh.g <gireesh.g@PENNANT0251.pennant.local>
2022-09-22 23:43:05 +0530

    PSD#206023:	Linking_Delinking Action Event missing in Loan Service Workflow

--- pff-core/src/main/java/com/pennant/backend/util/PennantStaticListUtil.java
+++ pff-core/src/main/java/com/pennant/backend/util/PennantStaticListUtil.java
@@ -2337,6 +2337,8 @@ public class PennantStaticListUtil {
 					"CTDS"));
 			events.add(new FinServicingEvent(FinServiceEvent.LOANDOWNSIZING,
 					Labels.getLabel("label_FinSerEvent_LoanDownSizing"), "LDS"));
+			events.add(new FinServicingEvent(FinServiceEvent.LINKDELINK,
+					Labels.getLabel("label_FinSerEvent_LinkDelink"), "LDFIN"));
 
 			if (ImplementationConstants.ALLOW_RESTRUCTURING) {
 				events.add(new FinServicingEvent(FinServiceEvent.RESTRUCTURE,
