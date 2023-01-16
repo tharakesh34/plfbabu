e136c689bd28457fd6b8de7a2af4643ef01a176a
gopal.p <gopal.p@PENNANT0127.pennant.local>
2023-01-16 12:13:29 +0530

    [#15625] Unique ID space issue.

--- PFSWeb/src/main/java/com/pennant/pff/pdc/upload/list/PDCUploadListCtrl.java
+++ PFSWeb/src/main/java/com/pennant/pff/pdc/upload/list/PDCUploadListCtrl.java
@@ -1,6 +1,7 @@
 package com.pennant.pff.pdc.upload.list;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.Window;
 
 import com.pennant.pff.upload.list.AUploadListCtrl;
@@ -10,16 +11,22 @@ import com.pennanttech.pff.file.UploadTypes;
 public class PDCUploadListCtrl extends AUploadListCtrl {
 	private static final long serialVersionUID = -296360092L;
 
-	protected Window chequeListWindow;
+	protected Window window;
 
 	protected PDCUploadListCtrl(@Autowired UploadService chequeUploadService) {
 		super(chequeUploadService, UploadTypes.CHEQUE);
 	}
 
 	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		this.window = (Window) comp;
+		super.doAfterCompose(comp);
+	}
+
+	@Override
 	protected void doSetProperties() {
 		super.moduleCode = "ChequeUpload";
-		super.onCreate(getArgument("stage"), this.chequeListWindow);
+		super.onCreate(getArgument("stage"), this.window);
 	}
 
 }
