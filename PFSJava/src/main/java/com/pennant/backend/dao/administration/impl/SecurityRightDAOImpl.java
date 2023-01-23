93e39851b39721752a6a457061f8089f2779de59
murthy.y <murthy.y@PENNANT0213.pennant.local>
2023-01-23 20:12:54 +0530

    Fields rights issue addressed.

--- PFSJava/src/main/java/com/pennant/backend/dao/administration/impl/SecurityRightDAOImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/dao/administration/impl/SecurityRightDAOImpl.java
@@ -58,16 +58,16 @@ public class SecurityRightDAOImpl extends SequenceDao<SecurityRight> implements 
 
 	@Override
 	public List<SecurityRight> getMenuRightsByUser(SecurityUser user) {
-		StringBuilder sql = new StringBuilder("select distinct RT.RightName");
+		StringBuilder sql = new StringBuilder("Select distinct RT.RightName");
 		sql.append(" from SecUserOperations uo");
-		sql.append(" inner join SecOperationRoles opr on opr.OprID = uo.OprID");
-		sql.append(" inner join SecRoles r on r.RoleID = opr.RoleID");
-		sql.append(" inner join SecRoleGroups rg on rg.RoleID = r.RoleID");
-		sql.append(" inner join SecGroupRights gr on gr.GrpID = rg.GrpID");
-		sql.append(" inner join SecRights rt on rt.RightID = gr.RightID and rt.RightType = ?");
-		sql.append(" where uo.UsrID = ?");
+		sql.append(" Inner Join SecOperationRoles opr on opr.OprID = uo.OprID");
+		sql.append(" Inner Join SecRoles r on r.RoleID = opr.RoleID");
+		sql.append(" Inner Join SecRoleGroups rg on rg.RoleID = r.RoleID");
+		sql.append(" Inner Join SecGroupRights gr on gr.GrpID = rg.GrpID");
+		sql.append(" Inner Join SecRights rt on rt.RightID = gr.RightID and rt.RightType = ?");
+		sql.append(" Where uo.UsrID = ?");
 
-		logger.trace(Literal.SQL + sql.toString());
+		logger.debug(Literal.SQL.concat(sql.toString()));
 
 		return jdbcOperations.query(sql.toString(), ps -> {
 			ps.setInt(1, 0);
@@ -83,28 +83,33 @@ public class SecurityRightDAOImpl extends SequenceDao<SecurityRight> implements 
 
 	@Override
 	public List<SecurityRight> getPageRights(SecurityRight right) {
-		StringBuilder sql = new StringBuilder("select distinct RT.RightName");
-		sql.append(" from SecUserOperations UO");
-		sql.append(" inner join SecOperationRoles OPR on OPR.OprID = UO.OprID");
-		sql.append(" inner join SecRoles R on R.RoleID = OPR.RoleID");
-		sql.append(" inner join SecRoleGroups RG on RG.RoleID = R.RoleID");
-		sql.append(" inner join SecGroupRights GR on GR.GrpID = RG.GrpID");
-		sql.append(" inner join SecRights RT on RT.RightID = GR.RightID ");
-		sql.append(" where UO.UsrID = :UsrID and R.RoleApp = :LoginAppId and RT.RightType <> 0 ");
+		StringBuilder sql = new StringBuilder("Select distinct RT.RightName");
+		sql.append(" From SecUserOperations UO");
+		sql.append(" Inner Join SecOperationRoles OPR on OPR.OprID = UO.OprID");
+		sql.append(" Inner Join SecRoles R on R.RoleID = OPR.RoleID");
+		sql.append(" Inner Join SecRoleGroups RG on RG.RoleID = R.RoleID");
+		sql.append(" Inner Join SecGroupRights GR on GR.GrpID = RG.GrpID");
+		sql.append(" Inner Join SecRights RT on RT.RightID = GR.RightID ");
+		sql.append(" Where UO.UsrID = ? and R.RoleApp = ? and RT.RightType <> ? ");
+		sql.append(" and RT.Page = ?");
+
 		if (StringUtils.isNotBlank(right.getRoleCd())) {
-			sql.append(" and R.RoleCd = :RoleCd");
-		}
-		sql.append(" and RT.Page = :Page");
-		if (StringUtils.isNotBlank(right.getMenuRight())) {
-			sql.append(" and GR.GrpID in ( select TGR.GrpID from SecGroupRights TGR inner join SecRights TR");
-			sql.append(" on TR.RightID = TGR.RightID where TR.RightName = :MenuRight )");
+			sql.append(" and R.RoleCd = ?");
 		}
 
-		// Execute the SQL, binding the arguments.
-		logger.trace(Literal.SQL + sql.toString());
-		SqlParameterSource paramSource = new BeanPropertySqlParameterSource(right);
+		logger.debug(Literal.SQL.concat(sql.toString()));
 
-		return jdbcTemplate.query(sql.toString(), paramSource, (rs, rowNum) -> {
+		return jdbcOperations.query(sql.toString(), ps -> {
+			ps.setLong(1, right.getUsrID());
+			ps.setLong(2, right.getLoginAppId());
+			ps.setInt(3, 0);
+			ps.setString(4, right.getPage());
+
+			if (StringUtils.isNotBlank(right.getRoleCd())) {
+				ps.setString(5, right.getRoleCd());
+			}
+
+		}, (rs, rowNum) -> {
 			SecurityRight sr = new SecurityRight();
 
 			sr.setRightName(rs.getString("RightName"));
@@ -115,39 +120,33 @@ public class SecurityRightDAOImpl extends SequenceDao<SecurityRight> implements 
 
 	@Override
 	public boolean isRightNameExists(String rightName) {
-		logger.debug(Literal.ENTERING);
-
-		boolean exists = false;
-		// Prepare the parameter source.
 		MapSqlParameterSource paramSource = new MapSqlParameterSource();
 		paramSource.addValue("rightName", rightName);
+
 		String sql = QueryUtil.getCountQuery(new String[] { "SecRights" }, "rightName = :rightName ");
-		logger.trace(Literal.SQL + sql);
-		Integer count = jdbcTemplate.queryForObject(sql, paramSource, Integer.class);
-		if (count > 0) {
-			exists = true;
-		}
-		logger.debug(Literal.LEAVING);
-		return exists;
+
+		logger.debug(Literal.SQL.concat(sql.toString()));
+
+		return jdbcTemplate.queryForObject(sql, paramSource, Integer.class) > 0;
 	}
 
 	@Override
 	public long save(SecurityRight right) {
-		logger.debug(Literal.ENTERING);
-
 		if (right.getId() == Long.MIN_VALUE) {
 			right.setId(getNextValue("SeqSecRights"));
-			logger.debug("get NextValue:" + right.getId());
 		}
+
 		StringBuilder sql = new StringBuilder();
 		sql.append("Insert into SecRights(RightID,RightType,RightName,Page,Version,LastMntBy,LastMntOn, ");
 		sql.append("RecordStatus, RoleCode,NextRoleCode,TaskId,NextTaskId,RecordType,WorkflowId) ");
 		sql.append("Values(:RightID, :RightType, :RightName, :Page, :Version, :LastMntBy, :LastMntOn,");
 		sql.append(":RecordStatus, :RoleCode, :NextRoleCode, :TaskId, :NextTaskId, :RecordType, :WorkflowId )");
-		logger.debug(Literal.SQL + sql.toString());
+
+		logger.debug(Literal.SQL.concat(sql.toString()));
+
 		SqlParameterSource beanParameters = new BeanPropertySqlParameterSource(right);
 		this.jdbcTemplate.update(sql.toString(), beanParameters);
-		logger.debug(Literal.ENTERING);
+
 		return right.getId();
 	}
 
