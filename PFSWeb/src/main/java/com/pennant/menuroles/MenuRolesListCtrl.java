edf295c64655c87f8768d3d76fe1decd198a367e
Sai Krishna P <saikrishna.p@pennanttech.com>
2023-02-27 11:21:32 +0530

    Re-factoring...

--- PFSWeb/src/main/java/com/pennant/menuroles/MenuRolesListCtrl.java
+++ PFSWeb/src/main/java/com/pennant/menuroles/MenuRolesListCtrl.java
@@ -2,10 +2,8 @@ package com.pennant.menuroles;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Predicate;
 
 import org.apache.commons.lang.StringUtils;
@@ -32,6 +30,8 @@ import org.zkoss.zul.Paging;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.Window;
 
+import com.pennant.backend.model.administration.SecurityGroup;
+import com.pennant.backend.model.administration.SecurityRole;
 import com.pennant.webui.util.GFCBaseListCtrl;
 import com.pennanttech.framework.core.SearchOperator;
 import com.pennanttech.framework.core.SearchOperator.Operators;
@@ -178,8 +178,10 @@ public class MenuRolesListCtrl extends GFCBaseListCtrl<MenuItem> {
 		Sheet sheet = null;
 
 		try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream stream = new ByteArrayOutputStream();) {
+			// Create a new sheet.
 			sheet = workbook.createSheet(name);
 
+			// Create header row.
 			Row row = sheet.createRow((int) rowIndex++);
 
 			Cell cell = row.createCell(0);
@@ -241,7 +243,7 @@ public class MenuRolesListCtrl extends GFCBaseListCtrl<MenuItem> {
 				rowIndex = createMenu(rowIndex, celIndex, sheet, item);
 			}
 		} else {
-			Cell cell = row.createCell(celIndex++);
+			Cell cell = row.createCell(celIndex);
 			cell.setCellValue(org.zkoss.util.resource.Labels.getLabel(menuItem.getId()));
 
 			cell = row.createCell(4);
@@ -262,53 +264,63 @@ public class MenuRolesListCtrl extends GFCBaseListCtrl<MenuItem> {
 	}
 
 	private String getRoles(String rightName, String appender) {
-		StringBuilder data = new StringBuilder();
-		StringBuilder sql = new StringBuilder();
+		// Prepare the where clause.
+		StringBuilder whereClause = new StringBuilder();
+		whereClause.append(" RoleId in (");
+		whereClause.append(" Select RoleId from SecRoleGroups where  GrpID in (");
+		whereClause.append(" Select GrpID from SecGroups where GrpID in (");
+		whereClause.append(" Select GrpID from SecGroupRights where RightId = (");
+		whereClause.append(" Select RightId from SecRights where RightName = '" + rightName + "'))))");
+
+		// Create the search object.
 		Search search = new Search();
+		search.setSearchClass(SecurityRole.class);
 		search.addField("RoleCd");
 		search.addTabelName("SecRoles");
-		sql.append(" RoleId in (");
-		sql.append(" Select RoleId from SecRoleGroups where  GrpID in (");
-		sql.append(" Select GrpID from SecGroups where GrpID in (");
-		sql.append(" Select GrpID from SecGroupRights where RightId = (");
-		sql.append(" Select RightId from SecRights where RightName ='" + rightName + "'))))");
-		search.addWhereClause(sql.toString());
+		search.addWhereClause(whereClause.toString());
 
-		List<Object> result = searchProcessor.getResults(search);
+		// Get the results.
+		List<SecurityRole> result = searchProcessor.getResults(search);
 
-		for (Object object : result) {
-			Map<String, Object> map = (Map) object;
+		StringBuilder data = new StringBuilder();
+
+		for (SecurityRole object : result) {
 			if (data.length() > 0) {
 				data.append(appender);
 			}
 
-			data.append(map.get("rolecd"));
+			data.append(object.getRoleCd());
 		}
 
 		return data.toString();
 	}
 
 	private String getGroups(String rightName, String appender) {
-		StringBuilder data = new StringBuilder();
-		StringBuilder sql = new StringBuilder();
+		// Prepare the where clause.
+		StringBuilder whereClause = new StringBuilder();
+		whereClause.append(" GrpID in (");
+		whereClause.append(" Select GrpID from SecGroups where GrpID in (");
+		whereClause.append(" Select GrpID from SecGroupRights where RightId = (");
+		whereClause.append(" Select RightId from SecRights where RightName = '" + rightName + "')))");
+
+		// Create the search object.
 		Search search = new Search();
+		search.setSearchClass(SecurityGroup.class);
 		search.addField("GrpCode");
 		search.addTabelName("SecGroups");
-		sql.append(" GrpID in (");
-		sql.append(" Select GrpID from SecGroups where GrpID in (");
-		sql.append(" Select GrpID from SecGroupRights where RightId = (");
-		sql.append(" Select RightId from SecRights where RightName ='" + rightName + "')))");
-		search.addWhereClause(sql.toString());
+		search.addWhereClause(whereClause.toString());
 
-		List<Object> result = searchProcessor.getResults(search);
+		// Get the results.
+		List<SecurityGroup> result = searchProcessor.getResults(search);
 
-		for (Object object : result) {
-			Map<String, Object> map = (Map) object;
+		StringBuilder data = new StringBuilder();
+
+		for (SecurityGroup object : result) {
 			if (data.length() > 0) {
 				data.append(appender);
 			}
 
-			data.append(map.get("grpcode"));
+			data.append(object.getGrpCode());
 		}
 
 		return data.toString();
@@ -317,9 +329,7 @@ public class MenuRolesListCtrl extends GFCBaseListCtrl<MenuItem> {
 	/**
 	 * Item renderer for listItems in the listBox.
 	 */
-	private class MenuRolesListModelItemRenderer implements ListitemRenderer<MenuItem>, Serializable {
-		private static final long serialVersionUID = 1L;
-
+	private class MenuRolesListModelItemRenderer implements ListitemRenderer<MenuItem> {
 		@Override
 		public void render(Listitem item, MenuItem data, int index) {
 			Listcell lc;
