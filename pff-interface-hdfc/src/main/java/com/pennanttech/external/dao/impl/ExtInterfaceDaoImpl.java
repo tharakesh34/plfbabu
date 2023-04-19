c7f93c398dc5718c0a638bd50ec5a010af01a7bb
sriramgoutham.ch <sriramgoutham.ch@PENNANT0216.pennant.local>
2023-04-19 19:25:01 +0530

    SFTP configuration added to Interfaces

--- pff-interface-hdfc/src/main/java/com/pennanttech/external/dao/impl/ExtInterfaceDaoImpl.java
+++ pff-interface-hdfc/src/main/java/com/pennanttech/external/dao/impl/ExtInterfaceDaoImpl.java
@@ -111,7 +111,7 @@ public class ExtInterfaceDaoImpl implements ExtInterfaceDao {
 		String queryStr;
 
 		List<ExternalConfig> list = new ArrayList<ExternalConfig>();
-		queryStr = "SELECT INTERFACE_TYPE,NO_OF_RECORDS,FILE_LOCATION,HOLD_TYPE,FILE_PREPEND,FILE_POSTPEND,FILE_EXTENSION,DATE_FORMAT,SUCCESS_INDICATOR,FAIL_INDICATOR FROM FILE_INTERFACE_CONFIG";
+		queryStr = "SELECT INTERFACE_TYPE,NO_OF_RECORDS,FILE_LOCATION,HOLD_TYPE,FILE_PREPEND,FILE_POSTPEND,FILE_EXTENSION,DATE_FORMAT,SUCCESS_INDICATOR,FAIL_INDICATOR,BACKUP_LOCATION,ACCESS_KEY,SECRET_KEY,HOST_NAME,PORT,PRIVATE_KEY,SSE_ALGORITHM,PREFIX,SFTP_LOCATION,IS_SFTP,LOCAL_BACKUP_LOCATION,SFTP_BUCKET FROM FILE_INTERFACE_CONFIG";
 
 		extNamedJdbcTemplate.getJdbcOperations().query(queryStr, rs -> {
 			ExternalConfig extConfig = new ExternalConfig();
@@ -123,9 +123,21 @@ public class ExtInterfaceDaoImpl implements ExtInterfaceDao {
 			extConfig.setFilePostpend(StringUtils.trimToEmpty(rs.getString("FILE_POSTPEND")));
 			extConfig.setFileExtension(StringUtils.trimToEmpty(rs.getString("FILE_EXTENSION")));
 			extConfig.setDateFormat(rs.getString("DATE_FORMAT"));
-
 			extConfig.setSuccessIndicator(rs.getString("SUCCESS_INDICATOR"));
 			extConfig.setFailIndicator(rs.getString("FAIL_INDICATOR"));
+
+			extConfig.setFileBackupLocation(rs.getString("BACKUP_LOCATION"));
+			extConfig.setAccessKey(rs.getString("ACCESS_KEY"));
+			extConfig.setSecretKey(rs.getString("SECRET_KEY"));
+			extConfig.setHostName(rs.getString("HOST_NAME"));
+			extConfig.setPort(rs.getInt("PORT"));
+			extConfig.setPrivateKey(rs.getString("PRIVATE_KEY"));
+			extConfig.setSseAlgo(rs.getString("SSE_ALGORITHM"));
+			extConfig.setSftpPrefix(rs.getString("PREFIX"));
+			extConfig.setFileSftpLocation(rs.getString("SFTP_LOCATION"));
+			extConfig.setIsSftp(rs.getString("IS_SFTP"));
+			extConfig.setFileLocalBackupLocation(rs.getString("LOCAL_BACKUP_LOCATION"));
+			extConfig.setSftpBucketLocation(rs.getString("SFTP_BUCKET"));
 
 			list.add(extConfig);
 		});
