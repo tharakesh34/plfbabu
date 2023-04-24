10d98a672374b02176ade5ff2effd01271d3d905
gireesh.g <gireesh.g@PENNANT0251.pennant.local>
2023-04-24 11:48:01 +0530

    Fee Type Code Should be visible along with Fee Type Description in Payment Instructions Screen

--- PFSWeb/src/main/java/com/pennant/webui/payment/paymentheader/PaymentHeaderDialogCtrl.java
+++ PFSWeb/src/main/java/com/pennant/webui/payment/paymentheader/PaymentHeaderDialogCtrl.java
@@ -1986,7 +1986,7 @@ public class PaymentHeaderDialogCtrl extends GFCBaseCtrl<PaymentHeader> {
 			BigDecimal calGST = BigDecimal.ZERO;
 			BigDecimal availAmount = pd.getAvailableAmount();
 			BigDecimal paidAmount = pd.getAmount();
-			String desc = pd.getFeeTypeDesc();
+			String desc = pd.getFeeTypeCode().concat(("-")).concat(pd.getFeeTypeDesc());
 
 			// GST Calculations
 			TaxHeader taxHeader = pd.getTaxHeader();
