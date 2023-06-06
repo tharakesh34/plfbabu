1598fc28003ddb30139325dd2358813c322fdbd2
saikiran.n <saikiran.n@PENNANT0249.pennant.local>
2023-06-07 00:31:50 +0530

    227448 - Entries in SOA after loan cancellation are not proper

--- PFSJava/src/main/java/com/pennant/backend/service/reports/impl/SOAReportGenerationServiceImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/service/reports/impl/SOAReportGenerationServiceImpl.java
@@ -2066,7 +2066,7 @@ public class SOAReportGenerationServiceImpl extends GenericService<StatementOfAc
 				}
 
 				if (StringUtils.isBlank(closingStatus)
-						|| FinanceConstants.CLOSE_STATUS_CANCELLED.equals(finMain.getClosingStatus())) {
+						|| !FinanceConstants.CLOSE_STATUS_CANCELLED.equals(finMain.getClosingStatus())) {
 					String event = "";
 					for (FinOverDueCharges finODCAmount : odcAmounts) {
 						if (finODCAmount.getSchDate().compareTo(finSchdDetail.getSchDate()) == 0
@@ -2515,13 +2515,8 @@ public class SOAReportGenerationServiceImpl extends GenericService<StatementOfAc
 					soaTranReport.setEvent(rHEventExcess + status + allocMsg);
 					soaTranReport.setTransactionDate(receiptDate);
 					soaTranReport.setCreditAmount(rd.getAmount());
+					soaTranReport.setDebitAmount(BigDecimal.ZERO);
 
-					if ("EXCESS".equals(rpaymentType) || "PAYABLE".equals(rpaymentType)) {
-						soaTranReport.setDebitAmount(rd.getAmount());
-						soaTranReport.setCreditAmount(BigDecimal.ZERO);
-					} else {
-						soaTranReport.setDebitAmount(BigDecimal.ZERO);
-					}
 					soaTranReport.setPriority(10);
 					if (!RepayConstants.PAYSTATUS_CANCEL.equals(receiptModeStatus)) {
 						soaTransactionReports.add(soaTranReport);
