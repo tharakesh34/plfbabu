960b5af400ee60b94f2b64cc88bfebefa6b7c4c8
gopal.p <gopal.p@PENNANT0127.pennant.local>
2023-06-24 04:42:48 +0530

    data engine & account updator upgrade

--- PFSJava/src/main/java/com/pennant/backend/service/finance/impl/FinanceDetailServiceImpl.java
+++ PFSJava/src/main/java/com/pennant/backend/service/finance/impl/FinanceDetailServiceImpl.java
@@ -3644,8 +3644,8 @@ public class FinanceDetailServiceImpl extends GenericFinanceDetailService implem
 		String roleCode = "";
 		List<AuditDetail> auditDetails = new ArrayList<AuditDetail>();
 
-		FinanceDetail fd = (FinanceDetail) aAuditHeader.getAuditDetail().getModelData();
-		fd.setValidateUpfrontFees(true);
+		FinanceDetail financeDetail = (FinanceDetail) aAuditHeader.getAuditDetail().getModelData();
+		financeDetail.setValidateUpfrontFees(true);
 		aAuditHeader = businessValidation(aAuditHeader, "doApprove", isWIF, false);
 
 		if (!isWIF) {
@@ -3656,6 +3656,8 @@ public class FinanceDetailServiceImpl extends GenericFinanceDetailService implem
 		}
 
 		AuditHeader auditHeader = ObjectUtil.clone(aAuditHeader);
+
+		FinanceDetail fd = (FinanceDetail) auditHeader.getAuditDetail().getModelData();
 
 		// process to send FIN-one request and create or update the cust data.
 		String moduleDefiner = fd.getModuleDefiner();
